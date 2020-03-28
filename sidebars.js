df10620c69a6acc173fc48c1d8ee7e82cd62f6cc
Hammad Jutt <jutt@ualberta.ca>
2020-03-27 20:31:21 -0600

    Update sidebar with Manifesto pages

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,11 @@
 module.exports = {
   someSidebar: {
     "📕 Handbook": ["introduction"],
+    "📜 Manifesto": [
+      "manifesto/pillar-articles",
+      "manifesto/metafam-way",
+      "manifesto/founders"
+    ],
     "🌳 Resources": ["resources/graphics", "resources/art", "resources/memes"],
     "About The Wiki": ["style-guide", "mdx"]
   }
