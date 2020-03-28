3dae18ce5283bd38ef7f64e7bd330378719c13ae
Hammad Jutt <jutt@ualberta.ca>
2020-03-27 20:21:33 -0600

    add memes page and fix sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+    "📕 Handbook": ["introduction"],
+    "🌳 Resources": ["resources/graphics", "resources/art", "resources/memes"],
+    "About The Wiki": ["style-guide", "mdx"]
+  }
 };
