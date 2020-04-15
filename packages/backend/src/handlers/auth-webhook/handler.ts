0e73c389745e51908cdf44a533227a0d1fd3b92d
Pacien Boisson <pakokrew@gmail.com>
2020-04-15 15:44:44 +0200

    Update backend to typescript

--- packages/backend/src/handlers/auth-webhook/handler.ts
+++ packages/backend/src/handlers/auth-webhook/handler.ts
@@ -1,8 +1,10 @@
+import { Request, Response } from 'express';
+
 const unauthorizedVariables = {
   'X-Hasura-Role': 'public',
 };
 
-const handler = async (req, res) => {
+const handler = async (req: Request, res: Response) => {
 
   // const userId = 'dc6b03eb-91fb-41d2-b739-ec74a5ef76fe';
   // const role = 'player';
