8dc0031fe3d5317d42bff584be9241dd736be60b
Pacien Boisson <pakokrew@gmail.com>
2020-07-13 11:40:26 +0200

    get3BoxProfile handler

--- packages/backend/src/handlers/actions/routes.ts
+++ packages/backend/src/handlers/actions/routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import { asyncHandlerWrapper } from '../../lib/apiHelpers';
+
+import getBoxProfile from './getBoxProfile/handler';
 import { updateBoxProfileHandler } from './updateBoxProfile/handler';
 
 export const actionRoutes = express.Router();
 
-actionRoutes.post(
-  '/updateBoxProfile',
-  asyncHandlerWrapper(updateBoxProfileHandler),
-);
+actionRoutes.post('/getBoxProfile', asyncHandlerWrapper(getBoxProfile));
+
+actionRoutes.post('/updateBoxProfile', asyncHandlerWrapper(updateBoxProfileHandler));
+
