d0ddd5298e55c1887af80c3ceea9629b59bcba81
Kris Urbas <krzysu@interia.pl>
2020-07-29 10:19:50 +0200

    add fonts to storybook

--- packages/design-system/.storybook/themeDecorator.js
+++ packages/design-system/.storybook/themeDecorator.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { GlobalStyle, ThemeProvider, theme } from '../src';
+import GoogleFontLoader from 'react-google-font-loader';
 
 const ThemeDecorator = (storyFn) => (
   <ThemeProvider theme={theme}>
+    <GoogleFontLoader
+      fonts={[
+        {
+          font: 'IBM+Plex+Mono',
+          weights: [400, 700],
+        },
+        {
+          font: 'IBM+Plex+Sans',
+          weights: [400, 700],
+        },
+        {
+          font: 'Press+Start+2P',
+        },
+      ]}
+    />
     <GlobalStyle />
     {storyFn()}
   </ThemeProvider>
