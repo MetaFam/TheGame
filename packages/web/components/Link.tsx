2d2ff96bf96a461d26f74f2627419b3ae8233177
dan13ram <dan13ram@gmail.com>
2020-08-03 13:25:30 +0530

    review comments incorporated

--- packages/web/components/Link.tsx
+++ packages/web/components/Link.tsx
@@ -2,15 +2,28 @@ import { Link, LinkProps as DSLinkProps } from '@metafam/ds';
 import NextLink, { LinkProps } from 'next/link';
 import React from 'react';
 
-type MetaLinkProps = LinkProps & DSLinkProps;
+type Props = Omit<DSLinkProps, keyof LinkProps> & LinkProps;
 
-export const MetaLink: React.FC<MetaLinkProps> = ({
+export const MetaLink: React.FC<Props> = ({
   children,
   href,
   as,
+  passHref,
+  prefetch,
+  replace,
+  scroll,
+  shallow,
   ...props
 }) => (
-  <NextLink href={href} as={as} passHref>
+  <NextLink
+    href={href}
+    as={as}
+    passHref={passHref || true}
+    prefetch={prefetch}
+    replace={replace}
+    scroll={scroll}
+    shallow={shallow}
+  >
     {/*  NextLink passes the href */}
     {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
     <Link {...props}>{children}</Link>
