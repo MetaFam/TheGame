e03ffe2763e1d53b95138856e2e06b3125af569c
dan13ram <dan13ram@users.noreply.github.com>
2020-08-05 13:34:57 +0530

    login landing page + fixed fonts (#51)
    * login landing page + fixed fonts
    
    * fixed eslint errors
    
    * fixed most of typecheck errors
    
    * review comments incorporated

--- packages/web/components/Link.tsx
+++ packages/web/components/Link.tsx
@@ -1,13 +1,31 @@
-import { Link } from '@metafam/ds';
+import { Link, LinkProps as DSLinkProps } from '@metafam/ds';
 import NextLink, { LinkProps } from 'next/link';
 import React from 'react';
 
-type Props = LinkProps;
+type Props = Omit<DSLinkProps, keyof LinkProps> & LinkProps;
 
-export const MetaLink: React.FC<Props> = ({ children, ...props }) => (
-  <NextLink {...props} passHref>
+export const MetaLink: React.FC<Props> = ({
+  children,
+  href,
+  as,
+  passHref,
+  prefetch,
+  replace,
+  scroll,
+  shallow,
+  ...props
+}) => (
+  <NextLink
+    href={href}
+    as={as}
+    passHref={passHref || true}
+    prefetch={prefetch}
+    replace={replace}
+    scroll={scroll}
+    shallow={shallow}
+  >
     {/*  NextLink passes the href */}
     {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-    <Link>{children}</Link>
+    <Link {...props}>{children}</Link>
   </NextLink>
 );
