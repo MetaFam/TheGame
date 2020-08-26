6a569429d9514a9ae9c2b32f25962fd39d151976
Hammad Jutt <jutt@ualberta.ca>
2020-08-26 02:46:07 -0600

    Remove incorrect migration

--- hasura/migrations/1595681063913_insert_seed_data/up.sql
+++ hasura/migrations/1595681063913_insert_seed_data/up.sql
@@ -1,3 +1,1 @@
-INSERT INTO "Skill" (id, name) VALUES ('e3e96e33-abcd-4f8e-a3f3-b360a567055c', 'Javascript');
-
 INSERT INTO "Guild" (id, type, identifier, name, logo) VALUES ('cd01cac9-abcd-42c3-908f-b8b0368eeef3', 'ARAGON', '0xd21cf378e2e40bda4597594738bb35e9ccc97da7', 'MetaFam', 'https://metagame.wtf/static/f3f97018016dd123ab7a9d6f8aaecc04/9911c/logo.png');
