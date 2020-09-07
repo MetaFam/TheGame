04b8a2368cede469febf114ab4de77a91edcff61
Hammad Jutt <jutt@ualberta.ca>
2020-09-06 18:54:23 -0600

    Fix handler for SourceCred migration
    Was breaking before due to unique key conflicts because sourceCredIdentity IDs have potential to change around.

--- packages/backend/src/handlers/graphql/mutations.ts
+++ packages/backend/src/handlers/graphql/mutations.ts
@@ -16,14 +16,14 @@ export const CreatePlayerFromETH = gql`
 `;
 
 export const UpsertAccount = gql`
-  mutation UpsertAccount($objects: [Account_insert_input!]!) {
-    insert_Account(
-      objects: $objects
-      on_conflict: {
-        constraint: Account_identifier_type_key
-        update_columns: [identifier]
-      }
-    ) {
+  mutation UpsertAccount(
+    $objects: [Account_insert_input!]!
+    $on_conflict: Account_on_conflict = {
+      constraint: Account_identifier_type_key
+      update_columns: []
+    }
+  ) {
+    insert_Account(objects: $objects, on_conflict: $on_conflict) {
       affected_rows
     }
   }
@@ -36,6 +36,56 @@ export const UpsertPlayer = gql`
   ) {
     insert_Player(on_conflict: $onConflict, objects: $objects) {
       affected_rows
+    }
+  }
+`;
+
+export const UpdatePlayer = gql`
+  mutation UpdatePlayer(
+    $ethAddress: String
+    $identityId: String
+    $username: String
+    $rank: Player_Rank_enum
+    $totalXp: numeric
+    $discordId: String
+  ) {
+    update_Player(
+      where: {
+        _or: [
+          {
+            ethereum_address: { _eq: $ethAddress }
+            scIdentityId: { _eq: $identityId }
+          }
+          {
+            ethereum_address: { _eq: $ethAddress }
+            username: { _eq: $username }
+          }
+          { scIdentityId: { _eq: $identityId } }
+          {
+            Accounts: {
+              _and: {
+                type: { _eq: "DISCORD" }
+                identifier: { _eq: $discordId }
+              }
+            }
+          }
+        ]
+      }
+      _set: {
+        ethereum_address: $ethAddress
+        scIdentityId: $identityId
+        username: $username
+        rank: $rank
+        totalXp: $totalXp
+      }
+    ) {
+      affected_rows
+      returning {
+        id
+        ethereum_address
+        scIdentityId
+        username
+      }
     }
   }
 `;
