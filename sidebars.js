1fb9f5d46943d7c7954e4f7408bb95a41dbc6d72
Dave Sayer <me@luxumbra.dev>
2020-10-08 12:12:42 +0100

    #128 design tweaks (#132)
    * Fixed couple of Markdown validation errors.
    
    * commented out pages that dont exist
    
    * Style tweaks done for #128:
    Changes as per  @peth-yursick comments.
    * Changed colors to match MyMeta
    * MyMeta font used for body copy
    * Removed white divider
    * Top navbar hidden
    * Removed space for footer
    * Vertically centred homepage content.
    
    * Typo removed
    
    * Fixed broken build - pesky window object
    
    * The : in the title was causing validation issues.

--- sidebars.js
+++ sidebars.js
@@ -1,53 +1,53 @@
 module.exports = {
-    someSidebar: {
-        "🐣 Start Here": [
-            "introduction",
-            "start-here/for-players",
-            "start-here/for-guilds",
-            "start-here/for-patrons",
-        ],
-        "🤨 WTF is MetaGame?": [
-            "wtf-is-metagame/in-search-of-commons-that-scale",
-            "wtf-is-metagame/the-coming-of-metagame",
-            "wtf-is-metagame/wtf-is-metagame",
-            "wtf-is-metagame/way-of-web3-founders",
-            "wtf-is-metagame/narrative-1-a-decentralized-factory",
-            "wtf-is-metagame/narrative-2-a-web-of-opportunity",
-            "wtf-is-metagame/metafam-way",
-        ],
-        "🧐 How Does It Work": [
-            "how-does-it-work/how-metagame-works",
-            "how-does-it-work/xp",
-            "how-does-it-work/phases-of-metagame",
-            "how-does-it-work/become-a-founder-of-metagame",
-        ],
-        "🗺 Enter MetaGame": [
-            "enter-metagame/signpost",
-            "enter-metagame/how-to-become-a-player",
-            "enter-metagame/roles-in-metagame",
-            "enter-metagame/navigation-board",
-            "enter-metagame/leaderboard",
-        ],
-        "🏡 The Great Houses": [
-            "great-houses/house-of-daos",
-            "great-houses/house-of-defiance",
-            "great-houses/house-of-dapps",
-            "great-houses/house-of-tokens",
-        ],
-        "⚔️ Guilding": [
-            "Guilding/what-why-how-of-your-dao",
-            "Guilding/guilding-101",
-            "Guilding/building-successful-teams",
-        ],
+  someSidebar: {
+    '🐣 Start Here': [
+      'introduction',
+      'start-here/for-players',
+      'start-here/for-guilds',
+      'start-here/for-patrons',
+    ],
+    '🤨 WTF is MetaGame?': [
+      'wtf-is-metagame/in-search-of-commons-that-scale',
+      'wtf-is-metagame/the-coming-of-metagame',
+      'wtf-is-metagame/wtf-is-metagame',
+      'wtf-is-metagame/way-of-web3-founders',
+      'wtf-is-metagame/narrative-1-a-decentralized-factory',
+      'wtf-is-metagame/narrative-2-a-web-of-opportunity',
+      'wtf-is-metagame/metafam-way',
+    ],
+    '🧐 How Does It Work': [
+      'how-does-it-work/how-metagame-works',
+      'how-does-it-work/xp',
+      'how-does-it-work/phases-of-metagame',
+      'how-does-it-work/become-a-founder-of-metagame',
+    ],
+    '🗺 Enter MetaGame': [
+      'enter-metagame/signpost',
+      'enter-metagame/how-to-become-a-player',
+      'enter-metagame/roles-in-metagame',
+      'enter-metagame/navigation-board',
+      'enter-metagame/leaderboard',
+    ],
+    '🏡 The Great Houses': [
+      'great-houses/house-of-daos',
+      //   'great-houses/house-of-defiance',
+      //   'great-houses/house-of-dapps',
+      //   'great-houses/house-of-tokens',
+    ],
+    '⚔️ Guilding': [
+      'Guilding/what-why-how-of-your-dao',
+      'Guilding/guilding-101',
+      'Guilding/building-successful-teams',
+    ],
 
-        "🌳 Other": [
-            "resources/glossary",
-            "resources/graphics",
-            "resources/art",
-            "resources/memes",
-            "resources/talks",
-            "resources/alignment2021",
-        ],
-        // "About The Wiki": ["style-guide", "mdx"],
-    },
+    '🌳 Other': [
+      'resources/glossary',
+      'resources/graphics',
+      'resources/art',
+      'resources/memes',
+      'resources/talks',
+      'resources/alignment2021',
+    ],
+    // "About The Wiki": ["style-guide", "mdx"],
+  },
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,107 +1,113 @@
-import React from "react";
-import classnames from "classnames";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import styles from "./styles.module.css";
+import React, { useEffect, useState } from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
 
 const features = [
-    {
-        title: "For Players",
-        imageUrl: "assets/players.png",
-        description: "",
-        linkText: "Through here",
-        linkTo: "docs/start-here/for-players",
-    },
-    {
-        title: "For Guilds",
-        imageUrl: "assets/guilds.png",
-        description: "",
-        linkText: "Through here",
-        linkTo: "docs/start-here/for-guilds",
-    },
-    {
-        title: "For Patrons",
-        imageUrl: "assets/patrons.png",
-        description: "",
-        linkText: "Through here",
-        linkTo: "docs/start-here/for-patrons",
-    },
+  {
+    title: 'For Players',
+    imageUrl: 'assets/players.png',
+    description: '',
+    linkText: 'Through here',
+    linkTo: 'docs/start-here/for-players',
+  },
+  {
+    title: 'For Guilds',
+    imageUrl: 'assets/guilds.png',
+    description: '',
+    linkText: 'Through here',
+    linkTo: 'docs/start-here/for-guilds',
+  },
+  {
+    title: 'For Patrons',
+    imageUrl: 'assets/patrons.png',
+    description: '',
+    linkText: 'Through here',
+    linkTo: 'docs/start-here/for-patrons',
+  },
 ];
 
 function Feature({ imageUrl, title, description, linkText, linkTo }) {
-    const imgUrl = useBaseUrl(imageUrl);
-    return (
-        <div className={classnames("col col--4", styles.feature)}>
-            {imgUrl && (
-                <div className='text--center'>
-                    <img
-                        className={styles.featureImage}
-                        src={imgUrl}
-                        alt={title}
-                    />
-                </div>
-            )}
-            <div className='text--center'>
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <Link
-                    className='button button--primary button--outline'
-                    to={useBaseUrl(linkTo)}
-                >
-                    {linkText}
-                </Link>
-            </div>
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={classnames('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className='text--center'>
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
-    );
+      )}
+      <div className='text--center'>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <Link
+          className='button button--primary button--outline'
+          to={useBaseUrl(linkTo)}
+        >
+          {linkText}
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 function Home() {
-    const context = useDocusaurusContext();
-    const { siteConfig = {} } = context;
-    return (
-        <Layout title={siteConfig.title} description={siteConfig.tagline}>
-            <header className={classnames("hero", styles.heroBanner)}>
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  let wikipath = 'home';
+
+  useEffect(() => {
+    wikipath = window.location.pathname === '/' ? 'home' : 'docs';
+  });
+
+  return (
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
+      <div className={classnames('inner-wrapper', wikipath)}>
+        <div className={classnames('wrapper')}>
+          <header className={classnames('hero', styles.heroBanner)}>
+            <div className='container'>
+              <img
+                alt='MetaGame Wiki Logo'
+                className={styles.wikiLogo}
+                width='555'
+                src={useBaseUrl('img/wiki-logo.png')}
+              />
+              <p className='hero__subtitle'>{siteConfig.tagline}</p>
+              <div className={styles.buttons}>
+                <Link
+                  className='button button--primary button--lg'
+                  to={useBaseUrl('docs/introduction')}
+                >
+                  EXPLORE
+                </Link>
+              </div>
+            </div>
+          </header>
+          <main>
+            {features && features.length && (
+              <section className={styles.features}>
                 <div className='container'>
-                    <img
-                        alt='MetaGame Wiki Logo'
-                        className={styles.wikiLogo}
-                        width='555'
-                        src={useBaseUrl("img/wiki-logo.png")}
-                    />
-                    <p className='hero__subtitle'>{siteConfig.tagline}</p>
-                    <div className={styles.buttons}>
-                        <Link
-                            className='button button--primary button--lg'
-                            to={useBaseUrl("docs/introduction")}
-                        >
-                            EXPLORE
-                        </Link>
-                    </div>
+                  <div
+                    className='row'
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    {features.map((props, idx) => (
+                      <Feature key={idx} {...props} />
+                    ))}
+                  </div>
                 </div>
-            </header>
-            <main>
-                {features && features.length && (
-                    <section className={styles.features}>
-                        <div className='container'>
-                            <div
-                                className='row'
-                                style={{
-                                    display: "flex",
-                                    justifyContent: "center",
-                                }}
-                            >
-                                {features.map((props, idx) => (
-                                    <Feature key={idx} {...props} />
-                                ))}
-                            </div>
-                        </div>
-                    </section>
-                )}
-            </main>
-        </Layout>
-    );
+              </section>
+            )}
+          </main>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default Home;
