29c3af6b02c9c7f2dfe35171771740771eb78260
Dave Sayer <me@luxumbra.dev>
2020-10-29 22:13:33 +0000

    homepage redesign (#150)
    * WIP on the homepage redesign
    
    * Refactor of the homepage after the redesign.
    
    * Was using the incorrect element
    
    * margin auto to make sure the content is centred

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,44 +10,140 @@ const features = [
   {
     title: 'For Players',
     imageUrl: 'assets/players.png',
-    description: '',
+    description:
+      '<p>MetaGame is for those who want to play an active role in building the future.</p> <p>For those who want to:</p> <ul><li>Build their knowledge, get experience & level up.</li> <li>Find cool projects, solve problems & get paid.</li> <li>Become a part of something bigger.</li></ul>',
     linkText: 'Through here',
     linkTo: 'docs/start-here/for-players',
   },
   {
     title: 'For Guilds',
     imageUrl: 'assets/guilds.png',
-    description: '',
-    linkText: 'Through here',
-    linkTo: 'docs/start-here/for-guilds',
+    description:
+      '<p>Its also for groups of people, those building tools & services for a decentralized future.</p> <p>For those who want:</p> <ul><li>Help finding tools, frameworks & funds accessible.</li> <li>Help getting value-aligned contributors & adopters.</li> <li>Become a part of the "new world" puzzle.</li></ul>',
+    // linkText: 'Through here',
+    // linkTo: 'docs/start-here/for-guilds',
   },
   {
     title: 'For Patrons',
     imageUrl: 'assets/patrons.png',
-    description: '',
-    linkText: 'Through here',
-    linkTo: 'docs/start-here/for-patrons',
+    description:
+      '<p>Those who really want to see MetaGame succeed, but prefer to help with funds.</p> <p>Why?</p> <ul><li>They love builder onboarding & support systems.</li> <li>Membership and other things, all paid in Seeds.</li> <li>Understanding MetaGame made them go: Fuck yeah!</li></ul>',
+    // linkText: 'Through here',
+    // linkTo: 'docs/start-here/for-patrons',
+  },
+  {
+    title: 'For Lamers',
+    imageUrl: 'assets/lamers.png',
+    description:
+      '<p>Those who prefer to push their self-interest over everyone & only detract from the commons.</p> <p>NOT for:</p> <ul><li>Those who want to get rich quick & buy Lambos.</li><li>Those who say, but do not do.</li><li>Those who like to complain.</li></ul>',
+    // linkText: 'Through here',
+    // linkTo: 'docs/start-here/for-patrons',
   },
 ];
 
-function Feature({ imageUrl, title, description, linkText, linkTo }) {
+const wrapperStyle = {
+  width: '740px',
+  maxWidth: '740px',
+  margin: '5rem auto',
+  // overflow: 'hidden',
+};
+const panelStyle = {
+  backgroundColor: 'rgba(255,255,255,0.08)',
+  // borderRadius: '6px',
+  display: 'flex',
+  alignItems: 'flex-start',
+  flexFlow: 'row nowrap',
+  flex: '0 0 100%',
+  marginBottom: '30px',
+  padding: '30px',
+  textAlign: 'left',
+  width: '100%',
+};
+const lastPanelStyle = {
+  ...panelStyle,
+  marginBottom: '50px',
+};
+const lamerPanelStyle = {
+  ...panelStyle,
+  marginBottom: '30px',
+};
+const imgWrap = {
+  flex: '0 0 33%',
+  width: '33%',
+};
+const mainWrap = {
+  flexGrow: 1,
+  paddingLeft: '0px',
+};
+const aspectBox = {
+  position: 'relative',
+  height: '205px',
+  width: '203px',
+  overflow: 'hidden',
+  textAlign: 'center',
+};
+const imgStyle = {
+  width: '100%',
+  height: '100%',
+};
+const lamerImgStyle = {
+  ...imgStyle,
+  width: '185px',
+  height: '182px',
+};
+
+const sectionHeading = {
+  fontFamily: '"IBM Plex Mono"',
+  fontSize: '24px',
+  color: '#a5b9f6',
+  marginBottom: '13px',
+};
+const panelTitleStyle = {
+  ...sectionHeading,
+  color: 'white',
+  marginBottom: '3px',
+};
+const textStyle = {
+  fontSize: '17px',
+};
+
+function Panel({
+  imageUrl,
+  title,
+  description,
+  linkText,
+  linkTo,
+  lastPlayerPanel,
+  lamerPanel,
+}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={classnames('col col--4', styles.feature)}>
+    <div
+      className={classnames(
+        'col',
+        styles.feature,
+        styles.panel,
+        lastPlayerPanel && styles.lastItem,
+      )}
+      style={lastPlayerPanel ? lastPanelStyle : panelStyle}
+    >
       {imgUrl && (
-        <div className={classnames('text--center', styles.imageWrapper)}>
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-          {/* <div> */}
-          <Link
-            className='button button--primary button--outline'
-            to={useBaseUrl(linkTo)}
-          >
-            <span>{title}</span>
-          </Link>
-          {/* </div> */}
+        <div style={imgWrap} className={styles.imageWrapper}>
+          <div style={aspectBox}>
+            <img
+              style={lamerPanel ? lamerImgStyle : imgStyle}
+              src={imgUrl}
+              alt={title}
+            />
+          </div>
         </div>
       )}
-      <div className='text--center'>{description && <p>{description}</p>}</div>
+      <div style={mainWrap}>
+        {title && <h3 style={panelTitleStyle}>{title}</h3>}
+        {description && (
+          <div dangerouslySetInnerHTML={{ __html: description }} />
+        )}
+      </div>
     </div>
   );
 }
@@ -56,15 +152,14 @@ function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
   let wikipath = 'home';
-
   useEffect(() => {
     wikipath = window.location.pathname === '/' ? 'home' : 'docs';
   });
 
   return (
     <Layout title={siteConfig.title} description={siteConfig.tagline}>
-      <div className={classnames('inner-wrapper', wikipath)}>
-        <div className={classnames('wrapper')}>
+      <div className={classnames('wrapper', wikipath)}>
+        <div className={classnames('inner-wrapper')} style={wrapperStyle}>
           <header className={classnames('hero', styles.heroBanner)}>
             <div className='container'>
               <img
@@ -74,14 +169,6 @@ function Home() {
                 src={useBaseUrl('img/wiki-logo.png')}
               />
               <p className={styles.heroSubtitle}>{siteConfig.tagline}</p>
-              <div className={styles.buttons}>
-                <Link
-                  className='button button--primary button--lg'
-                  to={useBaseUrl('docs/introduction')}
-                >
-                  EXPLORE
-                </Link>
-              </div>
             </div>
           </header>
           <main>
@@ -95,13 +182,52 @@ function Home() {
                       justifyContent: 'center',
                     }}
                   >
-                    {features.map((props, idx) => (
-                      <Feature key={idx} {...props} />
-                    ))}
+                    <h2 style={sectionHeading}>So, who is it for?</h2>
+                    {features &&
+                      features.map((props, i, idx) => (
+                        <>
+                          {features.length === i + 1 && (
+                            <h2 style={sectionHeading}>Who is it NOT for?</h2>
+                          )}
+                          <Panel
+                            key={idx}
+                            lastPlayerPanel={
+                              i === features.length - 2 ? 'last-item' : ''
+                            }
+                            lamerPanel={
+                              i === features.length - 1 ? 'lamer-panel' : ''
+                            }
+                            {...props}
+                          />
+                        </>
+                      ))}
                   </div>
                 </div>
               </section>
             )}
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <div className={styles.buttons}>
+                <Link
+                  className={classnames(
+                    'button button--primary button--lg button--explore',
+                    styles.button,
+                  )}
+                  to={useBaseUrl('docs/introduction')}
+                >
+                  EXPLORE
+                </Link>
+                <Link
+                  className={classnames(
+                    'button button--primary button--lg button--join',
+                    styles.button,
+                    styles.btnJoin,
+                  )}
+                  to={useBaseUrl('docs/enter-metagame/join-metagame')}
+                >
+                  JOIN
+                </Link>
+              </div>
+            </div>
           </main>
         </div>
       </div>
