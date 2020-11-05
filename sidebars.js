19d9a9743a29505729ccd3f5e9477d8cdf81c3c8
peth-yursick <55857222+peth-yursick@users.noreply.github.com>
2020-11-05 23:46:57 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    '🏡 Home': ['home'],
+    '🌀 MetaOS': ['home'],
     '🤨 Pillar Memes': [
       'wtf-is-metagame/in-search-of-commons-that-scale',
       'wtf-is-metagame/the-coming-of-metagame',
