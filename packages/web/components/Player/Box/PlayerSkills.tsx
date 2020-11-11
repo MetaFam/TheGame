45dfaff3dce5394cba730ffca5e6643d45e01cc0
Nolan Vanmoortel <nolan@o2b.dev>
2020-11-11 21:45:45 +0100

    Implement player mockup based on Design reference

---
+++ packages/web/components/Player/Box/PlayerSkills.tsx
@@ -0,0 +1,45 @@
+import { MetaTag, Wrap } from '@metafam/ds';
+import { PlayerFragmentFragment } from 'graphql/autogen/types';
+import React from 'react';
+
+import { PlayerBox } from './PlayerBoxe';
+
+type Props = { player: PlayerFragmentFragment; setRemoveBox: () => void };
+export const PlayerSkills: React.FC<Props> = ({ player, setRemoveBox }) => {
+  return (
+    <PlayerBox title="Skills" setRemoveBox={setRemoveBox}>
+      <Wrap>
+        {!(player.Player_Skills || []).length && (
+          <MetaTag
+            size="md"
+            fontWeight="normal"
+            backgroundColor="rgba(70, 20, 100, 0.8)"
+            pt={2}
+            pb={2}
+            pl={4}
+            pr={4}
+          >
+            Gamer
+          </MetaTag>
+        )}
+        {
+          // TODO based on reference design category color was not used ?
+        }
+        {(player.Player_Skills || []).map(({ Skill }) => (
+          <MetaTag
+            key={Skill.id}
+            size="md"
+            fontWeight="normal"
+            backgroundColor="rgba(70, 20, 100, 0.8)"
+            pt={2}
+            pb={2}
+            pl={4}
+            pr={4}
+          >
+            {Skill.name}
+          </MetaTag>
+        ))}
+      </Wrap>
+    </PlayerBox>
+  );
+};
--- packages/web/pages/player/[username].tsx
+++ packages/web/pages/player/[username].tsx
@@ -1,20 +1,8 @@
-import {
-  Container,
-  HStack,
-  Image,
-  MetaBox,
-  MetaTag,
-  P,
-  SimpleGrid,
-  Text,
-  Wrap,
-} from '@metafam/ds';
-import { FlexContainer } from 'components/Container';
-import { PlayerFeatures } from 'components/Player/PlayerFeatures';
-import { PlayerHero } from 'components/Player/PlayerHero';
+import { Box, Container, Flex } from '@metafam/ds';
+import BackgroundImage from 'assets/profile-background.jpg';
+import { PlayerHero } from 'components/Player/Box/PlayerHero';
 import { getPlayer } from 'graphql/getPlayer';
 import { getPlayers } from 'graphql/getPlayers';
-import { PersonalityTypes, SkillColors } from 'graphql/types';
 import {
   GetStaticPaths,
   GetStaticPropsContext,
@@ -22,73 +10,174 @@ import {
 } from 'next';
 import Error from 'next/error';
 import React from 'react';
-import { getPlayerDescription } from 'utils/playerHelpers';
+
+import { PlayerAchievements } from '../../components/Player/Box/PlayerAchievements';
+import { PlayerAddBox } from '../../components/Player/Box/PlayerAddBox';
+import { PlayerCollab } from '../../components/Player/Box/PlayerCollab';
+import { PlayerContactButtons } from '../../components/Player/Box/PlayerContactButtons';
+import { PlayerGallery } from '../../components/Player/Box/PlayerGallery';
+import { PlayerMemberships } from '../../components/Player/Box/PlayerMemberships';
+import { PlayerSkills } from '../../components/Player/Box/PlayerSkills';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 const PlayerPage: React.FC<Props> = ({ player }) => {
+  // TODO Fake data should be saved in back-end
+  const BOX_TYPE = {
+    PLAYER_SKILLS: 'Skills',
+    PLAYER_CONTACT_BUTTONS: 'Contact',
+    PLAYER_GALLERY: 'Gallery',
+    PLAYER_MEMBERSHIPS: 'Memberships',
+    PLAYER_ACHIEVEMENTS: 'Achievements',
+  };
+  const [boxAvailableList, setBoxAvailableList] = React.useState([]);
+  const [fakeData, setFakeData] = React.useState([
+    [
+      BOX_TYPE.PLAYER_SKILLS,
+      BOX_TYPE.PLAYER_CONTACT_BUTTONS,
+      BOX_TYPE.PLAYER_GALLERY,
+    ],
+    [BOX_TYPE.PLAYER_MEMBERSHIPS, BOX_TYPE.PLAYER_ACHIEVEMENTS],
+    [],
+  ]);
+
   if (!player) {
     return <Error statusCode={404} />;
   }
 
+  const addBox = (column: number, name: string) => {
+    setBoxAvailableList(boxAvailableList.filter((box) => box !== name));
+    const updatedFakeData = [...fakeData];
+    updatedFakeData[column].push(name);
+    setFakeData(updatedFakeData);
+  };
+
+  const removeBox = (column: number, name: string) => {
+    setBoxAvailableList([...boxAvailableList, name]);
+    const updatedFakeData = [...fakeData];
+    updatedFakeData[column] = updatedFakeData[column].filter(
+      (box) => box !== name,
+    );
+    setFakeData(updatedFakeData);
+  };
+
+  const getBox = (column: number, name: string): React.ReactNode => {
+    switch (name) {
+      case BOX_TYPE.PLAYER_SKILLS:
+        return (
+          <PlayerSkills
+            player={player}
+            setRemoveBox={() => removeBox(column, name)}
+          />
+        );
+      case BOX_TYPE.PLAYER_CONTACT_BUTTONS:
+        return (
+          <PlayerContactButtons
+            player={player}
+            setRemoveBox={() => removeBox(column, name)}
+          />
+        );
+      case BOX_TYPE.PLAYER_GALLERY:
+        return (
+          <PlayerGallery
+            player={player}
+            setRemoveBox={() => removeBox(column, name)}
+          />
+        );
+      case BOX_TYPE.PLAYER_MEMBERSHIPS:
+        return (
+          <PlayerMemberships
+            player={player}
+            setRemoveBox={() => removeBox(column, name)}
+          />
+        );
+      default:
+      case BOX_TYPE.PLAYER_ACHIEVEMENTS:
+        return (
+          <PlayerAchievements
+            player={player}
+            setRemoveBox={() => removeBox(column, name)}
+          />
+        );
+    }
+  };
+
   return (
-    <>
-      <PlayerHero player={player} />
-      <PlayerFeatures player={player} />
+    // TODO Should be a custom background and maybe on app level(with the header)
+    <Container
+      maxW="100%"
+      bgImage={`url(${BackgroundImage})`}
+      backgroundSize="cover"
+      mt={4}
+    >
       <Container maxW="xl">
-        <SimpleGrid columns={[1, 1, 2, 3]} spacing="8" pt="12">
-          <MetaBox title="About me">
-            <P>{getPlayerDescription(player)}</P>
-            {player.EnneagramType ? (
-              <HStack py={6} spacing={4}>
-                <Image
-                  w="100%"
-                  maxW="4rem"
-                  src={PersonalityTypes[player.EnneagramType.name].image}
-                  alt={player.EnneagramType.name}
-                  style={{ mixBlendMode: 'color-dodge' }}
-                />
-                <FlexContainer align="stretch">
-                  <Text color="white" fontWeight="bold">
-                    {player.EnneagramType.name}
-                  </Text>
-                  <Text color="blueLight">
-                    {player.EnneagramType.description}
-                  </Text>
-                </FlexContainer>
-              </HStack>
-            ) : null}
-          </MetaBox>
-          {player.Player_Skills.length ? (
-            <MetaBox title="Skills">
-              <Wrap>
-                {player.Player_Skills.map(({ Skill }) => (
-                  <MetaTag
-                    key={Skill.id}
-                    size="md"
-                    fontWeight="normal"
-                    backgroundColor={SkillColors[Skill.category]}
-                  >
-                    {Skill.name}
-                  </MetaTag>
-                ))}
-              </Wrap>
-            </MetaBox>
-          ) : null}
-          {player.daohausMemberships.length ? (
-            <MetaBox title="Memberships">
-              <Wrap>
-                {player.daohausMemberships.map((member) => (
-                  <MetaTag key={member.id} size="md" fontWeight="normal">
-                    {member.moloch.title}
-                  </MetaTag>
-                ))}
-              </Wrap>
-            </MetaBox>
-          ) : null}
-        </SimpleGrid>
+        <Flex
+          align="center"
+          direction={['column', null, null, 'row']}
+          alignItems="flex-start"
+        >
+          <Box width={['100%', null, null, '33%']} mr={[0, null, null, 4]}>
+            <Box mb="6">
+              <PlayerHero player={player} />
+            </Box>
+            {(fakeData || [[], [], []])[0].map((name) => (
+              <Box mb="6">{getBox(0, name)}</Box>
+            ))}
+            <Box mb="6">
+              <PlayerAddBox
+                boxList={boxAvailableList}
+                setNewBox={(name) => addBox(0, name)}
+              />
+            </Box>
+          </Box>
+          <Box
+            width={['100%', null, null, '66%']}
+            ml={[0, null, null, 4]}
+            mt="200px"
+          >
+            <Box width="100%">
+              <Box mb="6">
+                <PlayerCollab player={player} />
+              </Box>
+              <Flex
+                align="center"
+                direction={['column', null, null, 'row']}
+                alignItems="flex-start"
+              >
+                <Box
+                  width={['100%', null, null, '50%']}
+                  mr={[0, null, null, 4]}
+                >
+                  {(fakeData || [[], [], []])[1].map((name) => (
+                    <Box mb="6">{getBox(1, name)}</Box>
+                  ))}
+                  <Box mb="6">
+                    <PlayerAddBox
+                      boxList={boxAvailableList}
+                      setNewBox={(name) => addBox(1, name)}
+                    />
+                  </Box>
+                </Box>
+                <Box
+                  width={['100%', null, null, '50%']}
+                  ml={[0, null, null, 4]}
+                >
+                  {(fakeData || [[], [], []])[2].map((name) => (
+                    <Box mb="6">{getBox(2, name)}</Box>
+                  ))}
+                  <Box mb="6">
+                    <PlayerAddBox
+                      boxList={boxAvailableList}
+                      setNewBox={(name) => addBox(2, name)}
+                    />
+                  </Box>
+                </Box>
+              </Flex>
+            </Box>
+          </Box>
+        </Flex>
       </Container>
-    </>
+    </Container>
   );
 };
 
