e77582aa9cd55af782dc573ed745994f905f4c07
Hammad Jutt <jutt@ualberta.ca>
2020-12-14 15:13:56 -0700

    Remove deprecated app-react package

--- packages/app-react/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-  graphqlURL:
-    process.env.REACT_APP_GRAPHQL_URL || 'http://localhost:8080/v1/graphql',
-  infuraId:
-    process.env.REACT_APP_INFURA_ID || '781d8466252d47508e177b8637b1c2fd',
-};
