d7358e428c985d216a808083dffb2c1446ef8c72
dan13ram <dan13ram@gmail.com>
2020-12-15 18:05:30 +0530

    updated frontend to display nft gallery from 3box/opensea

--- packages/web/components/Player/Box/PlayerGallery.tsx
+++ packages/web/components/Player/Box/PlayerGallery.tsx
@@ -1,63 +1,149 @@
-import { Box, Flex, HStack, Text } from '@metafam/ds';
+import {
+  Box,
+  Flex,
+  HStack,
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  SimpleGrid,
+  Text,
+  useDisclosure,
+} from '@metafam/ds';
+import { MetaLink as Link } from 'components/Link';
+import { PlayerFragmentFragment } from 'graphql/autogen/types';
+import {
+  Collectible,
+  useOpenSeaCollectibles,
+} from 'lib/useOpenSeaCollectibles';
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
 
-import nft1Image from '../../../assets/fake/nft1.png';
-import nft2Image from '../../../assets/fake/nft2.png';
 import { PlayerBox } from './PlayerBoxe';
 
-// TODO Fake data
-type Props = { setRemoveBox: () => void };
-export const PlayerGallery: React.FC<Props> = ({ setRemoveBox }) => {
-  const [show, setShow] = React.useState(false);
-  const fakeEthPrice = 500;
-  const fakeData = [
-    { title: 'CryptoMon Cards - Aave', priceInEth: 0.025, img: nft1Image },
-    { title: 'metagamer', priceInEth: 6.942, img: nft2Image },
-  ];
+type Props = { player: PlayerFragmentFragment; setRemoveBox: () => void };
+
+const GalleryItem: React.FC<{ nft: Collectible; noMargin?: boolean }> = ({
+  nft,
+  noMargin = false,
+}) => (
+  <Link
+    href={nft.permaLink}
+    isExternal
+    mb={noMargin ? undefined : 6}
+    minW={0}
+    display="flex"
+  >
+    <HStack spacing={6}>
+      <Flex width="126px" height="126px">
+        <Box
+          bgImage={`url(${nft.imageUrl})`}
+          backgroundSize="contain"
+          backgroundRepeat="no-repeat"
+          backgroundPosition="center"
+          width="124px"
+          height="124px"
+          m="auto"
+        />
+      </Flex>
+      <Flex direction="column">
+        <Text
+          fontSize="xs"
+          fontFamily="heading"
+          mt={3}
+          mb={3}
+          casing="uppercase"
+          display="inline-block"
+          style={{ wordWrap: 'break-word', wordBreak: 'break-all' }}
+        >
+          {nft.title}
+        </Text>
+        <Text fontSize="sm">
+          {nft.priceInUsd && nft.priceInEth
+            ? `${nft.priceInEth}Ξ ($${nft.priceInUsd.toFixed(2)})`
+            : ``}
+        </Text>
+      </Flex>
+    </HStack>
+  </Link>
+);
+
+export const PlayerGallery: React.FC<Props> = ({ player, setRemoveBox }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { favorites, data, loading } = useOpenSeaCollectibles({ player });
   return (
     <PlayerBox title="Gallery" setRemoveBox={setRemoveBox}>
-      {(fakeData || []).slice(0, show ? 999 : 3).map((nft) => (
-        <HStack alignItems="end" mb={6}>
-          <Flex width="126px" height="126px" mr={6}>
-            <Box
-              bgImage={`url(${nft.img})`}
-              backgroundSize="contain"
-              backgroundRepeat="no-repeat"
-              backgroundPosition="center"
-              width="124px"
-              height="124px"
-              m="auto"
-            />
-          </Flex>
-          <Box>
-            <Text
-              fontSize="xs"
-              fontFamily="heading"
-              mt={3}
-              mb={3}
-              casing="uppercase"
-            >
-              {nft.title}
-            </Text>
-            <Text fontSize="sm" mb="1">
-              {nft.priceInEth}Ξ ($
-              {parseFloat(`${nft.priceInEth * fakeEthPrice}`).toFixed(2)})
-            </Text>
-          </Box>
-        </HStack>
-      ))}
-      {(fakeData || []).length > 3 && (
+      {!loading &&
+        favorites &&
+        favorites.map((nft) => <GalleryItem nft={nft} key={nft.tokenId} />)}
+      {!loading && data && data.length > 3 && (
         <Text
           as="span"
           fontFamily="body"
           fontSize="xs"
           color="cyanText"
           cursor="pointer"
-          onClick={() => setShow(!show)}
+          onClick={onOpen}
         >
-          View {show ? 'less' : 'all'}
+          View all
         </Text>
       )}
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        scrollBehavior="inside"
+      >
+        <ModalOverlay css={{ backdropFilter: 'blur(8px)' }}>
+          <ModalContent maxW="75rem" bg="none">
+            <Box
+              bg="rgba(70, 20, 100, 0.8)"
+              borderTopRadius="lg"
+              p={4}
+              w="100%"
+            >
+              <HStack>
+                <Text
+                  fontFamily="mono"
+                  fontSize="sm"
+                  fontWeight="bold"
+                  color="blueLight"
+                  as="div"
+                  mr="auto"
+                >
+                  Gallery
+                </Text>
+                <FaTimes
+                  color="#A5B9F6"
+                  opacity="0.4"
+                  cursor="pointer"
+                  onClick={onClose}
+                />
+              </HStack>
+            </Box>
+            <Box
+              overflowY="scroll"
+              overflowX="hidden"
+              maxH="80vh"
+              borderBottomRadius="lg"
+              w="100%"
+            >
+              <SimpleGrid
+                columns={{ base: 1, md: 2, lg: 3 }}
+                gap={6}
+                padding={6}
+                boxShadow="md"
+                bg="whiteAlpha.200"
+                css={{ backdropFilter: 'blur(8px)' }}
+              >
+                {data &&
+                  data.map((nft) => (
+                    <GalleryItem nft={nft} key={nft.tokenId} noMargin />
+                  ))}
+              </SimpleGrid>
+            </Box>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
     </PlayerBox>
   );
 };
---
+++ packages/web/lib/useOpenSeaCollectibles.ts
@@ -0,0 +1,115 @@
+import { CONFIG } from 'config';
+import {
+  CollectiblesFavorites,
+  PlayerFragmentFragment,
+} from 'graphql/autogen/types';
+import { useEffect, useState } from 'react';
+
+type OpenSeaCollectiblesOpts = {
+  player: PlayerFragmentFragment;
+};
+
+export type Collectible = {
+  address: string;
+  tokenId: string;
+  title: string;
+  imageUrl: string;
+  permaLink: string;
+  priceInEth: number | undefined;
+  priceInUsd: number | undefined;
+};
+
+export const useOpenSeaCollectibles = ({
+  player,
+}: OpenSeaCollectiblesOpts): {
+  favorites: Array<Collectible> | null;
+  data: Array<Collectible> | null;
+  loading: boolean;
+} => {
+  const [favorites, setFavorites] = useState<Array<Collectible>>([]);
+  const [data, setData] = useState<Array<Collectible>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const owner = player.ethereum_address;
+  const favoritesQuery = player.box_profile?.collectiblesFavorites
+    ? `${
+        CONFIG.openseaURL
+      }/assets/?order_direction=desc&owner=${owner}${getFavoriteQueryParams(
+        player.box_profile.collectiblesFavorites,
+      )}`
+    : '';
+
+  useEffect(() => {
+    async function load() {
+      setLoading(true);
+      if (favoritesQuery && owner) {
+        const [favoritesData, allData] = await Promise.all([
+          fetchOpenSeaData(favoritesQuery),
+          fetchAllOpenSeaData(owner),
+        ]);
+        setFavorites(favoritesData);
+        setData(allData);
+      } else if (owner) {
+        const allData = await fetchAllOpenSeaData(owner);
+        setData(allData);
+        setFavorites(allData.slice(0, 3));
+      }
+      setLoading(false);
+    }
+    load();
+  }, [favoritesQuery, owner]);
+
+  return { favorites, data, loading };
+};
+
+const getFavoriteQueryParams = (
+  favorites: Array<Pick<CollectiblesFavorites, 'tokenId' | 'address'>>,
+): string =>
+  favorites.reduce((query, { tokenId }) => `${query}&token_ids=${tokenId}`, '');
+
+const fetchAllOpenSeaData = async (
+  owner: string,
+): Promise<Array<Collectible>> => {
+  let offset = 0;
+  let data: Array<Collectible> = [];
+  let lastData: Array<Collectible> = [];
+  do {
+    const query = `${CONFIG.openseaURL}/assets/?order_direction=desc&owner=${owner}&limit=50&offset=${offset}`; // opensea utmost returns 50 per api call
+    // eslint-disable-next-line no-await-in-loop
+    lastData = await fetchOpenSeaData(query);
+    data = data.concat(lastData);
+    offset += 50;
+  } while (lastData.length > 0);
+  return data;
+};
+
+const fetchOpenSeaData = async (query: string): Promise<Array<Collectible>> => {
+  const response = await fetch(query);
+  return parseResponse(response);
+};
+
+const parseResponse = async (
+  response: Response,
+): Promise<Array<Collectible>> => {
+  if (!response.ok) return [];
+  const data = await response.json();
+  if (!data.assets) return [];
+  return (
+    data.assets
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .map((asset: any) => ({
+        address: asset.asset_contract.address,
+        tokenId: asset.token_id,
+        title: asset.name,
+        imageUrl: asset.image_url,
+        permaLink: asset.permalink,
+        priceInEth: asset.last_sale
+          ? Number(asset.last_sale.payment_token.eth_price)
+          : undefined,
+        priceInUsd: asset.last_sale
+          ? Number(asset.last_sale.payment_token.usd_price)
+          : undefined,
+      }))
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .filter((collectible: any) => collectible.title && collectible.imageUrl)
+  );
+};
