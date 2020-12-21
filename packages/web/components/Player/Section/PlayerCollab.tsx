e0b19699b0a2954c677247d27148f246411e31e2
Alec LaLonde <alec@convergencelabs.com>
2020-12-21 15:25:13 -0700

    Re-added spacetime types

--- packages/web/components/Player/Section/PlayerCollab.tsx
+++ packages/web/components/Player/Section/PlayerCollab.tsx
@@ -1,7 +1,7 @@
 import { Box, Divider, HStack, Text } from '@metafam/ds';
 import { PlayerFragmentFragment } from 'graphql/autogen/types';
 import React from 'react';
-import { FaClock, FaGlobe } from 'react-icons/fa';
+import { FaClock } from 'react-icons/fa';
 
 import { PlayerTimeZone } from '../PlayerTimeZone';
 
@@ -15,17 +15,6 @@ export const PlayerCollab: React.FC<Props> = ({ player }) => {
       }
     >
       <PlayerTimeZone player={player} />
-      {/* <Box>
-        <Text fontSize="xs" color="blueLight" casing="uppercase" mb={3}>
-          Location
-        </Text>
-        <HStack alignItems="baseline">
-          <FaGlobe color="blueLight" />
-          <Text fontSize="lg" fontFamily="mono" mb="1">
-            {player.box_profile?.location || '-'}
-          </Text>
-        </HStack>
-      </Box> */}
       <Box>
         <Text fontSize="xs" color="blueLight" casing="uppercase" mb={3}>
           Availability
