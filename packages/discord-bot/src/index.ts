12ba9dc315053c3701f6670fe7a3f1c1f1fb9ae5
Hammad Jutt <jutt@ualberta.ca>
2021-01-29 22:53:28 -0700

    Discord Bot MVP

---
+++ packages/discord-bot/src/index.ts
@@ -0,0 +1,19 @@
+// Use the Client that are provided by @typeit/discord NOT discord.js
+import { Client } from '@typeit/discord';
+
+import { CONFIG } from './config';
+
+async function start() {
+  const client = new Client({
+    classes: [
+      `${__dirname}/*Discord.ts`, // glob string to load the classes
+      `${__dirname}/*Discord.js`, // If you compile using "tsc" the file extension change to .js
+    ],
+    silent: false,
+    variablesChar: ':',
+  });
+
+  await client.login(CONFIG.discordBotToken);
+}
+
+start();
