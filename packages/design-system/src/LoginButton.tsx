2b7301e54d85dae0c0272afbabac4d89ce31c50d
ace-smart <minmax17842@gmail.com>
2021-02-04 12:51:35 +0400

    Improvied abstracted LoginButton. #295

--- packages/design-system/src/LoginButton.tsx
+++ packages/design-system/src/LoginButton.tsx
@@ -21,17 +21,21 @@ type UserInfo = {
 type LoginButtonProps = {
   connectWeb3: () => Promise<void>;
   disconnect: () => void;
+  hreffor: (user: any) => string;
   isConnected: boolean;
   user: UserInfo;
   fetching: boolean;
+  enableSetup?: boolean;
 };
 
 export const LoginButton: React.FC<LoginButtonProps> = ({
   connectWeb3,
   disconnect,
+  hreffor,
   isConnected,
   user,
   fetching,
+  enableSetup = false,
 }: LoginButtonProps) => {
   const handleLoginClick = useCallback(async () => {
     await connectWeb3();
@@ -48,21 +52,21 @@ export const LoginButton: React.FC<LoginButtonProps> = ({
           src={user.playerImage}
           name={user.playerName}
           as={Link}
-          href={`/player/${user.username}`}
+          href={hreffor(user)}
         />
         <Box>
-          <Link
-            href={`/player/${user.username}`}
-            fontFamily="body"
-            color="whiteAlpha.700"
-          >
+          <Link href={hreffor(user)} fontFamily="body" color="whiteAlpha.700">
             {user.player ? user.playerName : 'Unknown'}
           </Link>
           <HStack spacing={2}>
-            <Link color="cyan.400" href="/profile/setup">
-              Setup profile
-            </Link>
-            <Text color="cyan.400">|</Text>
+            {enableSetup && (
+              <>
+                <Link color="cyan.400" href="/profile/setup">
+                  Setup profile
+                </Link>
+                <Text color="cyan.400">|</Text>
+              </>
+            )}
             <Button
               onClick={disconnect}
               fontFamily="body"
