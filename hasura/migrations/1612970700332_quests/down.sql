57a2a7caf9ba5ef496ef8cffa519461a886841e5
Pacien Boisson <pakokrew@gmail.com>
2021-02-12 20:04:40 +0400

    Fixing down migration

--- hasura/migrations/1612970700332_quests/down.sql
+++ hasura/migrations/1612970700332_quests/down.sql
@@ -1,26 +1,23 @@
-
+alter table "public"."quest_completion" drop constraint "quest_completion_quest_id_fkey";
+alter table "public"."quest_completion" drop constraint "quest_completion_completed_by_player_id_fkey";
 alter table "public"."quest_completion" drop constraint "quest_completion_status_fkey";
-
-ALTER TABLE "public"."quest_completion" DROP COLUMN "status";
-
-DROP TABLE "public"."QuestCompletionStatus";
 
 DROP TABLE "public"."quest_completion";
 
+alter table "public"."quest_skill" drop constraint "quest_skill_quest_id_fkey";
+alter table "public"."quest_skill" drop constraint "quest_skill_skill_id_fkey";
+
 DROP TABLE "public"."quest_skill";
 
+alter table "public"."quest" drop constraint "quest_created_by_player_id_fkey";
+alter table "public"."quest" drop constraint "quest_guild_id_fkey";
+alter table "public"."quest" drop constraint "quest_status_fkey";
 alter table "public"."quest" drop constraint "quest_repetition_fkey";
 
-ALTER TABLE "public"."quest" DROP COLUMN "repetition";
+DROP TABLE "public"."quest";
 
 DROP TABLE "public"."QuestRepetition";
 
-alter table "public"."quest" drop constraint "quest_status_fkey";
-
-ALTER TABLE "public"."quest" ALTER COLUMN "status" DROP NOT NULL;
-
-ALTER TABLE "public"."quest" DROP COLUMN "status";
-
 DROP TABLE "public"."QuestStatus";
 
-DROP TABLE "public"."quest";
+DROP TABLE "public"."QuestCompletionStatus";
