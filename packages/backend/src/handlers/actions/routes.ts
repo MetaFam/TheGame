72f7c56acd6356c6492d2b37bacbf7550f545cb0
Hammad Jutt <jutt@ualberta.ca>
2021-02-18 21:49:19 -0700

    Fix linting errors

--- packages/backend/src/handlers/actions/routes.ts
+++ packages/backend/src/handlers/actions/routes.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 import { asyncHandlerWrapper } from '../../lib/apiHelpers';
 import { migrateSourceCredAccounts } from './migrateSourceCredAccounts/handler';
-import { updateBoxProfileHandler } from './updateBoxProfile/handler';
 import { questsRoutes } from './quests/routes';
+import { updateBoxProfileHandler } from './updateBoxProfile/handler';
 
 export const actionRoutes = express.Router();
 
