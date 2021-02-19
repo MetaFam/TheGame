f18b657ef73b0e86f4dc4214b8964bdac689d22a
Hammad Jutt <jutt@ualberta.ca>
2021-02-19 00:17:55 -0700

    Implement activation of identities and persisting ledger

--- packages/discord-bot/src/AppDiscord.ts
+++ packages/discord-bot/src/AppDiscord.ts
@@ -1,12 +1,13 @@
 import { Command, CommandMessage, Discord } from '@typeit/discord';
-import { sourcecred as sc } from 'sourcecred-publish-test';
+import { sourcecred as sc } from 'sourcecred';
 
 import { CONFIG } from './config';
 
-export const storage = new sc.ledger.storage.GithubStorage(
-  CONFIG.githubApiToken,
-  'MetaFam/XP',
-);
+export const storage = new sc.ledger.storage.GithubStorage({
+  apiToken: CONFIG.githubApiToken,
+  repo: 'MetaFam/XP',
+  branch: 'master',
+});
 
 const manager = new sc.ledger.manager.LedgerManager({
   storage,
@@ -19,9 +20,10 @@ const addressUtils = sc.plugins.ethereum.utils.address;
 @Discord('!')
 export abstract class AppDiscord {
   @Command('setAddress :ethAddress')
-  // @ts-expect-error should be allowed
+  // @ts-expect-error should be allowedyarn
   private async setAddress(message: CommandMessage) {
     const res = await ledgerLoadedPromise;
+
     if (res.error) {
       await message.reply(`Error Loading Ledger: ${res.error}`);
       return;
@@ -59,7 +61,23 @@ export abstract class AppDiscord {
 
     try {
       manager.ledger.addAlias(baseIdentityId, ethAlias);
-      await message.reply('Successfully linked ETH Address');
+      manager.ledger.activate(baseIdentityId);
+      const persistRes = await manager.persist();
+
+      if (persistRes.error) {
+        await message.reply(
+          `Error Updating Ledger: ${
+            persistRes.error
+          }.\n\n ${persistRes.localChanges
+            .map((c: any) => JSON.stringify(c.action))
+            .join('\n')}`,
+        );
+        return;
+      }
+
+      await message.reply(
+        'Successfully linked ETH Address and activated account',
+      );
     } catch (e) {
       await message.reply(`Unable to link address: ${e.message}`);
     }
