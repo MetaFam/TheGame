4d0d9501d0782b596271448afa2ea2f9929c2a4b
Alec LaLonde <alec@convergencelabs.com>
2021-02-25 15:43:04 -0700

    Changed update player username graphql mutation

--- packages/web/graphql/mutations/updateUsername.ts
+++ packages/web/graphql/mutations/updateUsername.ts
@@ -1,12 +1,10 @@
 import gql from 'fake-tag';
 
 export const UpdateUsernameMutation = gql`
-  mutation UpdatePlayerUsername($username: String!) {
-    update_player(_set: { username: $username }, where: {}) {
-      returning {
-        id
-        username
-      }
+  mutation UpdatePlayerUsername($playerId: uuid!, $username: String!) {
+    update_player_by_pk(pk_columns: { id: $playerId }, _set: { username: $username }) {
+      id
+      username
     }
   }
 `;
