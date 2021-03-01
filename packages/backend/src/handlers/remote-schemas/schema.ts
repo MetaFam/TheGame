b0ab6f1e2d072f9a4746ca2124e8cee1a67edca0
dan13ram <dan13ram@users.noreply.github.com>
2021-03-01 12:06:52 +0530

    updated backend for brightId (#362)
    * updated backend for brightId
    
    * updated schema
    
    * removed unwanted console logs
    
    * made brightId context constant

--- packages/backend/src/handlers/remote-schemas/schema.ts
+++ packages/backend/src/handlers/remote-schemas/schema.ts
@@ -1,14 +1,18 @@
 import { makeExecutableSchema } from 'graphql-tools';
 
+import { getBrightIdStatus } from './resolvers/brightId/resolver';
 import { getDaoHausMemberships } from './resolvers/daohaus/resolver';
 import { getBoxProfile } from './resolvers/getBoxProfile/resolver';
 import { typeDefs } from './typeDefs';
+import { uuid } from './types/uuid';
 
 const resolvers = {
   Query: {
     getBoxProfile,
     getDaoHausMemberships,
+    getBrightIdStatus,
   },
+  uuid,
 };
 
 export const schema = makeExecutableSchema({ typeDefs, resolvers });
---
+++ packages/backend/src/handlers/remote-schemas/types/uuid.ts
@@ -0,0 +1,38 @@
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
+
+const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const nilUUID = '00000000-0000-0000-0000-000000000000';
+
+const isUUID = (value: string) => {
+  return uuidRegex.test(value) || nilUUID === value;
+};
+
+export const uuid = new GraphQLScalarType({
+  name: 'uuid',
+  description:
+    'The `uuid` scalar type represents UUID values as specified by [RFC 4122](https://tools.ietf.org/html/rfc4122).',
+  serialize: (value) => {
+    if (!isUUID(value)) {
+      throw new TypeError(`UUID cannot represent non-UUID value: ${value}`);
+    }
+
+    return value.toLowerCase();
+  },
+  parseValue: (value) => {
+    if (!isUUID(value)) {
+      throw new TypeError(`UUID cannot represent non-UUID value: ${value}`);
+    }
+
+    return value.toLowerCase();
+  },
+  parseLiteral: (ast) => {
+    if (ast.kind === Kind.STRING) {
+      if (isUUID(ast.value)) {
+        return ast.value;
+      }
+    }
+
+    return undefined;
+  },
+});
