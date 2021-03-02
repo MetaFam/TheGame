bff8f4abe5596ff17c91893ba6e2740cc55ff3ed
dan13ram <dan13ram@gmail.com>
2021-03-02 17:26:23 +0530

    incorporated review comments

--- packages/web/lib/hooks/brightId.ts
+++ packages/web/lib/hooks/brightId.ts
@@ -1,12 +1,28 @@
 import { CONFIG } from 'config';
 import { PlayerFragmentFragment } from 'graphql/autogen/types';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 
 const BRIGHTID_CONTEXT = 'MetaGame';
 const DEEPLINK_ENDPOINT = `brightid://link-verification/${CONFIG.brightIdNodeUrl}/${BRIGHTID_CONTEXT}`;
 const UNIVERSAL_LINK_ENDPOINT = `${CONFIG.brightIdAppUrl}/link-verification/${CONFIG.brightIdNodeUrl}/${CONFIG.brightIdNodeUrl}/${BRIGHTID_CONTEXT}`;
 const VERIFICATION_ENDPOINT = `${CONFIG.brightIdAppUrl}/node/v5/verifications/${BRIGHTID_CONTEXT}`;
 const POLL_INTERVAL = 5000;
+
+type BrightIdVerificationStatus =
+  | {
+      unique: boolean;
+      app?: string;
+      context?: string;
+      contextIds: Array<string>;
+    }
+  | null
+  | undefined;
+
+const isStatusVerified = (
+  status: BrightIdVerificationStatus,
+  contextId: string,
+): boolean =>
+  status?.unique === true || status?.contextIds.includes(contextId) === true;
 
 export const useBrightIdStatus = ({
   player,
@@ -16,27 +32,19 @@ export const useBrightIdStatus = ({
   verified: boolean;
   deeplink: string;
   universalLink: string;
-} => {
-  const verified =
-    player.brightid_status?.unique === true ||
-    player.brightid_status?.contextIds.includes(player.id) === true;
-  const contextId = player.id;
-  const deeplink = `${DEEPLINK_ENDPOINT}/${contextId}`;
-  const universalLink = `${UNIVERSAL_LINK_ENDPOINT}/${contextId}`;
+} =>
+  useMemo(() => {
+    const contextId = player.id;
+    const verified = isStatusVerified(player.brightid_status, contextId);
+    const deeplink = `${DEEPLINK_ENDPOINT}/${contextId}`;
+    const universalLink = `${UNIVERSAL_LINK_ENDPOINT}/${contextId}`;
 
-  return { verified, deeplink, universalLink };
-};
-
-type BrightIdVerificationStatus = {
-  unique: boolean;
-  app: string;
-  context: string;
-  contextIds: Array<string>;
-};
+    return { verified, deeplink, universalLink };
+  }, [player]);
 
 const fetchVerificationData = async (
   contextId: string,
-): Promise<BrightIdVerificationStatus | null> => {
+): Promise<BrightIdVerificationStatus> => {
   try {
     const response = await fetch(`${VERIFICATION_ENDPOINT}/${contextId}`);
     if (!response.ok) return null;
@@ -63,9 +71,7 @@ export const useBrightIdUpdated = ({
 
     const update = () => {
       fetchVerificationData(contextId).then((status) => {
-        const isVerified =
-          status?.unique === true ||
-          status?.contextIds.includes(contextId) === true;
+        const isVerified = isStatusVerified(status, contextId);
         if (isSubscribed && isVerified) {
           window.location.reload();
         }
