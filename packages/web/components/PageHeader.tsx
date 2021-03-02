32af86e51cacb52008eda0c30165a0f0f3e31b99
dan13ram <dan13ram@users.noreply.github.com>
2021-03-02 20:47:00 +0530

    fixed seed price & login button not showing on mobile (#379)

--- packages/web/components/PageHeader.tsx
+++ packages/web/components/PageHeader.tsx
@@ -135,15 +135,15 @@ export const PageHeader: React.FC = () => {
       justify="space-between"
       wrap="wrap"
       color="offwhite"
-      px={8}
+      px={{ base: 4, lg: 8 }}
       position="relative"
-      display={{ base: 'none', lg: 'flex' }}
     >
       <Box>
         <Ticker />
       </Box>
 
       <Stack
+        display={{ base: 'none', lg: 'flex' }}
         width={drawer.width}
         height="100%"
         direction="row"
@@ -252,6 +252,7 @@ export const PageHeader: React.FC = () => {
         }}
       >
         <Stack
+          display={{ base: 'none', lg: 'flex' }}
           width={content.width}
           direction="row"
           flexWrap="wrap"
