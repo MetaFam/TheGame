76a7922bb53a9b5ae0c8bb1c4598945dde3f81eb
Alec LaLonde <alec@convergencelabs.com>
2021-03-04 16:37:59 -0700

    Revert "Added player username regex check constraint"
    This reverts commit c21562203bc2daa110fba901b545e382cad3c8d4.

--- hasura/migrations/1614710766234_alter_table_public_player_add_check_constraint_username_is_valid/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."player" drop constraint "username_is_valid";
--- hasura/migrations/1614710766234_alter_table_public_player_add_check_constraint_username_is_valid/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."player" add constraint "username_is_valid" check (username ~* '^[a-z0-9-](_(?!(\.|_))|\.(?!(_|\.))|[a-z0-9-]){0,40}[a-z0-9-]$');
