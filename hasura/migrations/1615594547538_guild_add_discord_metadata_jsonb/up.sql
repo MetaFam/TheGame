17755dc4d1c4936206a171cf87b0e6c5483a4d27
Alec LaLonde <alec@convergencelabs.com>
2021-03-12 17:24:18 -0700

    Added discord_metadata column to guild, moved inviteUrl there

---
+++ hasura/migrations/1615594547538_guild_add_discord_metadata_jsonb/up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "public"."guild" ADD COLUMN "discord_metadata" jsonb NULL;
+
+UPDATE "public"."guild" SET "discord_metadata" = ('{"inviteUrl": "' || discord_invite_url || '"}')::jsonb;
+
+ALTER TABLE "public"."guild" DROP COLUMN "discord_invite_url" CASCADE;
