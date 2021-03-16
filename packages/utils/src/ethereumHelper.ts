4ff514217dfe91ace1c2a9ed7e2bf6e795001948
Pacien Boisson <pakokrew@users.noreply.github.com>
2021-03-16 17:42:07 +0400

    Support connection from smart wallets (#416)
    * Support connection from smart wallets
    
    * Fix unreachable code

--- packages/utils/src/ethereumHelper.ts
+++ packages/utils/src/ethereumHelper.ts
@@ -1,9 +1,9 @@
-import { providers } from 'ethers';
+import { Contract, providers, utils } from 'ethers';
 
 export async function signerHelper(
   provider: providers.Web3Provider,
   rawMessage: string,
-) {
+): Promise<string> {
   const ethereum = provider.provider;
   const signer = provider.getSigner();
   const address = await signer.getAddress();
@@ -18,4 +18,49 @@ export async function signerHelper(
     params,
   });
   return signature;
+}
+
+const smartWalletABI = [
+  'function isValidSignature(bytes32 _message, bytes _signature) public view returns (bool)',
+];
+
+enum WalletType {
+  EOA,
+  SMART,
+}
+
+async function getWalletType(
+  address: string,
+  provider: providers.BaseProvider,
+): Promise<WalletType> {
+  const code = await provider.getCode(address);
+  return code === '0x' ? WalletType.EOA : WalletType.SMART;
+}
+
+export async function verifySignature(
+  address: string,
+  message: string,
+  signature: string,
+  provider: providers.BaseProvider,
+): Promise<boolean> {
+  const walletType = await getWalletType(address, provider);
+
+  if (walletType === WalletType.EOA) {
+    const recoveredAddress = utils.verifyMessage(message, signature);
+    return address === recoveredAddress;
+  }
+
+  // Smart wallet
+  const arrayishMessage = utils.toUtf8Bytes(message);
+  const hexMessage = utils.hexlify(arrayishMessage);
+  const hexArray = utils.arrayify(hexMessage);
+  const hashMessage = utils.hashMessage(hexArray);
+
+  const contract = new Contract(address, smartWalletABI, provider);
+  try {
+    const returnValue = await contract.isValidSignature(hashMessage, signature);
+    return returnValue;
+  } catch(error) {
+    throw new Error('unsupported smart wallet');
+  }
 }
