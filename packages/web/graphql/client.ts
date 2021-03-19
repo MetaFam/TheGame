e235440d404363d636cfa009c856a623603b8d0c
Will Holcomb <dys@dhappy.org>
2021-03-19 04:21:28 -0400

    redeployin' altered render.yaml 🥦

--- packages/web/graphql/client.ts
+++ packages/web/graphql/client.ts
@@ -2,8 +2,6 @@ import { createClient } from 'urql';
 
 import { CONFIG } from '../config';
 
-console.info(`Client URI: ${CONFIG.graphqlURL}`)
-
 export const client = createClient({
   url: CONFIG.graphqlURL,
   suspense: false,
