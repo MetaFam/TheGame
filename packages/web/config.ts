bd32046be33c94403054ba33f1335ede089d9b04
Will Holcomb <dys@dhappy.org>
2021-03-19 03:04:02 -0400

    printin' URI 🖨

--- packages/web/config.ts
+++ packages/web/config.ts
@@ -1,12 +1,12 @@
 export const CONFIG = {
   graphqlURL: ((() => {
     if (process.env.NEXT_PUBLIC_GRAPHQL_URL) {
-      return process.env.NEXT_PUBLIC_GRAPHQL_URL
+      return process.env.NEXT_PUBLIC_GRAPHQL_URL;
     }
     if (process.env.GRAPHQL_HOST) {
-      return `https://${process.env.GRAPHQL_HOST}.onrender.com/v1/graphql`
+      return `https://${process.env.GRAPHQL_HOST}.onrender.com/v1/graphql`;
     }
-    return 'http://localhost:8080/v1/graphql'
+    return 'http://localhost:8080/v1/graphql';
   })()),
   infuraId:
     process.env.NEXT_PUBLIC_INFURA_ID || '781d8466252d47508e177b8637b1c2fd',
