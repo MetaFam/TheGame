d34f9aba831b6395bb3c6839f446927a3abaf159
Will Holcomb <dys@dhappy.org>
2021-03-19 02:18:22 -0400

    no interpolation in render.yaml 🌮

--- packages/web/config.ts
+++ packages/web/config.ts
@@ -1,6 +1,13 @@
 export const CONFIG = {
-  graphqlURL:
-    process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:8080/v1/graphql',
+  graphqlURL: ((() => {
+    if (process.env.NEXT_PUBLIC_GRAPHQL_URL) {
+      return process.env.NEXT_PUBLIC_GRAPHQL_URL
+    }
+    if (process.env.GRAPHQL_HOST) {
+      return `https://${process.env.GRAPHQL_HOST}.onrender.com/v1/graphql`
+    }
+    return 'http://localhost:8080/v1/graphql'
+  })()),
   infuraId:
     process.env.NEXT_PUBLIC_INFURA_ID || '781d8466252d47508e177b8637b1c2fd',
   openseaApiKey: process.env.NEXT_OPENSEA_API_KEY || undefined,
