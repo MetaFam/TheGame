fe7ec85516fc722503dc362a90cae219658cdfdb
Will Holcomb <dys@dhappy.org>
2021-03-19 03:04:02 -0400

    printin' URI 🖨

--- packages/web/graphql/client.ts
+++ packages/web/graphql/client.ts
@@ -2,6 +2,8 @@ import { createClient } from 'urql';
 
 import { CONFIG } from '../config';
 
+console.info(`Client URI: ${CONFIG.graphqlURL}`)
+
 export const client = createClient({
   url: CONFIG.graphqlURL,
   suspense: false,
