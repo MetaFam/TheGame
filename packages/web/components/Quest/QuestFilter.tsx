2617417e6cab11d4742dd8fa2abaed77befeb84a
Pacien Boisson <pakokrew@gmail.com>
2021-03-24 19:03:15 +0400

    Can create quest from seed balance

--- packages/web/components/Quest/QuestFilter.tsx
+++ packages/web/components/Quest/QuestFilter.tsx
@@ -6,10 +6,12 @@ import {
   Switch,
   Text,
 } from '@metafam/ds';
-import React from 'react';
-import { Order_By, GetQuestsQueryVariables, QuestStatus_Enum } from 'graphql/autogen/types';
+import React, { useMemo } from 'react';
+import { useGetpSeedBalanceQuery, Order_By, GetQuestsQueryVariables, QuestStatus_Enum } from 'graphql/autogen/types';
 import { useUser } from '../../lib/hooks';
 import { QuestAggregates } from '../../lib/hooks/quests';
+import { numbers } from '@metafam/utils';
+const { BN, amountToDecimal } = numbers;
 
 type Props = {
   aggregates: QuestAggregates;
@@ -17,9 +19,36 @@ type Props = {
   setQueryVariable: (_: string, __: any) => void;
 };
 
-export const QuestFilter: React.FC<Props> = ({ aggregates, queryVariables, setQueryVariable }) => {
 
+export function isAllowedToCreateQuest(
+  balance?: string,
+): boolean {
+  if(!balance) return false;
+
+  const pSEEDDecimals = 18;
+  const minimumPooledSeedBalance = new BN(100);
+  const pSEEDBalanceInDecimal = amountToDecimal(
+    balance,
+    pSEEDDecimals,
+  );
+
+  const allowed = new BN(pSEEDBalanceInDecimal).gt(minimumPooledSeedBalance);
+
+  return allowed;
+}
+
+export const QuestFilter: React.FC<Props> = ({ aggregates, queryVariables, setQueryVariable }) => {
   const { user } = useUser();
+  const [res] = useGetpSeedBalanceQuery({
+    variables: {
+      address: user?.ethereum_address || '',
+    },
+    pause: !user?.ethereum_address,
+  });
+  const canCreateQuest = useMemo(
+    () => isAllowedToCreateQuest(res.data?.getTokenBalances?.pSeedBalance),
+    [res],
+  );
   const myId = user?.id;
 
   return (
@@ -31,6 +60,7 @@ export const QuestFilter: React.FC<Props> = ({ aggregates, queryVariables, setQu
         as="a"
         href="/quest/create"
         fontFamily="mono"
+        disabled={!canCreateQuest}
       >
         New Quest
       </MetaButton>
@@ -58,7 +88,7 @@ export const QuestFilter: React.FC<Props> = ({ aggregates, queryVariables, setQu
           <option value={QuestStatus_Enum.Closed}>Closed</option>
         </Select>
         <Select
-          value={queryVariables.guild_id as string}
+          value={queryVariables.guild_id as string || ''}
           onChange={(e) => setQueryVariable('guild_id', e.target.value)}
         >
           <option value="">All guilds</option>
