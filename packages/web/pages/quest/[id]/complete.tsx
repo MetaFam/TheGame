b6d3a9142783a85dbc4d9a15feecdde330003450
Pacien Boisson <pakokrew@gmail.com>
2021-04-02 17:11:38 +0400

    Reword complete quest

--- packages/web/pages/quest/[id]/complete.tsx
+++ packages/web/pages/quest/[id]/complete.tsx
@@ -55,7 +55,7 @@ const SubmitQuestCompletionPage: React.FC<Props> = ({ quest }) => {
         router.push(`/quest/${quest.id}`);
         toast({
           title: 'Submitted quest completion',
-          description: `You now have to wait that it gets accepted ;)`,
+          description: `Now, wait until it gets accepted 😉`,
           status: 'success',
           isClosable: true,
           duration: 4000,
