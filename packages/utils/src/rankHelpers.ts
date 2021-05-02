ceab4c13ea9c37c91874307b397b119945b96424
Alec LaLonde <alec@convergencelabs.com>
2021-05-01 23:31:25 -0600

    Linter fixes

--- packages/utils/src/rankHelpers.ts
+++ packages/utils/src/rankHelpers.ts
@@ -1,6 +1,6 @@
 // A summation of usersPerRank
 // This is the first index for which users will NOT be ranked
-export const computeRankCap = (usersPerRank: Array<number>) =>
+export const computeRankCap = (usersPerRank: Array<number>): number =>
   usersPerRank.reduce((sum, rankCount) => sum + rankCount, 0);
 
 // Computes the rank for the given index. This would be the index corresponding
