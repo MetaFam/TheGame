8b782c267e2a84318ad628d7c70bd2ac76b4b602
Alec LaLonde <alec@convergencelabs.com>
2021-05-06 12:23:37 -0600

    Removed prefix as it was colliding with existing bot

--- packages/discord-bot/src/discord/AppDiscord.ts
+++ packages/discord-bot/src/discord/AppDiscord.ts
@@ -1,7 +1,7 @@
-import { CommandMessage, CommandNotFound, Discord } from '@typeit/discord';
+import { Discord } from '@typeit/discord';
 import * as Path from 'path';
 
-@Discord('!', {
+@Discord('', {
   import: [
     // We are using tsc, so we want to load the compiled files
     Path.join(__dirname,  "commands", "*.js"),
@@ -9,8 +9,8 @@ import * as Path from 'path';
 })
 export abstract class AppDiscord {
   // This is triggered when a particular command doesn't exist
-  @CommandNotFound()
-  notFound(command: CommandMessage) {
-    command.reply("MetaGameBot doesn't recognize that command.");
-  }
+  // @CommandNotFound()
+  // notFound(command: CommandMessage) {
+  //   command.reply("MetaGameBot doesn't recognize that command.");
+  // }
 }
