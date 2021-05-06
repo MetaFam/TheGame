045a609bef4618b51c842c09f513bb2aa3b20804
Alec LaLonde <alec@convergencelabs.com>
2021-05-06 11:44:45 -0600

    Changed to load XP using accounts.json as beore

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -6,6 +6,11 @@ export const DISCORD_BOT_PERMISSIONS = '268435456';
 export const DISCORD_OAUTH_SCOPES = 'bot identify guilds';
 export const DISCORD_OAUTH_CALLBACK_PATH = `${WEB_PATH_JOIN_GUILD}/auth`;
 
+<<<<<<< 3d788980ae639628f9d8cd2504975206048a73f0
 export const SC_OUTPUT_BASE =
   'https://raw.githubusercontent.com/MetaFam/XP/gh-pages/';
   export const SC_ACCOUNTS_FILE = `${SC_OUTPUT_BASE}output/accounts.json`;
+=======
+export const SC_OUTPUT_BASE = 'https://raw.githubusercontent.com/MetaFam/XP/gh-pages/';
+export const SC_ACCOUNTS_FILE = `${SC_OUTPUT_BASE}output/accounts.json`;
+>>>>>>> Changed to load XP using accounts.json as beore
