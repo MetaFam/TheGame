64d2576b20f33c3baa30e1b059b22280be006a1f
dan13ram <dan13ram@gmail.com>
2021-05-11 17:35:28 +0530

    increased player listing to 150

--- packages/web/graphql/getPlayers.ts
+++ packages/web/graphql/getPlayers.ts
@@ -1,21 +1,31 @@
 import gql from 'fake-tag';
 
-import { GetPlayersQuery, GetPlayersQueryVariables } from './autogen/types';
+import {
+  GetPlayersQuery,
+  GetPlayersQueryVariables,
+  PlayerFragmentFragment,
+} from './autogen/types';
 import { client } from './client';
 import { PlayerFragment } from './fragments';
 
 const playersQuery = gql`
-  query GetPlayers($limit: Int) {
-    player(order_by: { total_xp: desc }, limit: $limit) {
+  query GetPlayers($limit: Int, $offset: Int) {
+    player(order_by: { total_xp: desc }, limit: $limit, offset: $offset) {
       ...PlayerFragment
     }
   }
   ${PlayerFragment}
 `;
 
-export const getPlayers = async (limit = 50) => {
+export const getPlayers = async (
+  limit = 50,
+  offset = 0,
+): Promise<PlayerFragmentFragment[]> => {
   const { data, error } = await client
-    .query<GetPlayersQuery, GetPlayersQueryVariables>(playersQuery, { limit })
+    .query<GetPlayersQuery, GetPlayersQueryVariables>(playersQuery, {
+      limit,
+      offset,
+    })
     .toPromise();
 
   if (!data) {
--- packages/web/pages/players.tsx
+++ packages/web/pages/players.tsx
@@ -1,13 +1,27 @@
 import { PageContainer } from 'components/Container';
 import { PlayerList } from 'components/PlayerList';
+import { PlayerFragmentFragment } from 'graphql/autogen/types';
 import { getPlayers } from 'graphql/getPlayers';
 import { InferGetStaticPropsType } from 'next';
 import React from 'react';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
+const LIMIT = 50;
+const OFFSET = 50;
+const TOTAL_PLAYERS = 150;
+
 export const getStaticProps = async () => {
-  const players = await getPlayers();
+  const promises: Promise<PlayerFragmentFragment[]>[] = new Array(
+    TOTAL_PLAYERS / OFFSET,
+  )
+    .fill(false)
+    .map((_, i) => getPlayers(LIMIT, i * OFFSET));
+  const playersArr = await Promise.all(promises);
+  const players = playersArr.reduce(
+    (_total, _players) => [..._total, ..._players],
+    [],
+  );
   return {
     props: {
       players,
