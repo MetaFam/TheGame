efe0e8bde3d070eaf373187e78513f4c41f3f659
dan13ram <dan13ram@users.noreply.github.com>
2021-05-12 08:03:54 +0530

    [MyMeta] increased player listing to 150 (#541)
    * increased player listing to 150
    
    * removed unnecessary extra var

--- packages/web/pages/players.tsx
+++ packages/web/pages/players.tsx
@@ -1,13 +1,26 @@
 import { PageContainer } from 'components/Container';
 import { PlayerList } from 'components/PlayerList';
+import { PlayerFragmentFragment } from 'graphql/autogen/types';
 import { getPlayers } from 'graphql/getPlayers';
 import { InferGetStaticPropsType } from 'next';
 import React from 'react';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
+const LIMIT = 50;
+const TOTAL_PLAYERS = 150;
+
 export const getStaticProps = async () => {
-  const players = await getPlayers();
+  const promises: Promise<PlayerFragmentFragment[]>[] = new Array(
+    TOTAL_PLAYERS / LIMIT,
+  )
+    .fill(false)
+    .map((_, i) => getPlayers(LIMIT, i * LIMIT));
+  const playersArr = await Promise.all(promises);
+  const players = playersArr.reduce(
+    (_total, _players) => [..._total, ..._players],
+    [],
+  );
   return {
     props: {
       players,
