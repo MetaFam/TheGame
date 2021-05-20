65d1bee758805982f96183085ee68b17306d4834
Hammad Jutt <jutt@ualberta.ca>
2021-05-20 00:23:37 -0600

    Update migrateSourceCredAccounts scripts to use Eth address as source of truth

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -10,5 +10,3 @@ export const DISCORD_OAUTH_CALLBACK_PATH = `${WEB_PATH_JOIN_GUILD}/auth`;
 export const SC_OUTPUT_BASE =
   'https://raw.githubusercontent.com/MetaFam/XP/gh-pages/';
 export const SC_ACCOUNTS_FILE = `${SC_OUTPUT_BASE}output/accounts.json`;
-export const SC_ADDRESS_BOOK_FILE =
-  'https://raw.githubusercontent.com/MetaFam/TheSource/master/addressbook.json';
