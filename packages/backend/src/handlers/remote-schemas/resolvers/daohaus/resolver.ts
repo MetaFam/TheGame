74fdfb251550089aefb269f15c6ea4cd92710aea
Matthew Cantelon <gh@matthewcantelon.ca>
2021-05-21 17:11:25 -0700

    feat: combine DAO memberships across chains for a player
    This only supports DaoHaus DAOs but works with xDai and Polygon. Backend
    only so far, needs frontend cleanup

--- packages/backend/src/handlers/remote-schemas/resolvers/daohaus/resolver.ts
+++ packages/backend/src/handlers/remote-schemas/resolvers/daohaus/resolver.ts
@@ -1,12 +1,24 @@
-import { daoHausClient } from '../../../../lib/daoHausClient';
-import { QueryResolvers } from '../../autogen/types';
+import * as daoHausClients from '../../../../lib/daoHausClient';
+import { Member, QueryResolvers } from '../../autogen/types';
 
 export const getDaoHausMemberships: QueryResolvers['getDaoHausMemberships'] = async (
   _,
   { memberAddress },
 ) => {
   if (!memberAddress) return [];
-  const res = await daoHausClient.GetDaoHausMemberships({ memberAddress });
 
-  return res.members;
+  const res = await Promise.all([
+    daoHausClients.mainnet.GetDaoHausMemberships({ memberAddress }),
+    daoHausClients.polygon.GetDaoHausMemberships({ memberAddress }),
+    daoHausClients.xdai.GetDaoHausMemberships({ memberAddress }),
+  ]);
+
+  // TODO should map network name onto this somehow
+
+  const members: Member[] = res.reduce(
+    (allMembers, network) => [...allMembers, ...network.members],
+    <Member[]>[],
+  );
+
+  return members;
 };
--- packages/backend/src/lib/daoHausClient.ts
+++ packages/backend/src/lib/daoHausClient.ts
@@ -3,6 +3,10 @@ import { GraphQLClient } from 'graphql-request';
 import { CONFIG } from '../config';
 import { getSdk } from './autogen/daohaus-sdk';
 
-export const daoHausClient = getSdk(
-  new GraphQLClient(CONFIG.daoHausGraphqlURL),
+export const mainnet = getSdk(new GraphQLClient(CONFIG.daoHausGraphqlURL));
+
+export const polygon = getSdk(
+  new GraphQLClient(CONFIG.daoHausPolygonGraphqlURL),
 );
+
+export const xdai = getSdk(new GraphQLClient(CONFIG.daoHausXdaiGraphqlURL));
