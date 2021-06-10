3dda8aa7ca9eac6676fe4accf275c6edfef10253
Matthew Cantelon <gh@matthewcantelon.ca>
2021-06-10 07:02:04 -0700

    fix: create clients once
    Suggestion from @dan13ram on  PR to cache the clients and avoid
    recreating on every request.

--- packages/backend/src/lib/daoHausClient.ts
+++ packages/backend/src/lib/daoHausClient.ts
@@ -3,13 +3,13 @@ import { GraphQLClient } from 'graphql-request';
 import { CONFIG } from '../config';
 import { getSdk, Sdk } from './autogen/daohaus-sdk';
 
-export function clientFactory(chain: string): Sdk {
-  switch (chain) {
-    case 'polygon':
-      return getSdk(new GraphQLClient(CONFIG.daoHausPolygonGraphqlURL));
-    case 'xdai':
-      return getSdk(new GraphQLClient(CONFIG.daoHausXdaiGraphqlURL));
-    default:
-      return getSdk(new GraphQLClient(CONFIG.daoHausGraphqlURL));
-  }
+const defaultGqlClient = new GraphQLClient(CONFIG.daoHausGraphqlURL);
+const clients = new Map([
+  ['polygon', new GraphQLClient(CONFIG.daoHausPolygonGraphqlURL)],
+  ['xdai', new GraphQLClient(CONFIG.daoHausXdaiGraphqlURL)],
+  ['ethereum', defaultGqlClient],
+]);
+
+export function getClient(chain: string): Sdk {
+  return getSdk(clients.get(chain) || defaultGqlClient);
 }
