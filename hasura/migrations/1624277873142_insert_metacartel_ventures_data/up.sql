dc6d4190d3ed473889fe45f2e0f12eaab09ccd69
mquellhorst <michiel@ooble.nl>
2021-06-21 14:19:08 +0200

    insert metacartel ventures data

---
+++ hasura/migrations/1624277873142_insert_metacartel_ventures_data/up.sql
@@ -0,0 +1,23 @@
+INSERT INTO "public"."guild" (
+  type, name, logo, description, 
+  join_button_url, moloch_address, 
+  website_url, guildname, discord_id,
+  status, discord_metadata,
+  discord_invite_url, twitter_url,
+  github_url
+) VALUES
+(
+  'FUNDING', 'MetaCartel Ventures', 
+ 'https://images.squarespace-cdn.com/content/5c8ceb5e2727be114994c1f3/1582727237261-7PIWNXCFPCSVWO0D3DDF/TEH+OG.png?content-type=image%2Fpng', 
+ 'MetaCartel Ventures (Venture DAO) is a for-profit DAO created by the MetaCartel community for the purposes of making investments into early-stage Decentralized Applications (DApps)',
+ 'https://metacartel.xyz/',
+ '0x4570b4faf71e23942b8b9f934b47ccedf7540162',
+ 'https://metacartel.xyz/',
+ 'metacartelventures',
+ NULL,
+ 'ACTIVE',
+ NULL,
+ NULL,
+ 'https://twitter.com/VENTURE_DAO',
+ NULL
+ );
