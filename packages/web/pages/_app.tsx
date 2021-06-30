936921cd7484c2832d79375387ee58132d88a061
Mano <manolingam1996@gmail.com>
2021-06-30 18:55:13 +0530

    minor change

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -20,7 +20,7 @@ const App: React.FC<WithUrqlProps> = ({
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>MetaGame</title>
-      {CONFIG.gaId != null && (
+      {!!CONFIG.gaId && (
         <>
           <script
             async
