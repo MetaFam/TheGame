498eaa442c4f169b00f6273e8956b8514929c7e7
Mano <manolingam1996@gmail.com>
2021-06-30 19:30:55 +0530

    added clarity snippet

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -39,6 +39,19 @@ const App: React.FC<WithUrqlProps> = ({
           />
         </>
       )}
+      {!!CONFIG.clarityId && (
+        <script type="text/javascript"
+         dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", '${CONFIG.clarityId}');
+          `
+         }}>
+        </script>
+      )}
     </Head>
     <Web3ContextProvider resetUrqlClient={resetUrqlClient}>
       <>
