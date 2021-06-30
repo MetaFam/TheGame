5955f50669161945f78e202b4d2bec1382d9a7d2
Mano <manolingam1996@gmail.com>
2021-06-30 13:38:23 +0530

    added tracking snippet for clarity

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -39,6 +39,19 @@ const App: React.FC<WithUrqlProps> = ({
           />
         </>
       )}
+      {CONFIG.clarityID != null && (
+        <script type="text/javascript"
+         dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", '${CONFIG.clarityID}');
+          `
+         }}>
+        </script>
+      )}
     </Head>
     <Web3ContextProvider resetUrqlClient={resetUrqlClient}>
       <>
