9a8f8ea065f200f4167a3ca33659a1248002070c
Alec LaLonde <alec@boxelderweb.com>
2021-07-07 10:41:11 -0600

    Add period to metacartel ventures description
    Co-authored-by: Will Holcomb <dys@dhappy.org>

--- hasura/migrations/1624277873142_insert_metacartel_ventures_data/up.sql
+++ hasura/migrations/1624277873142_insert_metacartel_ventures_data/up.sql
@@ -9,7 +9,7 @@ INSERT INTO "public"."guild" (
 (
   'FUNDING', 'MetaCartel Ventures', 
  'https://images.squarespace-cdn.com/content/5c8ceb5e2727be114994c1f3/1582727237261-7PIWNXCFPCSVWO0D3DDF/TEH+OG.png?content-type=image%2Fpng', 
- 'MetaCartel Ventures (Venture DAO) is a for-profit DAO created by the MetaCartel community for the purposes of making investments into early-stage Decentralized Applications (DApps)',
+ 'MetaCartel Ventures (Venture DAO) is a for-profit DAO created by the MetaCartel community for the purposes of making investments into early-stage Decentralized Applications (DApps).',
  'https://metacartel.xyz/',
  '0x4570b4faf71e23942b8b9f934b47ccedf7540162',
  'https://metacartel.xyz/',
