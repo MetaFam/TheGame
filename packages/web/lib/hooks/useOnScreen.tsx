0a101f01d5976bf836f3aa368cc719b02f850c93
dan13ram <dan13ram@gmail.com>
2021-07-08 17:09:09 +0530

    fixed glitches due to IntersectionObserver

---
+++ packages/web/lib/hooks/useOnScreen.tsx
@@ -0,0 +1,19 @@
+import { RefObject, useEffect, useState } from 'react';
+
+export const useOnScreen = (ref: RefObject<HTMLDivElement>): boolean => {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) =>
+      setIntersecting(entry.isIntersecting),
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+};
