8a7a4d54fe1dc2f96dd413c0e1cf567aa982dbb4
dan13ram <dan13ram@gmail.com>
2021-07-13 21:43:57 +0530

    better styling for player timezone & profile page

--- packages/web/components/Player/Section/PlayerCollab.tsx
+++ packages/web/components/Player/Section/PlayerCollab.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, HStack, Text } from '@metafam/ds';
+import { Box, Divider, HStack, SimpleGrid, Text } from '@metafam/ds';
 import { PlayerFragmentFragment } from 'graphql/autogen/types';
 import React from 'react';
 import { FaClock } from 'react-icons/fa';
@@ -7,23 +7,26 @@ import { PlayerTimeZone } from '../PlayerTimeZone';
 
 type Props = { player: PlayerFragmentFragment };
 export const PlayerCollab: React.FC<Props> = ({ player }) => (
-  <HStack
-    spacing={6}
+  <SimpleGrid
+    columns={2}
+    gap={0}
     divider={
       <Divider height="3rem" color="whiteAlpha.400" orientation="vertical" />
     }
   >
     <PlayerTimeZone player={player} />
-    <Box>
+    <Box borderLeft="1px solid" borderLeftColor="rgba(255,255,255,0.6)" pl={4}>
       <Text fontSize="xs" color="blueLight" casing="uppercase" mb={3}>
         Availability
       </Text>
       <HStack alignItems="baseline">
-        <FaClock color="blueLight" />
-        <Text fontSize="lg" fontFamily="mono" mb="1">
+        <Box width="1rem">
+          <FaClock color="blueLight" width="1rem" />
+        </Box>
+        <Text fontSize={{ base: 'md', sm: 'lg' }} fontFamily="mono" mb="1">
           {`${player.availability_hours || '0'} h/week`}
         </Text>
       </HStack>
     </Box>
-  </HStack>
+  </SimpleGrid>
 );
