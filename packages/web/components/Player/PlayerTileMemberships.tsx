85fd06f35cbad7bc3ab4b2478f347f8e8c733d98
dan13ram <dan13ram@gmail.com>
2021-07-13 13:01:00 +0530

    fixed daoHaus membership links

--- packages/web/components/Player/PlayerTileMemberships.tsx
+++ packages/web/components/Player/PlayerTileMemberships.tsx
@@ -1,6 +1,6 @@
-import { MetaTag, Wrap, WrapItem } from '@metafam/ds';
+import { MetaTag, Text, VStack, Wrap, WrapItem } from '@metafam/ds';
 import { PlayerFragmentFragment } from 'graphql/autogen/types';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 type Props = {
   player: PlayerFragmentFragment;
@@ -8,24 +8,37 @@ type Props = {
 
 const SHOW_MEMBERSHIPS = 4;
 
-export const PlayerTileMemberships: React.FC<Props> = ({ player }) => (
-  <Wrap>
-    {player.daohausMemberships
-      .filter((m) => m.moloch.title !== null)
-      .slice(0, SHOW_MEMBERSHIPS)
-      .map((member) => (
-        <WrapItem key={member.id}>
-          <MetaTag size="md" fontWeight="normal">
-            {member.moloch.title}
-          </MetaTag>
-        </WrapItem>
-      ))}
-    {player.daohausMemberships.length > SHOW_MEMBERSHIPS && (
-      <WrapItem>
-        <MetaTag size="md" fontWeight="normal">
-          {`+${player.daohausMemberships.length - SHOW_MEMBERSHIPS}`}
-        </MetaTag>
-      </WrapItem>
-    )}
-  </Wrap>
-);
+export const PlayerTileMemberships: React.FC<Props> = ({ player }) => {
+  const show = useMemo(
+    () =>
+      player.daohausMemberships.reduce(
+        (t, { moloch: { title } }) => t || !!title,
+        false,
+      ),
+    [player.daohausMemberships],
+  );
+  return show ? (
+    <VStack spacing={2} align="stretch">
+      <Text textStyle="caption">MEMBER OF</Text>
+      <Wrap>
+        {player.daohausMemberships
+          .filter((m) => m.moloch.title !== null)
+          .slice(0, SHOW_MEMBERSHIPS)
+          .map((member) => (
+            <WrapItem key={member.id}>
+              <MetaTag size="md" fontWeight="normal">
+                {member.moloch.title}
+              </MetaTag>
+            </WrapItem>
+          ))}
+        {player.daohausMemberships.length > SHOW_MEMBERSHIPS && (
+          <WrapItem>
+            <MetaTag size="md" fontWeight="normal">
+              {`+${player.daohausMemberships.length - SHOW_MEMBERSHIPS}`}
+            </MetaTag>
+          </WrapItem>
+        )}
+      </Wrap>
+    </VStack>
+  ) : null;
+};
