fcfb32cdc7f27bfd20f6bb78fb66ea815bfe53fd
Will Holcomb <dys@dhappy.org>
2021-07-18 08:19:37 -0400

    renaming for clarity & adding queued invalidation 🌠

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,4 +1,5 @@
 import bodyParser from 'body-parser';
+import Bottleneck from 'bottleneck';
 import express from 'express';
 
 import { CONFIG } from './config';
@@ -6,6 +7,12 @@ import { router } from './handlers/routes';
 import { errorMiddleware } from './lib/apiHelpers';
 
 const app = express();
+
+app.locals.limiter = new Bottleneck({
+  maxConcurrent: 10,
+});
+// tracks the current contents of Bottleneck
+app.locals.queuedRecacheFor = {};
 
 app.use(bodyParser.json());
 
