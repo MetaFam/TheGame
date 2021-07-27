1c878ad5049eb5c56a930dd453ca5cb931736118
dan13ram <dan13ram@gmail.com>
2021-07-27 13:58:46 +0530

    updated skeleton tiles

--- packages/web/components/Player/PlayerTileSkeleton.tsx
+++ packages/web/components/Player/PlayerTileSkeleton.tsx
@@ -1,17 +1,14 @@
 import {
   FlexProps,
-  HStack,
   MetaTile,
   MetaTileBody,
   MetaTileHeader,
   Skeleton,
   SkeletonCircle,
   SkeletonText,
-  Text,
   VStack,
 } from '@metafam/ds';
 import React from 'react';
-import { FaGlobe } from 'react-icons/fa';
 
 export const PlayerTileSkeleton: React.FC<FlexProps> = (props) => (
   <MetaTile flex={1} {...props}>
@@ -20,34 +17,20 @@ export const PlayerTileSkeleton: React.FC<FlexProps> = (props) => (
         <SkeletonCircle w="5.5rem" h="5.5rem" />
         <VStack spacing="1.5rem">
           <Skeleton h="1.25rem" w="8rem" />
-          <HStack spacing="0.5rem">
-            <Skeleton h="1.25rem" w="4.5rem" />
-            <Skeleton h="1.25rem" w="4.5rem" />
-            <Skeleton h="1.25rem" w="4.5rem" />
-          </HStack>
-          <HStack spacing="0.5rem">
-            <FaGlobe color="blueLight" fontSize="0.875rem" />
-            <Skeleton h="1.25rem" w="7rem" />
-          </HStack>
+          <Skeleton h="1.25rem" w="15rem" />
+          <Skeleton h="1.25rem" w="7rem" />
         </VStack>
       </VStack>
-      <VStack spacing={2} align="stretch" pt="1rem">
-        <Text textStyle="caption">ABOUT</Text>
+      <VStack spacing={2} align="stretch" pt="2rem">
         <SkeletonText noOfLines={4} spacing={4} w="100%" />
       </VStack>
     </MetaTileHeader>
     <MetaTileBody>
-      <VStack spacing={2} align="stretch" pt="1rem">
-        <Text textStyle="caption">SKILLS</Text>
+      <VStack spacing={2} align="stretch" pt="2rem">
         <SkeletonText noOfLines={2} spacing={4} />
       </VStack>
-      <VStack spacing={2} align="stretch" pt="1rem">
-        <Text textStyle="caption">MEMBER OF</Text>
+      <VStack spacing={2} align="stretch" pt="2rem">
         <SkeletonText noOfLines={2} spacing={4} />
-      </VStack>
-      <VStack spacing={2} align="stretch" pt="1rem">
-        <Text textStyle="caption">CONTACT</Text>
-        <SkeletonText noOfLines={1} spacing={4} />
       </VStack>
     </MetaTileBody>
   </MetaTile>
