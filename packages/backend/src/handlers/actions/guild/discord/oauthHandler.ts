1989a4f73476624a5eb6c88473529f7d12fe3ca9
Alec LaLonde <alec@convergencelabs.com>
2021-08-17 14:23:27 -0600

    use new guildposition enum

--- packages/backend/src/handlers/actions/guild/discord/oauthHandler.ts
+++ packages/backend/src/handlers/actions/guild/discord/oauthHandler.ts
@@ -12,6 +12,7 @@ import {
   DiscordGuildAuthResponse,
   Guild_Insert_Input,
   Guild_Metadata_Insert_Input,
+  GuildPosition_Enum,
   GuildStatus_Enum,
   GuildType_Enum,
 } from '../../../../lib/autogen/hasura-sdk';
@@ -135,7 +136,7 @@ const createNewGuild = async (
     guildname: discordGuild.name.toLowerCase().replace(/[^a-z0-9]/g, ''),
     discord_id: discordGuild.id,
     status: GuildStatus_Enum.Pending,
-    position: 'EXTERNAL',
+    position: GuildPosition_Enum.External,
   };
 
   if (discordMetadata.logoHash != null) {
