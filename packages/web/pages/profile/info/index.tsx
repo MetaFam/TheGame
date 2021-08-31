cfda09fdd9c2c4d37d1b3e7f914046f766a19188
Will Holcomb <dys@dhappy.org>
2021-08-31 12:17:04 -0400

    committing partial rollback of next.js upgragde for external validation 🦃

--- packages/web/pages/profile/info/index.tsx
+++ packages/web/pages/profile/info/index.tsx
@@ -1,53 +1,85 @@
-import { PageContainer } from 'components/Container';
-import { useForm } from 'react-hook-form';
 import { Caip10Link } from '@ceramicnetwork/stream-caip10-link';
-import React from 'react';
 import {
+  Button,
+  FormControl,
   FormErrorMessage,
   FormLabel,
-  FormControl,
+  Image,
   Input,
-  Button
-} from '@chakra-ui/react';
-import { useEffect } from 'react';
+  Stack,
+} from '@metafam/ds';
+import { PageContainer } from 'components/Container';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 import { useWeb3 } from '../../../lib/hooks';
 
 const InfoPage = () => {
+  const [did, setDid] = useState<string>();
+  const image = useRef<any>();
   const {
     handleSubmit,
     register,
     formState: { errors, isSubmitting },
-    setValue
+    setValue,
   } = useForm();
-  const { ceramic, idx, address } = useWeb3();
+  const { ceramic, idx, address, storageClient } = useWeb3();
+  console.log(did);
+
+  const handleFileUpload = useCallback(
+    (event: any) => {
+      console.log(event.target.files);
+      // const blobFile = new Blob(event.target.files);
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        console.log(reader.result);
+        if (image && image.current) {
+          image.current.onload = async () => {
+            console.log(image.current.width, image.current);
+            const cid = await storageClient?.put(event.target.files);
+            console.log({ cid });
+          };
+          image.current.src = reader.result;
+        }
+      });
+      reader.readAsDataURL(event.target.files[0]);
+    },
+    [storageClient],
+  );
 
   useEffect(() => {
     // fetch from IDX
-    (async() => {
+    (async () => {
       if (ceramic && address) {
-        const caip10 = await Caip10Link.fromAccount(ceramic, `${address}@eip155:1`)
-        if(caip10.did){
+        const caip10 = await Caip10Link.fromAccount(
+          ceramic,
+          `${address}@eip155:1`,
+        );
+        if (caip10.did) {
+          setDid(caip10.did);
           const result = await idx?.get('basicProfile', caip10.did);
-          Object.entries(result as Record<string, any>).forEach(
-            ([key, value]) => {
-              if (['image', 'background'].includes(key)){
-                value = (value as Record<string, any>).original.src;
+          Object.entries(result as Record<string, unknown>).forEach(
+            ([key, object]) => {
+              let value = object;
+              if (['image', 'background'].includes(key)) {
+                const {
+                  original: { src: url },
+                } = value as Record<string, Record<string, string>>;
+                value = url;
               }
-              if(key === "birthDate"){
-                value = new Date(value as string)
+              if (key === 'birthDate') {
+                value = new Date(value as string);
               }
-              setValue(key, value)
-            })
+              setValue(key, value);
+            },
+          );
         }
       }
-    })()
-
-    // loop over
-
-  }, [ceramic, address])
+    })();
+  }, [ceramic, address, idx, setValue]);
 
   function onSubmit(values: any) {
-    console.log(values)
+    console.log(values);
     return new Promise((resolve) => {
       setTimeout(() => {
         alert(JSON.stringify(values, null, 2));
@@ -58,16 +90,21 @@ const InfoPage = () => {
   return (
     <PageContainer>
       Hello Meta Profile
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <Image ref={image} visibility="hidden" />
+      <Stack as="form" onSubmit={handleSubmit(onSubmit)}>
         <FormControl isInvalid={errors.name}>
           <FormLabel htmlFor="name">First name</FormLabel>
           <Input
             name="name"
             placeholder="name"
             ref={register}
-            {...register("name", {
-              required: "This is required",
-              minLength: { value: 4, message: "Minimum length should be 4" }
+            {...register('name', {
+              required: 'This is required',
+              minLength: { value: 2, message: 'Minimum length should be 2' },
+              maxLength: {
+                value: 150,
+                message: 'Maximum length should be 150',
+              },
             })}
           />
           <FormErrorMessage>
@@ -80,22 +117,51 @@ const InfoPage = () => {
             name="image"
             placeholder="image"
             ref={register}
-            {...register("image", {
-              required: "This is required",
-              minLength: { value: 4, message: "Minimum length should be 4" }
+            {...register('image', {
+              required: 'This is required',
+              minLength: { value: 4, message: 'Minimum length should be 4' },
+              maxLength: {
+                value: 150,
+                message: 'Maximum length should be 150',
+              },
             })}
           />
           <FormErrorMessage>
             {errors.image && errors.image.message}
           </FormErrorMessage>
         </FormControl>
-        <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
+        <FormControl isInvalid={errors.background}>
+          <FormLabel htmlFor="background">First name</FormLabel>
+          <Input
+            name="background"
+            type="file"
+            placeholder="background"
+            ref={register}
+            onChange={handleFileUpload}
+            {...register('background', {
+              required: 'This is required',
+              minLength: { value: 4, message: 'Minimum length should be 4' },
+              maxLength: {
+                value: 150,
+                message: 'Maximum length should be 150',
+              },
+            })}
+          />
+          <FormErrorMessage>
+            {errors.background && errors.background.message}
+          </FormErrorMessage>
+        </FormControl>
+        <Button
+          mt={4}
+          colorScheme="teal"
+          isLoading={isSubmitting}
+          type="submit"
+        >
           Submit
         </Button>
-      </form>
+      </Stack>
     </PageContainer>
-  )
-}
-
+  );
+};
 
 export default InfoPage;
