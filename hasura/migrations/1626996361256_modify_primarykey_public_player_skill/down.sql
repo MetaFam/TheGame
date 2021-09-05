1c437a2e4b1a3a67952cfccf5c420443cfdfcf67
Istvan Richter <istvan.richter@miro.technology>
2021-09-05 14:32:07 +0200

    Add id and rank field to player_skill

---
+++ hasura/migrations/1626996361256_modify_primarykey_public_player_skill/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."player_skill" drop constraint "player_skill_pkey";
---
+++ hasura/migrations/1626996361256_modify_primarykey_public_player_skill/up.sql
@@ -0,0 +1,3 @@
+alter table "public"."player_skill"
+    add constraint "player_skill_pkey" 
+    primary key ( "id" );
