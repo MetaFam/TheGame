f54ce9702ac57571100073c296207d8dc80459fe
Will Holcomb <dys@dhappy.org>
2021-09-22 14:24:11 -0400

    updating Ceramic dependencies 👅

--- packages/backend/src/handlers/actions/storage/handler.ts
+++ packages/backend/src/handlers/actions/storage/handler.ts
@@ -4,10 +4,13 @@ import { getFilesFromPath, Web3Storage } from 'web3.storage';
 import { CONFIG } from '../../../config'
 
 // eslint-disable-next-line import/no-default-export
-export default async (req: Request | any, res: Response): Promise<Record<string, any>> => {
+export default async (req: Request | any, res: Response):
+Promise<Record<string, any>> => {
   const storage = new Web3Storage({ token: CONFIG.web3StorageToken })
 
-  const filePaths = Object.values(req.files as any[]).map(([{ path }]) => path)
+  const filePaths = Object.values(req.files as any[]).map(
+    ([{ path }]) => path
+  )
   const files = await getFilesFromPath(filePaths)
 
   const cid = await storage.put(files as any)
