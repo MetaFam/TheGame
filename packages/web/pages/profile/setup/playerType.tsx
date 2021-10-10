8b9bcdc2095db65ac42c3963c7b3e4ee4b7cd1b7
vidvidvid <weetopol@gmail.com>
2021-10-10 20:18:45 +0200

    fix header and description mess

--- packages/web/pages/profile/setup/playerType.tsx
+++ packages/web/pages/profile/setup/playerType.tsx
@@ -1,4 +1,3 @@
-import { MetaHeading, Text } from '@metafam/ds';
 import { SetupPlayerType } from 'components/Setup/SetupPlayerType';
 import { SetupProfile } from 'components/Setup/SetupProfile';
 import { SetupContextProvider } from 'contexts/SetupContext';
@@ -6,13 +5,6 @@ import React from 'react';
 
 const PlayerTypeSetup: React.FC = () => (
   <SetupContextProvider>
-    <MetaHeading mb={5} textAlign="center">
-      Player Type
-    </MetaHeading>
-    <Text mb={10}>
-      Please read the features of each player type below. And select the one
-      that suits you best.
-    </Text>
     <SetupProfile>
       <SetupPlayerType />
     </SetupProfile>
