90c9963487cb19ab71dd708b2c6fceea483d7c51
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-10-18 19:29:43 +0530

    fix: availabilty input bug fixed

--- packages/web/pages/profile/setup/availability.tsx
+++ packages/web/pages/profile/setup/availability.tsx
@@ -3,7 +3,7 @@ import { SetupProfile } from 'components/Setup/SetupProfile';
 import { SetupContextProvider } from 'contexts/SetupContext';
 import { useUser } from 'lib/hooks';
 import { InferGetStaticPropsType } from 'next';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const getStaticProps = async () => ({
   props: {
@@ -17,12 +17,15 @@ const AvailabilitySetup: React.FC<DefaultSetupProps> = () => {
   const [availability, setAvailability] = useState<string>('');
   const { user } = useUser({ redirectTo: '/' });
 
-  if (user?.player) {
-    const { player } = user;
-    if (player.availability_hours && !availability) {
-      setAvailability(player.availability_hours.toString());
+  useEffect(() => {
+    if (user?.player) {
+      const { player } = user;
+      if (player.availability_hours && !availability) {
+        setAvailability(player.availability_hours.toString());
+      }
     }
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <SetupContextProvider>
