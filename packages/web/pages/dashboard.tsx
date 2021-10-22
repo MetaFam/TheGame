7b1f381ba61516fc194db87677665e7c8f3e5e25
vidvidvid <weetopol@gmail.com>
2021-10-22 18:04:13 +0200

    feat: set up dashboard page

---
+++ packages/web/pages/dashboard.tsx
@@ -0,0 +1,6 @@
+import { PageContainer } from 'components/Container';
+import React from 'react';
+
+const Dashboard: React.FC = () => <PageContainer>dashboard</PageContainer>;
+
+export default Dashboard;
