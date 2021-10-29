0044e6e4faf320d99e38caca611c5582756ac4d9
vidvidvid <weetopol@gmail.com>
2021-10-29 18:37:42 +0200

    refactor: split Leaderboard

--- packages/web/pages/dashboard.tsx
+++ packages/web/pages/dashboard.tsx
@@ -2,7 +2,6 @@ import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
 import {
-  Avatar,
   Box,
   ButtonGroup,
   DeleteIcon,
@@ -14,11 +13,11 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
-  VStack,
 } from '@metafam/ds';
 import classnames from 'classnames';
 import { PageContainer } from 'components/Container';
 import { Calendar } from 'components/Dashboard/Calendar';
+import { Leaderboard } from 'components/Dashboard/Leaderboard';
 import { containerQueries } from 'components/Dashboard/Section';
 import { Seed } from 'components/Dashboard/Seed';
 import { XP } from 'components/Dashboard/XP';
@@ -613,126 +612,7 @@ export const Grid: FC = () => {
                   }}
                 >
                   <MetaHeading>Leaderboard</MetaHeading>
-                  <VStack
-                    className="leaderboard"
-                    width="100%"
-                    mt={5}
-                    fontFamily="exo2"
-                    fontWeight="700"
-                  >
-                    <Box
-                      className="player player__chip"
-                      display="flex"
-                      width="100%"
-                      px={8}
-                      py={2}
-                      flexFlow="row nowrap"
-                      alignItems="center"
-                      justifyContent="flex-start"
-                      backgroundColor="blackAlpha.500"
-                      borderRadius="md"
-                    >
-                      <Box className="player__position" flex={0} mr={3}>
-                        1
-                      </Box>
-                      <Avatar
-                        className="player__avatar"
-                        bg="cyan.200"
-                        border="2px solid"
-                        borderColor="diamond"
-                        src="https://pbs.twimg.com/profile_images/1349660293778067456/x4HwomRZ_400x400.jpg"
-                        mr={3}
-                      />
-                      <Box className="player__name">pΞTH</Box>
-                      <Box className="player__score" textAlign="right" flex={1}>
-                        429
-                      </Box>
-                    </Box>
-                    <Box
-                      className="player player__chip"
-                      display="flex"
-                      width="100%"
-                      px={8}
-                      py={2}
-                      flexFlow="row nowrap"
-                      alignItems="center"
-                      justifyContent="flex-start"
-                      backgroundColor="blackAlpha.500"
-                      borderRadius="md"
-                    >
-                      <Box className="player__position" flex={0} mr={3}>
-                        2
-                      </Box>
-                      <Avatar
-                        className="player__avatar"
-                        bg="cyan.200"
-                        border="2px solid"
-                        borderColor="diamond"
-                        src="https://pbs.twimg.com/profile_images/964343435682512896/JEnt0Aed_400x400.jpg"
-                        mr={3}
-                      />
-                      <Box className="player__name">Misanth</Box>
-                      <Box className="player__score" textAlign="right" flex={1}>
-                        390
-                      </Box>
-                    </Box>
-                    <Box
-                      className="player player__chip"
-                      display="flex"
-                      width="100%"
-                      px={8}
-                      py={2}
-                      flexFlow="row nowrap"
-                      alignItems="center"
-                      justifyContent="flex-start"
-                      backgroundColor="blackAlpha.500"
-                      borderRadius="md"
-                    >
-                      <Box className="player__position" flex={0} mr={3}>
-                        3
-                      </Box>
-                      <Avatar
-                        className="player__avatar"
-                        bg="cyan.200"
-                        border="2px solid"
-                        borderColor="diamond"
-                        src="https://metafam.imgix.net/https%3A%2F%2Fipfs.infura.io%2Fipfs%2FQmRKgJ6nA8CaxDRFYQ2o7SLVF4N8zGbezEKnC993foTZX5?ixlib=js-2.3.2&amp;ar=1%3A1&amp;height=200&amp;s=3f23e361b4ac8a70e211a706b95398a9"
-                        mr={3}
-                      />
-                      <Box className="player__name">luxumbra</Box>
-                      <Box className="player__score" textAlign="right" flex={1}>
-                        321
-                      </Box>
-                    </Box>
-                    <Box
-                      className="player player__chip"
-                      display="flex"
-                      width="100%"
-                      px={8}
-                      py={2}
-                      flexFlow="row nowrap"
-                      alignItems="center"
-                      justifyContent="flex-start"
-                      backgroundColor="blackAlpha.500"
-                      borderRadius="md"
-                    >
-                      <Box className="player__position" flex={0} mr={3}>
-                        4
-                      </Box>
-                      <Avatar
-                        className="player__avatar"
-                        bg="cyan.200"
-                        border="2px solid"
-                        borderColor="diamond"
-                        src="https://metafam.imgix.net/https%3A%2F%2Fipfs.infura.io%2Fipfs%2FQmXqFv1JQrUvyVEZXSsriJrxgx9uhBX99RessphRCX5D8j?ixlib=js-2.3.2&amp;ar=1%3A1&amp;height=200&amp;s=ed9e9235a18d845328ddc473bf7bfe5d"
-                        mr={3}
-                      />
-                      <Box className="player__name">Alec</Box>
-                      <Box className="player__score" textAlign="right" flex={1}>
-                        305
-                      </Box>
-                    </Box>
-                  </VStack>
+                  <Leaderboard />
                 </Box>
               )}
             </ContainerQuery>
