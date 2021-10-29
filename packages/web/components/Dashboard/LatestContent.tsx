36e61c4e6190a4816c05065d73d66384e44c5f5a
vidvidvid <35112939+vidvidvid@users.noreply.github.com>
2021-10-29 22:16:31 +0200

    Dashboard structure (#885)
    * refactor: split Calendar
    
    * refactor: split Seed
    
    * refactor: split XP
    
    * refactor: split Leaderboard
    
    * refactor: LatestContent
    
    * fix: rename component
    
    * feat: create GridItem component
    
    * feat: dashboard config with static values
    
    * refactor: implement compact structure with GridItem and utils/dashboard

---
+++ packages/web/components/Dashboard/LatestContent.tsx
@@ -0,0 +1,39 @@
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@metafam/ds';
+import React from 'react';
+
+export const LatestContent: React.FC = () => (
+  <Tabs mt={5} size="lg" variant="line" colorScheme="gray.600" isFitted>
+    <TabList borderBottomWidth={0}>
+      <Tab
+        color="gray.600"
+        _selected={{ color: 'white', borderColor: 'white' }}
+      >
+        Read
+      </Tab>
+      <Tab
+        color="gray.600"
+        _selected={{ color: 'white', borderColor: 'white' }}
+      >
+        Listen
+      </Tab>
+      <Tab
+        color="gray.600"
+        _selected={{ color: 'white', borderColor: 'white' }}
+      >
+        Watch
+      </Tab>
+    </TabList>
+
+    <TabPanels>
+      <TabPanel className="chakra-tabs__tab-panel--read">
+        <p>A feed of news and events</p>
+      </TabPanel>
+      <TabPanel className="chakra-tabs__tab-panel--listen">
+        <p>A feed podcast episodes from Anchor or wherever.</p>
+      </TabPanel>
+      <TabPanel className="chakra-tabs__tab-panel--watch">
+        <p>Feed of MetaMedia YouTube content</p>
+      </TabPanel>
+    </TabPanels>
+  </Tabs>
+);
