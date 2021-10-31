f4fda8054acc1d55b91e8b5424f56f72cb5472c9
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-10-31 20:46:53 +0530

    fix: setting intial state using useState

--- packages/web/pages/profile/setup/availability.tsx
+++ packages/web/pages/profile/setup/availability.tsx
@@ -3,7 +3,7 @@ import { SetupProfile } from 'components/Setup/SetupProfile';
 import { SetupContextProvider } from 'contexts/SetupContext';
 import { useUser } from 'lib/hooks';
 import { InferGetStaticPropsType } from 'next';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 export const getStaticProps = async () => ({
   props: {
@@ -14,18 +14,10 @@ export const getStaticProps = async () => ({
 export type DefaultSetupProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 const AvailabilitySetup: React.FC<DefaultSetupProps> = () => {
-  const [availability, setAvailability] = useState<string>('');
   const { user } = useUser({ redirectTo: '/' });
-
-  useEffect(() => {
-    if (user?.player) {
-      const { player } = user;
-      if (player.availability_hours && !availability) {
-        setAvailability(player.availability_hours.toString());
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const [availability, setAvailability] = useState<string>(
+    user?.player?.availability_hours?.toString() || '',
+  );
 
   return (
     <SetupContextProvider>
