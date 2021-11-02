3013f559522c153c1c29fa4584ce4833e575ba90
vidvidvid <weetopol@gmail.com>
2021-11-02 10:23:22 +0100

    fix: rebasing/merging problem

--- packages/web/graphql/mutations/player.ts
+++ packages/web/graphql/mutations/player.ts
@@ -56,16 +56,6 @@ export const UpdateProfilePronouns = gql`
   mutation updateProfilePronouns($playerId: uuid!, $input: player_set_input!) {
     update_player_by_pk(pk_columns: { id: $playerId }, _set: $input) {
       id
-      availability_hours
-      timezone
-    }
-  }
-`;
-
-export const UpdateProfilePronouns = gql`
-  mutation updateProfilePronouns($playerId: uuid!, $input: player_set_input!) {
-    update_player_by_pk(pk_columns: { id: $playerId }, _set: $input) {
-      id
     }
   }
 `;
