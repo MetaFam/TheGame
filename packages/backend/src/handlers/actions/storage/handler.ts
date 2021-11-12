a470d1a5d91927e0b701f70ee55b9f71047be34b
W.J. “dysbulic” H <dys@dhappy.org>
2021-11-12 02:12:46 -0500

    semicolons 🐓

--- packages/backend/src/handlers/actions/storage/handler.ts
+++ packages/backend/src/handlers/actions/storage/handler.ts
@@ -1,33 +1,27 @@
 import { Request, Response } from 'express';
-import fs from 'fs'
-import Path from 'path'
-import { Web3Storage } from 'web3.storage'
+import fs from 'fs';
+import Path from 'path';
+import { Web3Storage } from 'web3.storage';
 
-import { CONFIG } from '../../../config'
+import { CONFIG } from '../../../config';
 
 // eslint-disable-next-line import/no-default-export
-export default async (req: Request, res: Response):
-Promise<Response> => {
-  const storage = new Web3Storage({ token: CONFIG.web3StorageToken })
-  const input = req.files as Record<string, Array<Express.Multer.File>>
+export default async (req: Request, res: Response): Promise<Response> => {
+  const storage = new Web3Storage({ token: CONFIG.web3StorageToken });
+  const input = req.files as Record<string, Array<Express.Multer.File>>;
 
-  const files = Object.entries(input).map(
-    ([key, [{ path }]]) => ({
-      name: key,
-      stream: () => (
-        fs.createReadStream(
-          Path.isAbsolute(path) ? path : Path.join(process.cwd(), path)
-        ) as any
-      ),
-    })
-  )
+  const files = Object.entries(input).map(([key, [{ path }]]) => ({
+    name: key,
+    stream: () =>
+      fs.createReadStream(
+        Path.isAbsolute(path) ? path : Path.join(process.cwd(), path),
+      ) as any,
+  }));
 
-  const cid = await storage.put(files)
+  const cid = await storage.put(files);
 
   const uploadedFiles = Object.fromEntries(
-    Object.keys(input).map(
-      (key: string) => [key, `${cid}/${key}`]
-    )
-  )
+    Object.keys(input).map((key: string) => [key, `${cid}/${key}`]),
+  );
   return res.json(uploadedFiles);
 };
