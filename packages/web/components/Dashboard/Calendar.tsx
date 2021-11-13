8e8c3cdc5371fdbcd05a0408c964f0c1ed044c74
vidvidvid <weetopol@gmail.com>
2021-11-13 20:29:15 +0100

    styling: margin & size

--- packages/web/components/Dashboard/Calendar.tsx
+++ packages/web/components/Dashboard/Calendar.tsx
@@ -11,10 +11,11 @@ export const Calendar: React.FC = () => {
       title="calendar"
       src={`https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=${timezone}&showPrint=0&mode=AGENDA&showTitle=0&showNav=1&showTabs=0&showPrint=0&showCalendars=0&src=${calId}&color=%23F09300`}
       style={{
-        border: 'solid 1px #777',
+        // border: 'solid 1px #777',
+        marginTop: 20,
       }}
-      width={400}
-      height={600}
+      width="100%"
+      height="110%"
       scrolling="no"
     />
   );
