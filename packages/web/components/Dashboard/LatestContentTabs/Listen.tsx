b3a99c35621119f4b3d57d2ab1cc6858b438c209
vidvidvid <weetopol@gmail.com>
2021-11-15 21:31:48 +0100

    feat: podcast player + title + description

--- packages/web/components/Dashboard/LatestContentTabs/Listen.tsx
+++ packages/web/components/Dashboard/LatestContentTabs/Listen.tsx
@@ -1,8 +1,13 @@
-import { Box, Text } from '@metafam/ds';
-import React from 'react';
+import 'react-h5-audio-player/lib/styles.css';
+
+import { Box, Heading, Text } from '@metafam/ds';
+import React, { useState } from 'react';
+import AudioPlayer from 'react-h5-audio-player';
 import { parse } from 'rss-to-json';
 
 export const Listen: React.FC = () => {
+  const [items, setItems] = useState([]);
+
   const getData = async () => {
     const podcastDataRss = await parse(
       'https://anchor.fm/s/57a641c/podcast/rss',
@@ -10,12 +15,31 @@ export const Listen: React.FC = () => {
     );
 
     console.log('podcast data', podcastDataRss);
+    setItems(podcastDataRss.items);
+    console.log('items', items);
   };
 
-  getData();
+  if (items.length === 0) getData();
+  console.log('items', items);
 
   return (
     <Box>
+      {items.map((item) => (
+        <Box>
+          <Heading size="xs" color="white">
+            {item.title}
+          </Heading>
+          <Text>{item.description.replace(/<\/?[^>]+(>|$)/g, '')}</Text>
+          <AudioPlayer
+            // autoPlay
+            preload="none"
+            src={item.enclosures[0].url}
+            onPlay={(e) => console.log('onPlay')}
+            // other props here
+          />
+        </Box>
+      ))}
+
       <Text>A feed podcast episodes from Anchor or wherever.</Text>
     </Box>
   );
