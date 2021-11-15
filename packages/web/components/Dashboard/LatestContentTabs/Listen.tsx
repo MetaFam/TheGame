2f158e25ec7036c283999fdeea0dfb8855b6ec55
vidvidvid <weetopol@gmail.com>
2021-11-15 20:58:24 +0100

    feat: fetch rss data from anchor.fm -> conver to json

--- packages/web/components/Dashboard/LatestContentTabs/Listen.tsx
+++ packages/web/components/Dashboard/LatestContentTabs/Listen.tsx
@@ -1,8 +1,22 @@
 import { Box, Text } from '@metafam/ds';
 import React from 'react';
+import { parse } from 'rss-to-json';
 
-export const Listen: React.FC = () => (
-  <Box>
-    <Text>A feed podcast episodes from Anchor or wherever.</Text>
-  </Box>
-);
+export const Listen: React.FC = () => {
+  const getData = async () => {
+    const podcastDataRss = await parse(
+      'https://anchor.fm/s/57a641c/podcast/rss',
+      {},
+    );
+
+    console.log('podcast data', podcastDataRss);
+  };
+
+  getData();
+
+  return (
+    <Box>
+      <Text>A feed podcast episodes from Anchor or wherever.</Text>
+    </Box>
+  );
+};
