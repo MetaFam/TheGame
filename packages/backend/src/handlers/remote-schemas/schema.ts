b9d4fc15a519bdfa88102b65df8c12d6423c1f8b
vidvidvid <weetopol@gmail.com>
2021-11-24 22:42:16 +0100

    chore: replace graphql-tools for @graphql-tools/schema

--- packages/backend/src/handlers/remote-schemas/schema.ts
+++ packages/backend/src/handlers/remote-schemas/schema.ts
@@ -1,4 +1,4 @@
-import { makeExecutableSchema } from 'graphql-tools';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 
 import { getBrightIdStatus } from './resolvers/brightId/resolver';
 import { getDaoHausMemberships } from './resolvers/daohaus/resolver';
