f2cee04788285dbcf688c76accd8498c97eca907
vidvidvid <weetopol@gmail.com>
2021-11-25 09:01:13 +0100

    chore: move calendarId to config

--- packages/web/components/Dashboard/Calendar.tsx
+++ packages/web/components/Dashboard/Calendar.tsx
@@ -1,6 +1,5 @@
+import { CONFIG } from 'config';
 import React, { useEffect, useState } from 'react';
-
-const calId = 'nih59ktgafmm64ed4qk6ue8vv4@group.calendar.google.com';
 
 export const Calendar: React.FC = () => {
   const [isComponentMounted, setIsComponentMounted] = useState(false);
@@ -16,7 +15,7 @@ export const Calendar: React.FC = () => {
   return (
     <iframe
       title="calendar"
-      src={`https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=${timezone}&showPrint=0&mode=AGENDA&showTitle=0&showNav=1&showTabs=0&showPrint=0&showCalendars=0&src=${calId}&color=%23F09300`}
+      src={`https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=${timezone}&showPrint=0&mode=AGENDA&showTitle=0&showNav=1&showTabs=0&showPrint=0&showCalendars=0&src=${CONFIG.calendarId}&color=%23F09300`}
       style={{
         // border: 'solid 1px #777',
         marginTop: 20,
