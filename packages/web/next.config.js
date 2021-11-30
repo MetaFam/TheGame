881bb558e615e0744753293f911fafbeca5d7ba1
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-11-30 23:04:16 +0530

    fix: webpack 5 added in next.config

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -13,12 +13,29 @@ module.exports = withTM(
         },
       ];
     },
+    future: {
+      webpack5: true,
+    },
     webpack: (config, { isServer, webpack }) => {
       if (!isServer) {
         config.plugins.push(
           // jsdom is required for draft-js SSR only
           new webpack.IgnorePlugin(/jsdom$/),
         );
+        config.resolve.fallback = {
+          fs: false,
+          net: false,
+          http: false,
+          os: false,
+          https: false,
+          child_process: false,
+          stream: false,
+          'browserify-zlib': false,
+          zlib: false,
+          crypto: false,
+          path: false,
+          tls: false,
+        };
       }
 
       return config;
