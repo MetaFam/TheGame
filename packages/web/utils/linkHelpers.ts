341b154cd1d0b6f1f2acafc43dbb3f166e7eafc3
W.J. “dysbulic” H <dys@dhappy.org>
2021-11-29 21:48:35 -0500

    function to generate bafy….ipfs.dweb.link subdomain-style IPFS links 🎤

---
+++ packages/web/utils/linkHelpers.ts
@@ -0,0 +1,15 @@
+import CID from 'cids';
+import { CONFIG } from 'config';
+
+export const httpLink = (uri: string | undefined | null) => {
+  const [, cid, path] =
+    uri?.match(/^(?:ipfs|dweb):(?:\/\/)?([^/]+)(?:\/(.*))?$/) ?? [];
+
+  if (cid) {
+    const v1CID = new CID(cid).toV1().toString('base32');
+    const pattern = CONFIG.ipfsLinkPattern;
+    return pattern.replace(/\${cid}/g, v1CID).replace(/\${path}/g, path ?? '');
+  }
+
+  return uri;
+};
