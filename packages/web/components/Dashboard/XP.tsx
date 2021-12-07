d3a688eb77f889f5a8016d7ef575fcc4c01ccf8b
Udit Takkar <53316345+Udit-takkar@users.noreply.github.com>
2021-12-07 19:56:21 +0530

    Add XP Display to the Dashboard (#899)
    Displays the total and seasonal XP accumulation on the new dashboard.

--- packages/web/components/Dashboard/XP.tsx
+++ packages/web/components/Dashboard/XP.tsx
@@ -1,45 +1,213 @@
 import {
+  Box,
+  Button,
+  ButtonGroup,
+  MetaTag,
   Stat,
   StatArrow,
   StatGroup,
   StatHelpText,
   StatLabel,
   StatNumber,
+  Text,
 } from '@metafam/ds';
-import React from 'react';
+import { animated, useSpring } from '@react-spring/web';
+import { useUser } from 'lib/hooks';
+import { useUserXP } from 'lib/hooks/useUserXp';
+import React, { FC, ReactNode, useState } from 'react';
+import { FaChartBar } from 'react-icons/fa';
+import {
+  AreaSeries,
+  AreaSeriesPoint,
+  FlexibleXYPlot,
+  GradientDefs,
+  LineSeries,
+} from 'react-vis';
 
-export const XP: React.FC = () => (
-  <StatGroup mt={5} flex="0 0 50%">
-    <Stat mb={3}>
-      <StatLabel>This Week</StatLabel>
-      <StatNumber>45</StatNumber>
-      <StatHelpText>
-        <StatArrow type="decrease" />
-        55%
-      </StatHelpText>
-    </Stat>
+import { XPChartWrapperStyles } from './config';
 
-    <Stat mb={3} flex="0 0 50%">
-      <StatLabel>Last Week</StatLabel>
-      <StatNumber>78</StatNumber>
-      <StatHelpText>
-        <StatArrow type="decrease" />
-        22%
-      </StatHelpText>
-    </Stat>
+export const XP = (): React.ReactElement => {
+  const { user } = useUser();
 
-    <Stat alignSelf="flex-start" justifySelf="flex-end" flex="0 0 50%">
-      <StatLabel>All Time</StatLabel>
-      <StatNumber>2463</StatNumber>
-      <StatHelpText color="diamond">Diamond</StatHelpText>
-    </Stat>
-    <Stat alignSelf="flex-start" justifySelf="flex-end" flex="0 0 50%">
-      <StatLabel>XP/SEED Ratio</StatLabel>
-      <StatNumber>3.4</StatNumber>
-      <StatHelpText>
-        <StatArrow type="decrease" />
-        0.3
-      </StatHelpText>
-    </Stat>
-  </StatGroup>
+  const xpStats = useUserXP(user?.ethereum_address || '');
+
+  if (xpStats == null) {
+    return (
+      <Text fontStyle="italic" textAlign="center">
+        Unknown
+      </Text>
+    );
+  }
+  const {
+    userTotalXP,
+    variationThisWeek,
+    variationLastWeek,
+    thisWeekXP,
+    lastWeekXP,
+    userWeeklyCred,
+  } = xpStats;
+  return (
+    <>
+      <StatGroup mt={5} flex="0 0 50%">
+        <Stat mb={3}>
+          <StatLabel>This Week</StatLabel>
+          <StatNumber>{thisWeekXP}</StatNumber>
+          <StatHelpText>
+            <StatArrow type={variationThisWeek < 0 ? 'decrease' : 'increase'} />
+            {variationThisWeek}%
+          </StatHelpText>
+        </Stat>
+
+        <Stat mb={3} flex="0 0 50%">
+          <StatLabel>Last Week</StatLabel>
+          <StatNumber>{lastWeekXP}</StatNumber>
+          <StatHelpText>
+            <StatArrow type={variationLastWeek < 0 ? 'decrease' : 'increase'} />
+            {lastWeekXP}%
+          </StatHelpText>
+        </Stat>
+
+        <Stat alignSelf="flex-start" justifySelf="flex-end" flex="0 0 100%">
+          <StatLabel>All Time</StatLabel>
+          <StatNumber>{userTotalXP}</StatNumber>
+          {user?.player?.rank && (
+            <MetaTag
+              backgroundColor={user.player.rank.toLowerCase()}
+              size="md"
+              color="blackAlpha.600"
+            >
+              {user.player.rank}
+            </MetaTag>
+          )}
+        </Stat>
+      </StatGroup>
+      <Box
+        className="chartWrapper"
+        position="absolute"
+        width="100%"
+        height="100%"
+        bottom={0}
+        left={0}
+        zIndex={0}
+        sx={XPChartWrapperStyles}
+      >
+        {userWeeklyCred && <Chart data={userWeeklyCred} />}
+      </Box>
+    </>
+  );
+};
+
+type ChartType = {
+  children?: ReactNode;
+  data: Array<number>;
+};
+
+const ChartRange = ({ value = {} }): React.ReactElement => (
+  <Box>{`${value}d`}</Box>
 );
+const AnimatedChartRange = animated(ChartRange);
+
+export const Chart: FC<ChartType> = ({ data }) => {
+  const [scale, setScale] = useState<number>(30);
+  const props = useSpring({
+    config: {
+      mass: 10,
+      tension: 280,
+      friction: 100,
+    },
+    range: scale,
+  });
+
+  const switchScale = (range: number) => {
+    setScale(range);
+  };
+
+  function makePlots(weeks: Array<number>) {
+    const plots: Array<AreaSeriesPoint> = weeks
+      .slice(-Math.floor(scale / 7))
+      .map((d, i) => ({
+        x: i + 1,
+        y: d,
+        y0: 0,
+      }));
+    return plots;
+  }
+
+  const plots = makePlots(data);
+
+  return (
+    <Box>
+      <Box position="absolute" top={6} right={6} zIndex={250}>
+        <ButtonGroup isAttached>
+          {[30, 90].map((s) => (
+            <Button
+              key={s}
+              aria-label="Toggle chart scale"
+              icon={<FaChartBar />}
+              borderColor="pinkShadeOne"
+              background="rgba(17, 17, 17, 0.9)"
+              color="pinkShadeOne"
+              _hover={{ color: 'white', borderColor: 'white' }}
+              variant="outline"
+              isRound
+              onClick={() => switchScale(s)}
+              size="xs"
+              sx={{
+                '&:focus, &:hover': {
+                  background: 'transparent',
+                },
+              }}
+            >
+              {s}d
+            </Button>
+          ))}
+        </ButtonGroup>
+      </Box>
+      <FlexibleXYPlot
+        className="xp-chart"
+        height={200}
+        margin={{
+          right: -2,
+          left: -2,
+          bottom: -2,
+        }}
+      >
+        <LineSeries
+          className="xp-chart-path"
+          curve="linear"
+          strokeStyle="solid"
+          animation={{ damping: 80, stiffness: 800 }}
+          stroke="#A426A4"
+          opacity={0.5}
+          data={plots}
+        />
+        <GradientDefs>
+          <linearGradient id="MetaGradient" x1="0" x2="0" y1="0" y2="1">
+            <stop offset="0%" stopColor="white" stopOpacity={0.9} />
+            <stop offset="100%" stopColor="black" stopOpacity={1} />
+          </linearGradient>
+        </GradientDefs>
+        <AreaSeries
+          className="xp-chart-path--fill"
+          curve="linear"
+          color="url(#MetaGradient)"
+          animation={{ damping: 80, stiffness: 800 }}
+          // stroke="#A426A4"
+          opacity={0.2}
+          data={plots}
+        />
+      </FlexibleXYPlot>
+      <Box
+        aria-label="XP Graph scale"
+        position="absolute"
+        bottom={5}
+        right={5}
+        opacity={0.25}
+        fontSize="xl"
+        fontWeight={700}
+      >
+        <AnimatedChartRange value={props.range.to((x) => x.toFixed(0))} />
+      </Box>
+    </Box>
+  );
+};
---
+++ packages/web/lib/hooks/useUserXp.ts
@@ -0,0 +1,82 @@
+import { Constants } from '@metafam/utils';
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+import { SCAccount, SCAccountsData } from 'sourcecred';
+
+interface XPProps {
+  userTotalXP: number;
+  variationThisWeek: number;
+  variationLastWeek: number;
+  thisWeekXP: number;
+  lastWeekXP: number;
+  userWeeklyCred: number[];
+}
+
+export const useUserXP = (userAddress: string): XPProps | null => {
+  const [xpDataObj, setXPDataObj] = useState<XPProps | null>(null);
+  useEffect(() => {
+    if (ethers.utils.isAddress(userAddress))
+      getXP(userAddress).then((res) => {
+        if (res) setXPDataObj(res);
+      });
+  }, [userAddress]);
+  return xpDataObj;
+};
+
+const getXP = async (userAddress: string): Promise<XPProps | null> => {
+  try {
+    const accountsData: SCAccountsData = await (
+      await fetch(Constants.SC_ACCOUNTS_FILE)
+    ).json();
+
+    const scAccount = await accountsData.accounts.find((account) =>
+      accountFilter(account, userAddress),
+    );
+
+    if (!scAccount) return null;
+
+    const userTotalXP = roundToTwoDecimal(scAccount.totalCred);
+    const numWeeks = scAccount.cred.length;
+    const userWeeklyCred = scAccount.cred;
+
+    let variationThisWeek = calculateVariation(
+      userWeeklyCred[numWeeks - 1],
+      userWeeklyCred[numWeeks - 2],
+    );
+    let variationLastWeek = calculateVariation(
+      userWeeklyCred[numWeeks - 2],
+      userWeeklyCred[numWeeks - 3],
+    );
+
+    const thisWeekXP = roundToTwoDecimal(userWeeklyCred[numWeeks - 1]);
+    const lastWeekXP = roundToTwoDecimal(userWeeklyCred[numWeeks - 2]);
+    variationThisWeek = roundToTwoDecimal(variationThisWeek);
+    variationLastWeek = roundToTwoDecimal(variationLastWeek);
+
+    return {
+      userTotalXP,
+      variationThisWeek,
+      variationLastWeek,
+      thisWeekXP,
+      lastWeekXP,
+      userWeeklyCred,
+    };
+  } catch (err: unknown) {
+    // throw new Error(err);
+    return null;
+  }
+};
+
+const accountFilter = (a: SCAccount, userAddress: string) => {
+  const { account } = a;
+
+  const acc = account.identity.aliases.find(
+    (alias) => alias.description.toLowerCase() === userAddress.toLowerCase(),
+  );
+  return !!acc;
+};
+
+const calculateVariation = (first: number, second: number) =>
+  (100 * (first - second)) / second;
+
+const roundToTwoDecimal = (num: number): number => Math.round(num * 100) / 100;
