125c5d85e9a11e1f32523726cc48311d1cdee0dc
Tommy <tommymaher10@gmail.com>
2021-12-11 21:09:02 +0000

    Create landing.tsx
    testing

--- packages/web/pages/landing.tsx
+++ packages/web/pages/landing.tsx
@@ -1,35 +1,7 @@
 import React from 'react';
-import Intro from '../components/Landing/Intro';
-import Game from '../components/Landing/Game';
-import Build from '../components/Landing/Build';
-import Revolution from '../components/Landing/Revolution';
-import WildWeb from '../components/Landing/WildWeb';
-import Frontier from '../components/Landing/Frontier';
-import Together from '../components/Landing/Together';
-import WhatWeDo from '../components/Landing/WhatWeDo';
-import Optimal from '../components/Landing/Optimal';
-import Unplug from '../components/Landing/Unplug';
-import Who from '../components/Landing/Who';
-import JustWatch from '../components/Landing/JustWatch';
-import Cards from '../components/Landing/Cards';
 
 function Landing(): JSX.Element {
-  return <div>
-    <Intro />
-    <Game />
-    <Build />
-    <Revolution/>
-    <WildWeb />
-    <Frontier />
-    <Together /> 
-    <WhatWeDo /> 
-    <Optimal />
-    <Unplug />
-    <Who />
-    <Cards /> 
-    <JustWatch />
-
-  </div>;
+  return <div>testing</div>;
 }
 
 export default Landing;
