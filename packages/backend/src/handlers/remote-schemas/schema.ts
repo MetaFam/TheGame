4ba0803b39ee03e1f6cb3118f3f32a451bf1da64
vidvidvid <weetopol@gmail.com>
2021-12-20 17:43:33 +0100

    fix: revert the @graphql-tools/schema -> graphql-tools

--- packages/backend/src/handlers/remote-schemas/schema.ts
+++ packages/backend/src/handlers/remote-schemas/schema.ts
@@ -1,4 +1,4 @@
-import { makeExecutableSchema } from '@graphql-tools/schema';
+import { makeExecutableSchema } from 'graphql-tools';
 
 import { getBrightIdStatus } from './resolvers/brightId/resolver';
 import { getDaoHausMemberships } from './resolvers/daohaus/resolver';
