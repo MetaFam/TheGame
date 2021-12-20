5fef8118f1e9e47d9674a683124cd2dafad2375b
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-12-20 12:29:16 +0530

    fix: remove nextjs-cors package

--- packages/web/pages/api/feed.ts
+++ packages/web/pages/api/feed.ts
@@ -1,21 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import NextCors from 'nextjs-cors';
 import { parse } from 'rss-to-json';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Run the cors middleware
-  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD'],
-    origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
-
   try {
     const apiResponse = await parse('https://metagame.substack.com/feed', {
-      timeout: 1000,
+      timeout: 2000,
     });
 
     return res.json({ error: null, response: apiResponse });
