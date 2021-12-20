79dc6f9e505fe67251ff3839f6f38d7bc1357ce2
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-12-20 12:36:38 +0530

    fix: use req object

--- packages/web/pages/api/feed.ts
+++ packages/web/pages/api/feed.ts
@@ -3,14 +3,18 @@ import { parse } from 'rss-to-json';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const apiResponse = await parse('https://metagame.substack.com/feed', {
-      timeout: 2000,
-    });
+  if (req.method === 'GET') {
+    try {
+      const apiResponse = await parse('https://metagame.substack.com/feed', {
+        timeout: 2000,
+      });
 
-    return res.json({ error: null, response: apiResponse });
-  } catch (err) {
-    return res.json({ error: err, response: null });
+      return res.json({ error: null, response: apiResponse });
+    } catch (err) {
+      return res.json({ error: err, response: null });
+    }
+  } else {
+    return res.json({ error: 'Error: Incorrect Method', response: null });
   }
 }
 
