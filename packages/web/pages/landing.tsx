5a4da251a13026d42c711dd4b9d6921833caf769
Tommy <tommymaher10@gmail.com>
2022-01-03 21:34:11 +0000

    Landing: Added second section and necessary assets

--- packages/web/pages/landing.tsx
+++ packages/web/pages/landing.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import Intro from '../components/landing/Intro';
-
+import Intro from '../components/Landing/Intro';
+import Game from '../components/Landing/Game';
 function Landing(): JSX.Element {
   return <div>
     <Intro />
+    <Game /> 
   </div>;
 }
 
