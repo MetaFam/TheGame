dc2c0fc39b8a644e06ebea9bedcc682e3e3530c9
Tommy <tommymaher10@gmail.com>
2022-01-03 23:09:31 +0000

    Landing: Build section done

--- packages/web/pages/landing.tsx
+++ packages/web/pages/landing.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Intro from '../components/Landing/Intro';
 import Game from '../components/Landing/Game';
+import Build from '../components/Landing/Build';
 function Landing(): JSX.Element {
   return <div>
     <Intro />
     <Game /> 
+    <Build />
   </div>;
 }
 
