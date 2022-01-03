c08d036bfc307f56f1b9712e2afeff5ad42158ae
Firat Tale <firat.tale@auto1.com>
2022-01-03 14:18:13 +0100

    Add BG Image

--- packages/web/pages/404.tsx
+++ packages/web/pages/404.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Box, Flex, Heading } from '@chakra-ui/react';
 import { MetaButton } from '@metafam/ds';
+import BackgroundImage from 'assets/404-bg.svg';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { FC } from 'react';
@@ -7,19 +10,63 @@ const Custom404: FC = () => {
   const router = useRouter();
 
   return (
-    <>
-      <h1>Oops!</h1>
-      <h3>We can't seem to find the page you're looking for.</h3>
-      <p>Here are some good places to start exploring MetaGame, instead:</p>
-      <MetaButton onClick={() => router.push('/')}>Home</MetaButton>
-      <MetaButton onClick={() => router.push('/learn/wiki')}>Wiki</MetaButton>
-      <Link href="https://forum.metagame.wtf">
-        <MetaButton>Forum</MetaButton>
-      </Link>
-      <Link href="https://discord.gg/metagame">
-        <MetaButton>Discord</MetaButton>
-      </Link>
-    </>
+    <Flex
+      p={{ base: 10, sm: 6 }}
+      flexDirection={'column'}
+      backgroundImage={`url(${BackgroundImage})`}
+      backgroundSize={'contain'}
+      backgroundPosition={{ sm: 'top', xl: '70%' }}
+      backgroundRepeat={'no-repeat'}
+    >
+      <Heading
+        as="h1"
+        size="4xl"
+        fontFamily={'Exo 2'}
+        pb={8}
+        fontSize={{ base: '90px', sm: '39px', md: '67px' }}
+      >
+        Oops!
+      </Heading>
+      <Heading
+        as="h2"
+        fontFamily={'Exo 2'}
+        pb={8}
+        fontSize={{ base: '38px', sm: '20px', md: '31px' }}
+      >
+        We can't seem to find the page you're looking for.
+      </Heading>
+      <Box fontSize={{ base: '25px', sm: '16px' }}>
+        Here are some good places to start exploring MetaGame, instead:
+      </Box>
+      <Flex direction="column">
+        <Box pt={8}>
+          <MetaButton
+            width={{ sm: '100%', xl: '25%' }}
+            onClick={() => router.push('/')}
+          >
+            Home
+          </MetaButton>
+        </Box>
+        <Box pt={8}>
+          <MetaButton
+            width={{ sm: '100%', xl: '25%' }}
+            onClick={() => router.push('/learn/wiki')}
+          >
+            Wiki
+          </MetaButton>
+        </Box>
+        <Box pt={8}>
+          <Link href="https://forum.metagame.wtf">
+            <MetaButton width={{ sm: '100%', xl: '25%' }}>Forum</MetaButton>
+          </Link>
+        </Box>
+        <Box pt={8}>
+          <Link href="https://discord.gg/metagame">
+            <MetaButton width={{ sm: '100%', xl: '25%' }}>Discord</MetaButton>
+          </Link>
+        </Box>
+      </Flex>
+    </Flex>
   );
 };
 
