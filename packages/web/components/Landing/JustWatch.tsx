0434d9a995547df6bc7af61765280905e1ca7d6e
Tommy <tommymaher10@gmail.com>
2022-01-04 16:53:14 +0000

    large commit: added most final sections, one more to go

---
+++ packages/web/components/Landing/JustWatch.tsx
@@ -0,0 +1,73 @@
+import {
+    Container,
+    Box,
+    Text,
+
+    Button 
+    } from "@chakra-ui/react"
+  import BackgroundImage from 'assets/landing/just-watch-background.png'
+
+  import { FaArrowDown } from 'react-icons/fa';
+  
+  function Intro() {
+    return (
+      <Box
+        width="100%"
+        minHeight = "1040px"
+        backgroundImage={`url(${BackgroundImage})`}
+        bgPosition="center"
+        bgSize="cover"
+  
+      >
+  
+        <Container
+          flexDirection="column"
+          justifyContent="flex-end"
+          display="flex" alignItems="center"
+          width="100%"
+          height="100vh"
+          pb = '78px'
+          maxWidth="1200px"
+          textAlign = "center"
+        >
+          
+          <Text
+                max-width="1200px"
+           fontSize="76px" 
+           lineHeight = "80px"
+           fontWeight="normal"
+            color="white"
+             mt="10px"
+              mb="35px"
+              >
+           The revolution will be televized,
+but don’t just watch.
+          </Text>
+          <Button
+           fontWeight="normal"
+            fontSize="18px"
+             bg="#B40C85" 
+             mb="16px"
+             width='284px'
+             textTransform = "uppercase"
+              >
+              Join Nao
+                </Button>
+          <Button 
+            fontWeight="normal"
+            fontSize="18px"
+            bg="transparent" 
+            border='1px'
+            borderColor="white"
+            color="white"
+            width='284px'
+            textTransform = "uppercase"
+            rightIcon={<FaArrowDown />}>
+              Explore Moar
+          </Button>
+        </Container>
+      </Box>
+    );
+  }
+  
+  export default Intro;
