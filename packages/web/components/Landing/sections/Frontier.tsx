ace61dc921c3150b2b86746fc42ac5aede887169
Tommy <tommymaher10@gmail.com>
2022-01-07 23:56:24 +0000

    feat: landing ( Improved file structure for landing page and tidied improrts

--- packages/web/components/Landing/sections/Frontier.tsx
+++ packages/web/components/Landing/sections/Frontier.tsx
@@ -2,12 +2,12 @@ import {
     Container,
     Box,
     Text,
-   
-  } from "@chakra-ui/react"
-  import BackgroundImage from 'assets/landing/frontier-background.png'
-  
-  
-  
+
+} from "@chakra-ui/react"
+import BackgroundImage from 'assets/landing/frontier-background.png'
+
+
+
 function Frontier() {
     return (
         <Box
@@ -18,41 +18,40 @@ function Frontier() {
             bgSize="cover"
         >
             <Container
-                  width="100%"
-                  height="100vh"
-                  display="flex"
-                  flexDirection="column"
-                  justifyContent="center"
-                  maxWidth="100%"
-                  pl="137px"
-            >
-                <Text
-                fontSize="38px"
-                LineHeight="56px"
-                fontWeight="normal"
-                color="white"
+                width="100%"
+                height="100vh"
                 display="flex"
                 flexDirection="column"
-                maxWidth = "524px"
+                justifyContent="center"
+                maxWidth="100%"
+                pl="137px"
+            >
+                <Text
+                    fontSize="38px"
+                    LineHeight="56px"
+                    fontWeight="normal"
+                    color="white"
+                    display="flex"
+                    flexDirection="column"
+                    maxWidth="524px"
                 >
                     Many have already woken up to the world-shaping potential of Web3 technologies.
                 </Text>
 
                 <Text
-                pt = "56px"
-          fontSize="38px"
-          LineHeight="56px"
-          fontWeight="normal"
-          color="white"
-          display="flex"
-          flexDirection="column"
-          maxWidth = "524px"
+                    pt="56px"
+                    fontSize="38px"
+                    LineHeight="56px"
+                    fontWeight="normal"
+                    color="white"
+                    display="flex"
+                    flexDirection="column"
+                    maxWidth="524px"
                 >
-                   Some are grabbing the opportunity to build the future they want to live in.
+                    Some are grabbing the opportunity to build the future they want to live in.
                 </Text>
             </Container>
         </Box>
     );
 }
-
 export default Frontier;
