d0c3b1f586b023e66e3bd032ccc267837e32f313
Tommy <tommymaher10@gmail.com>
2022-01-08 02:17:07 +0000

    testing type declaration

--- packages/web/components/Landing/sections/Build.tsx
+++ packages/web/components/Landing/sections/Build.tsx
@@ -1,53 +1,51 @@
 import { Box, Container, Text } from '@metafam/ds';
 import BackgroundImage from 'assets/landing/build-background.png';
 
-function Build() {
-  return (
-    <Box
+export const Build: React.FC = () => (
+  <Box
+    width="100%"
+    minHeight="1040px"
+    backgroundImage={`url(${BackgroundImage})`}
+    bgPosition="center"
+    bgSize="cover"
+  >
+    <Container
       width="100%"
-      minHeight="1040px"
-      backgroundImage={`url(${BackgroundImage})`}
-      bgPosition="center"
-      bgSize="cover"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      maxWidth="100%"
+      pl="137px"
     >
-      <Container
-        width="100%"
-        height="100vh"
+      <Text
+        fontSize="38px"
+        LineHeight="56px"
+        fontWeight="normal"
+        color="white"
         display="flex"
         flexDirection="column"
-        justifyContent="center"
-        maxWidth="100%"
-        pl="137px"
+        maxWidth="524px"
       >
-        <Text
-          fontSize="38px"
-          LineHeight="56px"
-          fontWeight="normal"
-          color="white"
-          display="flex"
-          flexDirection="column"
-          maxWidth="524px"
-        >
-          Many have already woken up to the world-shaping potential of Web3
-          technologies.
-        </Text>
+        Many have already woken up to the world-shaping potential of Web3
+        technologies.
+      </Text>
 
-        <Text
-          pt="56px"
-          fontSize="38px"
-          LineHeight="56px"
-          fontWeight="normal"
-          color="white"
-          display="flex"
-          flexDirection="column"
-          maxWidth="524px"
-        >
-          Some are grabbing the opportunity to build the future they want to
-          live in.
-        </Text>
-      </Container>
-    </Box>
-  );
-}
+      <Text
+        pt="56px"
+        fontSize="38px"
+        LineHeight="56px"
+        fontWeight="normal"
+        color="white"
+        display="flex"
+        flexDirection="column"
+        maxWidth="524px"
+      >
+        Some are grabbing the opportunity to build the future they want to live
+        in.
+      </Text>
+    </Container>
+  </Box>
+);
 
 export default Build;
