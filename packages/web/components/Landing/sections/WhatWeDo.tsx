3bb135d36b5f852f23696f910a2ed96f0efa0ddc
Tommy <tommymaher10@gmail.com>
2022-01-08 00:07:32 +0000

    fixed ordered list

--- packages/web/components/Landing/sections/WhatWeDo.tsx
+++ packages/web/components/Landing/sections/WhatWeDo.tsx
@@ -3,7 +3,7 @@ import {
     Box,
     Text,
     ListItem,
-    UnorderedList,
+    OrderedList,
 } from "@metafam/ds"
 import BackgroundImage from 'assets/landing/whatWeDo-background.png'
 
@@ -35,14 +35,14 @@ function WhatWeDo() {
                     <Text pt="130px" pb="35px" fontWeight = "700">
                     What are we doing?
                     </Text>
-                    <UnorderedList pb="35px" fontWeight = "300" opacity = "0.8">
+                    <OrderedList pb="35px" fontWeight = "300" opacity = "0.8">
                         <ListItem >Curating knowledge</ListItem>
                         <ListItem>Organizing events</ListItem>
                         <ListItem>Producing content</ListItem>
                          <ListItem>Building technologies</ListItem>
                          <ListItem>Uniting aligned people & communities</ListItem>
                          <ListItem>Putting together the pieces of the New World puzzle</ListItem>
-                    </UnorderedList>
+                    </OrderedList>
 
                     <Text fontWeight = "700" width = "100%">In short, anything & everything related to DAOs & helping people build the future they want to live in</Text>
                    
