a3566768f80fc5bd546b7de1f401625e95a4543f
Tommy <tommymaher10@gmail.com>
2022-01-08 00:58:32 +0000

    prettier and cleanup folders

--- packages/web/components/Landing/sections/Who.tsx
+++ packages/web/components/Landing/sections/Who.tsx
@@ -1,48 +1,39 @@
-import {
-    Container,
-    Box,
-    Text,
-} from "@metafam/ds"
-import BackgroundImage from 'assets/landing/who-background.png'
-
-
+import { Box, Container, Text } from '@metafam/ds';
+import BackgroundImage from 'assets/landing/who-background.png';
 
 function Who() {
-    return (
+  return (
+    <Box
+      width="100%"
+      minHeight="1040px"
+      backgroundImage={`url(${BackgroundImage})`}
+      bgPosition="center"
+      bgSize="cover"
+    >
+      <Container
+        width="100%"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="flex-end"
+        maxWidth="100%"
+        alignItems="center"
+        textAlign="center"
+      >
         <Box
-            width="100%"
-            minHeight="1040px"
-            backgroundImage={`url(${BackgroundImage})`}
-            bgPosition="center"
-            bgSize="cover"
+          fontSize="67px"
+          LineHeight="80px"
+          fontWeight="normal"
+          color="white"
+          maxWidth="1200px"
         >
-            <Container
-                width="100%"
-                height="100vh"
-                display="flex"
-                flexDirection="column"
-                justifyContent="flex-end"
-                maxWidth="100%"
-                alignItems="center"
-                textAlign="center"
-
-            >
-                <Box fontSize="67px"
-                     LineHeight="80px"
-                     fontWeight="normal"
-                     color="white"
-                     maxWidth="1200px">
-
-                    <Text pt="130px" pb="35px" textAlign = "center">
-                    So, whom is it for?
-                    </Text>
-                   
-                   
-                </Box>
-
-            </Container>
+          <Text pt="130px" pb="35px" textAlign="center">
+            So, whom is it for?
+          </Text>
         </Box>
-    );
+      </Container>
+    </Box>
+  );
 }
 
 export default Who;
