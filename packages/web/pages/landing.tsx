f66bdd151207f221718cb6c7d78d925605212c4b
Tommy <tommymaher10@gmail.com>
2022-01-12 08:46:01 +0000

    updated node modules

--- packages/web/pages/landing.tsx
+++ packages/web/pages/landing.tsx
@@ -1,19 +1,17 @@
+import Build from 'components/Landing/Sections/Build';
+import Cards from 'components/Landing/Sections/Cards';
+import Frontier from 'components/Landing/Sections/Frontier';
+import Game from 'components/Landing/Sections/Game';
+import Intro from 'components/Landing/Sections/Intro';
+import JustWatch from 'components/Landing/Sections/JustWatch';
+import Optimal from 'components/Landing/Sections/Optimal';
+import Revolution from 'components/Landing/Sections/Revolution';
+import Together from 'components/Landing/Sections/Together';
+import Unplug from 'components/Landing/Sections/Unplug';
+import WhatWeDo from 'components/Landing/Sections/WhatWeDo';
+import Who from 'components/Landing/Sections/Who';
+import WildWeb from 'components/Landing/Sections/WildWeb';
 import React from 'react';
-
-// eslint-disable-next-line import/no-named-as-default
-import Build from '../components/Landing/Sections/Build';
-import Cards from '../components/Landing/Sections/Cards';
-import Frontier from '../components/Landing/Sections/Frontier';
-import Game from '../components/Landing/Sections/Game';
-import Intro from '../components/Landing/Sections/Intro';
-import JustWatch from '../components/Landing/Sections/JustWatch';
-import Optimal from '../components/Landing/Sections/Optimal';
-import Revolution from '../components/Landing/Sections/Revolution';
-import Together from '../components/Landing/Sections/Together';
-import Unplug from '../components/Landing/Sections/Unplug';
-import WhatWeDo from '../components/Landing/Sections/WhatWeDo';
-import Who from '../components/Landing/Sections/Who';
-import WildWeb from '../components/Landing/Sections/WildWeb';
 
 export const getStaticProps = async () => ({
   props: {
