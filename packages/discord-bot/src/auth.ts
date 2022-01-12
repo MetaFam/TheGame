07f0d86f252a5f9216d3dde978228929f4ce9a6e
W.J. “dysbulic” H <dys@dhappy.org>
2022-01-12 05:08:05 -0500

    fixing ESM require & attempting to redeploy on push to a PR 🧁

--- packages/discord-bot/src/auth.ts
+++ packages/discord-bot/src/auth.ts
@@ -8,8 +8,12 @@ import {
   OAuth2CodeExchangeResponse,
 } from './types';
 
+// This is necessary to prevent transpilation to a require statement
+// eslint-disable-next-line @typescript-eslint/no-implied-eval
+const importDynamic = new Function('modulePath', 'return import(modulePath)');
+
 const fetch = async (url: RequestInfo, init?: RequestInit | undefined) => {
-  const { default: nodeFetch } = await import('node-fetch');
+  const { default: nodeFetch } = await importDynamic('node-fetch');
   return nodeFetch(url, init);
 };
 
