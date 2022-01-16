dbeffb1dbb415c03cad2c33e4cd8fe8153655910
vidvidvid <weetopol@gmail.com>
2022-01-16 16:14:56 +0100

    feat: display roles on /quest/id card

--- packages/web/components/Roles.tsx
+++ packages/web/components/Roles.tsx
@@ -1,4 +1,4 @@
-import { SelectSearch } from '@metafam/ds';
+import { MetaTag, SelectSearch, Tooltip, Wrap, WrapItem } from '@metafam/ds';
 import { PlayerRole } from 'graphql/autogen/types';
 import React from 'react';
 import { RoleOption } from 'utils/roleHelpers';
@@ -33,4 +33,20 @@ export const RolesSelect: React.FC<SetupRolesProps> = ({
     id={`roles-select-container-${id || ''}`}
     inputId={`roles-select-input-${id || ''}`}
   />
+);
+interface RolesProps {
+  roles: PlayerRole[];
+}
+export const RolesTags: React.FC<RolesProps> = ({ roles }) => (
+  <Wrap>
+    {roles.map((role) => (
+      <WrapItem key={role.role}>
+        <Tooltip label={role.description}>
+          <MetaTag size="md" fontWeight="normal" backgroundColor="purpleTag">
+            {role.label}
+          </MetaTag>
+        </Tooltip>
+      </WrapItem>
+    ))}
+  </Wrap>
 );
