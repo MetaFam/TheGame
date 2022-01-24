a056db3b31c5a7f48bafd3650da48e9161db3cbf
vidvidvid <weetopol@gmail.com>
2022-01-24 19:12:31 +0100

    quest-roles-demo init
    feat: backend changes

---
+++ hasura/migrations/1642777028373_add_player_roles/up.sql
@@ -0,0 +1,4 @@
+INSERT INTO "public"."PlayerRole" (role, label, description, basic) 
+VALUES ('BRIDGEBUILDING', 'Bridgebuilding', 'All about fostering connections and enabling the flow of people & resources between different organizations.', false),
+('VIDEOMAKING', 'Videomaking', 'As the name suggest - it''s about production of videos no matter the kind; educational, entertaining or both.', false),
+('RAINMAKING', 'Rainmaking', 'Making sure value is recognized & project tokens get bought or liquidity added by long term holders.', false);
