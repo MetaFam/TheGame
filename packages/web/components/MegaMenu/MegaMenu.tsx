aaa5504e0d40a2e29273866984458f1473c3dcbc
vidvidvid <weetopol@gmail.com>
2022-02-04 11:03:08 +0100

    style: megamenu changes
    title color, title font weight, position of modal

--- packages/web/components/MegaMenu/MegaMenu.tsx
+++ packages/web/components/MegaMenu/MegaMenu.tsx
@@ -61,7 +61,7 @@ import { Player } from 'graphql/autogen/types';
 import { useUser, useWeb3 } from 'lib/hooks';
 import { usePSeedBalance } from 'lib/hooks/balances';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { MenuLinkItem, MenuLinkSet, MenuSectionLinks } from 'utils/menuLinks';
 import { getPlayerURL } from 'utils/playerHelpers';
 
@@ -195,129 +195,98 @@ const DesktopMenuItem = ({
   </MenuItem>
 );
 
-const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState({
-    width: 0,
-    height: 0,
-  });
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const handleResize = () => {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      };
-
-      window.addEventListener('resize', handleResize);
-
-      handleResize();
-
-      return () => window.removeEventListener('resize', handleResize);
-    }
-    return undefined;
-  }, []);
-  return windowSize;
-};
-
 // Nav links on desktop -- text and links from utils/menuLinks.ts
-const DesktopNavLinks = () => {
-  const size = useWindowSize();
-
-  return (
-    <Flex
-      justifyContent="center"
-      alignContent="center"
-      display={{ base: 'none', lg: 'flex' }}
-      minW={{ base: 'auto', md: '40%' }}
-      ml={-20}
-    >
-      {MenuSectionLinks.map((section: MenuLinkSet) => (
-        <Menu
-          key={section.label}
-          offset={
-            section.label === 'invest' && size.width > 1463 && size.width < 2200
-              ? [0, -10]
-              : [-56, -10]
-          }
-        >
-          {({ isOpen }) => (
-            <>
-              <MenuButton
-                as={Button}
-                variant="link"
-                minW="fit-content"
-                color="#FFF"
-                fontSize={['md', 'md', 'md', 'lg']}
-                fontWeight={600}
-                textTransform="uppercase"
-                mx={23}
-                _expanded={{ color: '#FFF' }}
-                _focus={{ outline: 'none', border: 'none' }}
-              >
-                {section.label}
-                {isOpen ? (
-                  <ChevronUpIcon color="#FFF" />
-                ) : (
-                  <ChevronDownIcon color="#FFF" />
-                )}
-                <Icon
-                  position="absolute"
-                  left="calc(50% - 21px)"
-                  top={14}
-                  width={6}
-                  borderColor="transparent"
-                  h={isOpen ? 'auto' : 0}
-                  opacity={isOpen ? 1 : 0}
-                  transition="opacity 0.2s"
-                  zIndex="2"
-                >
-                  <path
-                    d="M12 0L24 12C14.6274 12 9.37258 12 0 12L12 0Z"
-                    fill="rgba(42, 31, 71, 0.99)"
-                  />
-                </Icon>
-              </MenuButton>
+const DesktopNavLinks = () => (
+  <Flex
+    justifyContent="center"
+    alignContent="center"
+    display={{ base: 'none', lg: 'flex' }}
+    minW={{ base: 'auto', md: '40%' }}
+    ml={-20}
+  >
+    {MenuSectionLinks.map((section: MenuLinkSet) => (
+      <Menu
+        key={section.label}
+        offset={[0, -8]}
+        preventOverflow
+        placement="top"
+      >
+        {({ isOpen }) => (
+          <>
+            <MenuButton
+              as={Button}
+              variant="link"
+              minW="fit-content"
+              color="#FFF"
+              fontSize={['md', 'md', 'md', 'lg']}
+              fontWeight={700}
+              textTransform="uppercase"
+              mx={23}
+              _expanded={{ color: 'cyan.300' }}
+              _focus={{ outline: 'none', border: 'none' }}
+            >
+              {section.label}
               {isOpen ? (
-                <Box
-                  zIndex="-2"
-                  position="absolute"
-                  minW="100vw"
-                  top="81px"
-                  left="calc(100% - 100vw)"
-                  mx="0"
-                  h="100vh"
-                  bg="linear-gradient(rgba(0,0,0,0.9) 10%, rgba(0,0,0,0) 96%)"
-                  sx={{ filter: 'blur(3rem)' }}
-                />
+                <ChevronUpIcon color="#FFF" />
               ) : (
-                <span />
+                <ChevronDownIcon color="#FFF" />
               )}
-              <MenuList
-                display="grid"
-                gridTemplateColumns={
-                  section.menuItems.length > 3
-                    ? 'repeat(2, 1fr)'
-                    : 'repeat(1, 1fr)'
-                }
-                width={section.menuItems.length > 3 ? '948px' : '474px'}
-                p="2rem"
-                boxShadow="dark-lg"
-                bg="linear-gradient(180deg, rgba(42, 31, 71, 0.9) 6.18%, rgba(17, 3, 32, 0.86) 140%)"
-                borderRadius="0.618vmax"
-                border="0"
+              <Icon
+                position="absolute"
+                left="calc(50% - 21px)"
+                top={14}
+                width={6}
+                borderColor="transparent"
+                h={isOpen ? 'auto' : 0}
+                opacity={isOpen ? 1 : 0}
+                transition="opacity 0.2s"
+                zIndex="2"
               >
-                {section.menuItems.map((item: MenuLinkItem) => (
-                  <DesktopMenuItem {...item} key={item.title} />
-                ))}
-              </MenuList>
-            </>
-          )}
-        </Menu>
-      ))}
-    </Flex>
-  );
-};
+                <path
+                  d="M12 0L24 12C14.6274 12 9.37258 12 0 12L12 0Z"
+                  fill="rgba(42, 31, 71, 0.99)"
+                />
+              </Icon>
+            </MenuButton>
+            {isOpen ? (
+              <Box
+                zIndex="-2"
+                position="absolute"
+                minW="100vw"
+                top="81px"
+                left="calc(100% - 100vw)"
+                mx="0"
+                h="100vh"
+                bg="linear-gradient(rgba(0,0,0,0.9) 10%, rgba(0,0,0,0) 96%)"
+                sx={{ filter: 'blur(3rem)' }}
+              />
+            ) : (
+              <span />
+            )}
+            <MenuList
+              display="grid"
+              gridTemplateColumns={
+                section.menuItems.length > 3
+                  ? 'repeat(2, 1fr)'
+                  : 'repeat(1, 1fr)'
+              }
+              width={section.menuItems.length > 3 ? '948px' : '474px'}
+              p="2rem"
+              boxShadow="dark-lg"
+              bg="linear-gradient(180deg, rgba(42, 31, 71, 0.9) 6.18%, rgba(17, 3, 32, 0.86) 140%)"
+              borderRadius="0.618vmax"
+              border="0"
+            >
+              {section.menuItems.map((item: MenuLinkItem) => (
+                <DesktopMenuItem {...item} key={item.title} />
+              ))}
+            </MenuList>
+          </>
+        )}
+      </Menu>
+    ))}
+  </Flex>
+);
 
 // Search -- not working yet
 // const Search = () => (
