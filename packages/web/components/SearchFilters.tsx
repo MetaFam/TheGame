8f29987c9f9bb90085c18fa9f5559136baba8227
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2022-02-14 01:51:33 +0530

    feat: search filters for search page added

---
+++ packages/web/components/SearchFilters.tsx
@@ -0,0 +1,81 @@
+import { Box, Text, VStack, Wrap } from '@metafam/ds';
+// import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import { GlobalFilters } from '../utils/GlobalSearch';
+
+const FilterItems = [
+  {
+    name: GlobalFilters.PLAYERS,
+    baseUrl: '/search/players',
+  },
+  {
+    name: GlobalFilters.GUILDS,
+    baseUrl: '/search/guilds',
+  },
+  {
+    name: GlobalFilters.PATRONS,
+    baseUrl: '/search/patrons',
+  },
+];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const SearchFilters: any = ({
+  activeFilter,
+  search,
+}: {
+  activeFilter: string;
+  search: string;
+}) => {
+  const router = useRouter();
+
+  return (
+    <VStack
+      w="100%"
+      spacing={{ base: '4', md: '8' }}
+      pb={{ base: '16', lg: '0' }}
+    >
+      <Text fontSize="lg" fontWeight="bold" as="div" mx="auto">
+        Search Results for "{search}"
+      </Text>
+      <Wrap
+        transition="all 0.25s"
+        py="2"
+        style={{ backdropFilter: 'blur(7px)' }}
+        position="sticky"
+        top="-1px"
+        borderTop="1px solid transparent"
+        zIndex="1"
+        justify="center"
+        w={'100%'}
+        maxW={'79rem'}
+        //   bg={isSticky ? 'purpleTag70' : 'whiteAlpha.200'}
+        px={'1.5rem'}
+        borderRadius={'6px'}
+      >
+        {FilterItems.map((item) => (
+          <Box
+            justify="center"
+            p="4"
+            borderBottomWidth={activeFilter === item.name ? '1px' : '0px'}
+            borderBottomColor={
+              activeFilter === item.name ? 'cyanText' : undefined
+            }
+            onClick={() => router.push(`${item.baseUrl}?q=${search}`)}
+          >
+            <Text
+              cursor="pointer"
+              color={activeFilter === item.name ? 'cyanText' : 'gray.400'}
+              fontSize="xl"
+            >
+              {item.name}
+            </Text>
+          </Box>
+        ))}
+      </Wrap>
+    </VStack>
+  );
+};
+
+export default SearchFilters;
