bd8f89e3f7379822995fb46e013f0d9cd0d24b6b
vidvidvid <weetopol@gmail.com>
2022-02-18 12:33:44 +0100

    feat: add WTF is XP section, fix descriptions

--- packages/web/components/Seeds/cardsConfig.tsx
+++ packages/web/components/Seeds/cardsConfig.tsx
@@ -6,20 +6,21 @@ import {
   JoinTheFund,
   PlantingAndWatering,
   UsefulnessOfSeedsModal,
+  WTFisXP,
 } from './modals';
 
 export const cardsConfig = [
+  {
+    title: 'WTF is XP?',
+    description:
+      'XP is a unit of contribution to MetaGame, measured by SourceCred.',
+    Content: WTFisXP,
+  },
   {
     title: 'Usefulness of Seeds',
     description:
       'A reward for those actively contributing to the creation of MetaGame - or much more?',
     Content: UsefulnessOfSeedsModal,
-  },
-  {
-    title: 'BUYING & SELLING Seeds',
-    description:
-      'Looking to buy some Seeds? Want to sell some Seeds? Just follow these few simple steps',
-    Content: BuyingAndSellingModal,
   },
   {
     title: 'BECOME A PATRON',
@@ -28,22 +29,28 @@ export const cardsConfig = [
     Content: BecomeAPatron,
   },
   {
+    title: 'BUYING & SELLING Seeds',
+    description:
+      'Looking to buy some Seeds? Want to sell some Seeds? Just follow these few simple steps',
+    Content: BuyingAndSellingModal,
+  },
+  {
     title: 'PLANTING & WATERING SEEDS',
     description:
       'Being a good player means planting Seeds & being a good patron means watering them.',
     Content: PlantingAndWatering,
   },
   {
-    title: 'GET RANKED!',
-    description:
-      'Leaderboards are a thing! The higher you reach, the more fame, glory and perks await you!',
-    Content: GetRankedModal,
-  },
-  {
     title: 'JOIN THE SEED FUND',
     description:
       'Don’t feel like watering Seeds yourself? Worry not & let us carry the buckets 🙂',
     Content: JoinTheFund,
+  },
+  {
+    title: 'GET RANKED!',
+    description:
+      'Leaderboards are a thing! The higher you reach, the more fame, glory and perks await you!',
+    Content: GetRankedModal,
   },
   {
     title: 'FAQ',
