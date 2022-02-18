273fee55430d62c6ea0f4b339d0da7bbb43cad75
Alec LaLonde <alec@convergencelabs.com>
2022-02-18 12:41:47 -0700

    Added chain icon to DAO listings

--- packages/web/components/Player/PlayerGuild.tsx
+++ packages/web/components/Player/PlayerGuild.tsx
@@ -12,27 +12,45 @@ export const LinkGuild: React.FC<LinkGuildProps> = ({
   children,
 }) => {
   if (guildname != null) {
-    return (
-      <Link
-        role="group"
-        _hover={{ textDecoration: 'none' }}
-        href={`/guild/${guildname}`}
-      >
-        {children}
-      </Link>
-    );
+    return <InternalGuildLink guildName={guildname} children={children} />;
   }
   if (daoUrl != null) {
-    return (
-      <Link
-        role="group"
-        _hover={{ textDecoration: 'none' }}
-        href={daoUrl}
-        isExternal
-      >
-        {children}
-      </Link>
-    );
+    return <DaoHausLink daoUrl={daoUrl} children={children} />;
   }
   return <>{children}</>;
 };
+
+type InternalGuildLinkProps = {
+  guildName: string;
+};
+
+export const InternalGuildLink: React.FC<InternalGuildLinkProps> = ({
+  guildName,
+  children,
+}) => (
+  <Link
+    role="group"
+    _hover={{ textDecoration: 'none' }}
+    href={`/guild/${guildName}`}
+  >
+    {children}
+  </Link>
+);
+
+type DaoHausLinkProps = {
+  daoUrl: string | null;
+};
+
+export const DaoHausLink: React.FC<DaoHausLinkProps> = ({ daoUrl, children }) =>
+  daoUrl != null ? (
+    <Link
+      role="group"
+      _hover={{ textDecoration: 'none' }}
+      href={daoUrl}
+      isExternal
+    >
+      {children}
+    </Link>
+  ) : (
+    <>{children}</>
+  );
