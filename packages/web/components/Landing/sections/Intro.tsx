17f47df097f7e974c8e6b5f919f7dc26019eba7d
luxumbra <me@luxumbra.dev>
2022-02-20 02:38:01 +0000

    linting issues fixed

--- packages/web/components/Landing/sections/Intro.tsx
+++ packages/web/components/Landing/sections/Intro.tsx
@@ -1,5 +1,4 @@
 import {
-  AspectRatio,
   Box,
   BoxedNextImage,
   Button,
@@ -10,13 +9,13 @@ import {
   Stack,
   Text,
 } from '@metafam/ds';
-import { animated, useSpring } from '@react-spring/web';
+import { animated } from '@react-spring/web';
 import BackgroundImage from 'assets/landing/sections/section-1.jpg';
 import LogoNew from 'assets/logo-new.png';
 import { useOnScreen } from 'lib/hooks/useOnScreen';
 import { useRouter } from 'next/router';
-import { forwardRef, useEffect, useRef } from 'react';
-import { BsArrowDown, BsArrowRight } from 'react-icons/bs';
+import { useRef } from 'react';
+import { BsArrowDown } from 'react-icons/bs';
 
 const gradientShift = keyframes`
   0% {
@@ -30,119 +29,114 @@ const gradientShift = keyframes`
   }
 `;
 
-export const Intro: React.FC = () => {
-  const { push } = useRouter();
-
-  return (
-    <Container
-      id="section-1"
-      pos="relative"
-      height="100vh"
-      maxH="100vh"
-      maxW="full"
-      // px={8}
-      sx={{
-        h2: {
-          '& > .gradient': {
-            background:
-              'conic-gradient(from 92.2deg at 53.45% 74.83%, #8EBBFF 0deg, #DE3FFF 88.12deg, #79F8FB 105deg, #7C56FF 165deg, #FF61E6 251.25deg, #927CFF 286.87deg, #76EBF2 326.25deg, #8EBBFF 360deg)',
-            backgroundPosition: '-254%',
-            backgroundSize: '133%',
-            backgroundClip: 'text',
-            textFillColor: 'transparent',
-            WebkitTextFillColor: 'transparent',
-            transition: 'background 0.3s ease',
-          },
-          '&:hover': {
-            '.gradient': {
-              animation: `${gradientShift} 2s infinite`,
-            },
+export const Intro: React.FC = () => (
+  <Container
+    id="section-1"
+    pos="relative"
+    height="100vh"
+    maxH="100vh"
+    maxW="full"
+    // px={8}
+    sx={{
+      h2: {
+        '& > .gradient': {
+          background:
+            'conic-gradient(from 92.2deg at 53.45% 74.83%, #8EBBFF 0deg, #DE3FFF 88.12deg, #79F8FB 105deg, #7C56FF 165deg, #FF61E6 251.25deg, #927CFF 286.87deg, #76EBF2 326.25deg, #8EBBFF 360deg)',
+          backgroundPosition: '-254%',
+          backgroundSize: '133%',
+          backgroundClip: 'text',
+          textFillColor: 'transparent',
+          WebkitTextFillColor: 'transparent',
+          transition: 'background 0.3s ease',
+        },
+        '&:hover': {
+          '.gradient': {
+            animation: `${gradientShift} 2s infinite`,
           },
         },
-        '.gradient': {
+      },
+      '.gradient': {
+        background: 'linear-gradient(90deg, #FF61E6 -29.22%, #7C56FF 107.53%)',
+        backgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+      },
+      '.border-grad': {
+        border: '1px double transparent',
+        background: 'transparent',
+        backgroundImage:
+          'linear-gradient(#1B0D2A, #1B0D2A), radial-gradient(circle at top left, #FF61E6 -29.22%, #7C56FF 107.53%)',
+        backgroundClip: 'padding-box, border-box',
+        backgroundOrigin: 'border-box',
+        WebkitBackgroundOrigin: 'border-box',
+        boxSizing: 'border-box',
+        '& > span': {
           background:
             'linear-gradient(90deg, #FF61E6 -29.22%, #7C56FF 107.53%)',
           backgroundClip: 'text',
           WebkitTextFillColor: 'transparent',
+          transition: 'all 0.3s ease',
         },
-        '.border-grad': {
-          border: '1px double transparent',
-          background: 'transparent',
+        '&:hover': {
           backgroundImage:
             'linear-gradient(#1B0D2A, #1B0D2A), radial-gradient(circle at top left, #FF61E6 -29.22%, #7C56FF 107.53%)',
-          backgroundClip: 'padding-box, border-box',
-          backgroundOrigin: 'border-box',
-          WebkitBackgroundOrigin: 'border-box',
-          boxSizing: 'border-box',
+          backgroundSize: '130%',
           '& > span': {
             background:
-              'linear-gradient(90deg, #FF61E6 -29.22%, #7C56FF 107.53%)',
+              'linear-gradient(-90deg, #FF61E6 -29.22%, #7C56FF 107.53%)',
             backgroundClip: 'text',
             WebkitTextFillColor: 'transparent',
-            transition: 'all 0.3s ease',
-          },
-          '&:hover': {
-            backgroundImage:
-              'linear-gradient(#1B0D2A, #1B0D2A), radial-gradient(circle at top left, #FF61E6 -29.22%, #7C56FF 107.53%)',
-            backgroundSize: '130%',
-            '& > span': {
-              background:
-                'linear-gradient(-90deg, #FF61E6 -29.22%, #7C56FF 107.53%)',
-              backgroundClip: 'text',
-              WebkitTextFillColor: 'transparent',
-            },
           },
         },
-      }}
+      },
+    }}
+  >
+    <Box
+      pos="absolute"
+      top={0}
+      left={0}
+      backgroundImage={`url(${BackgroundImage})`}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      height="100%"
+      width="100%"
+      zIndex={0}
     >
-      <Box
-        pos="absolute"
-        top={0}
-        left={0}
-        backgroundImage={`url(${BackgroundImage})`}
-        bgPosition="center"
-        bgRepeat="no-repeat"
-        bgSize="cover"
-        height="100%"
-        width="100%"
-        zIndex={0}
-      >
-        {/* <AspectRatio ratio={9 / 16}>
+      {/* <AspectRatio ratio={9 / 16}>
           <video src={IntroVideo} muted loop autoplay></video>
         </AspectRatio> */}
-      </Box>
-      <Container
-        d="flex"
-        maxW={{ base: '100%', md: '7xl', '2xl': '8xl' }}
-        height="100%"
-        alignItems="flex-end"
-        justifyContent="center"
-      >
-        <AnimatedIntroHero />
-      </Container>
+    </Box>
+    <Container
+      d="flex"
+      maxW={{ base: '100%', md: '7xl', '2xl': '8xl' }}
+      height="100%"
+      alignItems="flex-end"
+      justifyContent="center"
+    >
+      <AnimatedIntroHero />
     </Container>
-  );
-};
+  </Container>
+);
 
 // type IntroHeroProps = {
 //   isDisplayed: boolean;
 // }
 
-export const IntroHero = () => {
+export const IntroHero: React.FC = () => {
   const { push } = useRouter();
   const heroRef = useRef<HTMLDivElement>(null);
   const onScreen = useOnScreen(heroRef, '300px');
-  const heroProps = useSpring({
-    opacity: onScreen ? 1 : 0,
-  });
+  // const heroProps = useSpring({
+  //   opacity: onScreen ? 1 : 0,
+  // });
 
-  useEffect(() => {
-    if (onScreen) {
-      console.log('hero onscreen');
-    } else {
-      console.log('hero offscreen');
-    }
-  }, [onScreen]);
+  // useEffect(() => {
+  //   if (onScreen) {
+  //     console.log('hero onscreen');
+  //   } else {
+  //     console.log('hero offscreen');
+  //   }
+  // }, [onScreen]);
 
   const handleSectionNav = (sectionId: string) => {
     push(`#${sectionId}`);
