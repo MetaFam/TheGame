c9b777f1f0b551b58e80308266ad86b9ce6e14ec
dan13ram <dan13ram@gmail.com>
2022-03-02 22:01:26 +0530

    feat: join button

---
+++ packages/web/components/JoinButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@metafam/ds';
+import { MetaLink } from 'components/Link';
+
+export const JoinButton: React.FC<{ text: string }> = ({
+  text = 'Join Us',
+}) => (
+  <MetaLink _hover={{}} href="/join">
+    <Button className="border-grad" colorScheme="white" rounded="md" size="lg">
+      {text}
+    </Button>
+  </MetaLink>
+);
