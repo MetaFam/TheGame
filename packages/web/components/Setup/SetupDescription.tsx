396c28ab4ffc3dc571704201001909b7bb74502b
vidvidvid <weetopol@gmail.com>
2022-03-16 21:43:28 +0100

    feat: add description step

--- packages/web/components/Setup/SetupDescription.tsx
+++ packages/web/components/Setup/SetupDescription.tsx
@@ -1,34 +1,37 @@
-import { Flex, Input } from '@metafam/ds';
+import { Flex, Textarea } from '@metafam/ds';
 import React from 'react';
 
 import { ProfileWizardPane } from './ProfileWizardPane';
 import { WizardPaneCallbackProps } from './WizardPane';
 
-export const SetupPronouns: React.FC = () => {
-  const field = 'pronouns';
+export const SetupDescription: React.FC = () => {
+  const field = 'description';
 
   return (
     <ProfileWizardPane
       {...{ field }}
-      title="Pronouns"
-      prompt="Which pronouns do you prefer?"
+      title="Description"
+      prompt="This is where you get to tell the world who you are! What interests you? What are you up to these days? What are your goals & aspirations?? Why are you here???"
     >
       {({ register, errored }: WizardPaneCallbackProps) => {
         const { ref: registerRef, ...props } = register(field, {
           maxLength: {
-            value: 150,
-            message: 'Maximum length is 150 characters.',
+            value: 420,
+            message: 'Maximum length is 420 characters.',
           },
         });
 
         return (
           <Flex justify="center" mt={5}>
-            <Input
-              background="dark"
-              placeholder="they / them"
-              w="auto"
+            <Textarea
+              maxW="36rem"
+              placeholder="Come on now, don't be shy 🙃"
+              h="10em"
+              color="white"
               _focus={errored ? { borderColor: 'red' } : undefined}
+              bg="dark"
               ref={(ref) => {
+                console.log('ref', ref);
                 ref?.focus();
                 registerRef(ref);
               }}
---
+++ packages/web/pages/profile/setup/description.tsx
@@ -0,0 +1,23 @@
+import { SetupDescription } from 'components/Setup/SetupDescription';
+import { SetupProfile } from 'components/Setup/SetupProfile';
+import { SetupContextProvider } from 'contexts/SetupContext';
+import { InferGetStaticPropsType } from 'next';
+import React from 'react';
+
+export const getStaticProps = async () => ({
+  props: {
+    hideTopMenu: true,
+  },
+});
+
+export type DefaultSetupProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const UsernameSetup: React.FC<DefaultSetupProps> = () => (
+  <SetupContextProvider>
+    <SetupProfile>
+      <SetupDescription />
+    </SetupProfile>
+  </SetupContextProvider>
+);
+
+export default UsernameSetup;
