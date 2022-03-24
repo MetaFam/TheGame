2d0ca1ee5bb87b5d12837fd468853f942619a2e3
Alec LaLonde <alec@convergencelabs.com>
2022-03-24 09:17:19 -0600

    Fixed new PlayerRole migration

--- hasura/migrations/1647440669863_update_PlayerRole/up.sql
+++ hasura/migrations/1647440669863_update_PlayerRole/up.sql
@@ -4,8 +4,4 @@ UPDATE "PlayerRole" SET description = 'Designers are here to conjure up  epic in
 UPDATE "PlayerRole" SET description = 'Worried about wellbeing, making sure glasses are full, questions answers & players content.' WHERE role = 'INNKEEPER';
 UPDATE "PlayerRole" SET description = 'Shillers are here to help us communicate with the wider world and convert the masses.' WHERE role = 'SHILLER';
 UPDATE "PlayerRole" SET description = 'From scribing notes to writing specs & memes that could convert a stoic; they have a way with words.' WHERE role = 'WRITER';
-
-INSERT INTO "public"."PlayerRole" (role, label, description) 
-VALUES ('FUNDSTER', 'Fundster', 'Simply too busy to help people build, they are here to buy tokens & donate instead.');
-
-DELETE FROM "PlayerRole" WHERE role='PATRON';
+UPDATE "PlayerRole" SET description = 'Simply too busy to help people build, they are here to buy tokens & donate instead.', label = 'Fundster' WHERE role = 'PATRON';
