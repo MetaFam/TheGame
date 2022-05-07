c1864cf53aa2eea49a77d363d5b448bb9c01ba5a
Alec LaLonde <alec@convergencelabs.com>
2022-05-07 14:19:14 -0600

    Test relative import

--- packages/backend/src/handlers/remote-schemas/resolvers/discord/resolver.ts
+++ packages/backend/src/handlers/remote-schemas/resolvers/discord/resolver.ts
@@ -1,62 +1,59 @@
 import { createDiscordClient } from '@metafam/discord-bot';
 import { Role } from 'discord.js';
+import { client } from 'lib/hasuraClient';
 
-import { client } from '../../../../lib/hasuraClient';
 import { QueryResolvers } from '../../autogen/types';
 
-export const getGuildDiscordRoles: QueryResolvers['getGuildDiscordRoles'] = async (
-  _,
-  { guildDiscordId },
-) => {
-  if (!guildDiscordId) return [];
+export const getGuildDiscordRoles: QueryResolvers['getGuildDiscordRoles'] =
+  async (_, { guildDiscordId }) => {
+    if (!guildDiscordId) return [];
 
-  const discordClient = await createDiscordClient();
-  const discordGuild = await discordClient.guilds.fetch(guildDiscordId);
+    const discordClient = await createDiscordClient();
+    const discordGuild = await discordClient.guilds.fetch(guildDiscordId);
 
-  if (discordGuild != null) {
-    await discordGuild.roles.fetch();
-    return discordGuild.roles.cache.map((role: Role) => ({
-      id: role.id,
-      position: role.position,
-      name: role.name,
-    }));
-  }
+    if (discordGuild != null) {
+      await discordGuild.roles.fetch();
+      return discordGuild.roles.cache.map((role: Role) => ({
+        id: role.id,
+        position: role.position,
+        name: role.name,
+      }));
+    }
 
-  return [];
-};
+    return [];
+  };
 
-export const getDiscordServerMemberRoles: QueryResolvers['getDiscordServerMemberRoles'] = async (
-  _,
-  { guildId, playerId },
-) => {
-  const getGuildPlayerResponse = await client.GetGuildPlayerDiscordIds({
-    guildId,
-    playerId,
-  });
-  const guildDiscordId = getGuildPlayerResponse.guild_player[0].Guild.discordId;
-  const playerDiscordId =
-    getGuildPlayerResponse.guild_player[0].Player.discordId;
+export const getDiscordServerMemberRoles: QueryResolvers['getDiscordServerMemberRoles'] =
+  async (_, { guildId, playerId }) => {
+    const getGuildPlayerResponse = await client.GetGuildPlayerDiscordIds({
+      guildId,
+      playerId,
+    });
+    const guildDiscordId =
+      getGuildPlayerResponse.guild_player[0].Guild.discordId;
+    const playerDiscordId =
+      getGuildPlayerResponse.guild_player[0].Player.discordId;
 
-  if (guildDiscordId == null || playerDiscordId == null) return [];
+    if (guildDiscordId == null || playerDiscordId == null) return [];
 
-  const discordClient = await createDiscordClient();
-  const discordGuild = await discordClient.guilds.fetch(guildDiscordId);
+    const discordClient = await createDiscordClient();
+    const discordGuild = await discordClient.guilds.fetch(guildDiscordId);
 
-  if (discordGuild != null) {
-    await discordGuild.members.fetch(playerDiscordId);
-    await discordGuild.roles.fetch();
-    const member = discordGuild.members.cache.get(playerDiscordId);
+    if (discordGuild != null) {
+      await discordGuild.members.fetch(playerDiscordId);
+      await discordGuild.roles.fetch();
+      const member = discordGuild.members.cache.get(playerDiscordId);
 
-    if (member == null) return [];
+      if (member == null) return [];
 
-    // these are returned in descending order by position
-    // (meaning, most significant role is first)
-    return member.roles.cache.map((role: Role) => ({
-      id: role.id,
-      position: role.position,
-      name: role.name,
-    }));
-  }
+      // these are returned in descending order by position
+      // (meaning, most significant role is first)
+      return member.roles.cache.map((role: Role) => ({
+        id: role.id,
+        position: role.position,
+        name: role.name,
+      }));
+    }
 
-  return [];
-};
+    return [];
+  };
