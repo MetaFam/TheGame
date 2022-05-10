fe522aecaef8217ab0456f4c626ae68427b7c1d4
Alec LaLonde <alec@convergencelabs.com>
2022-05-09 21:05:13 -0600

    Use latest eth address rather than the first when migrating XP data

--- packages/utils/src/sourceCredHelpers.ts
+++ packages/utils/src/sourceCredHelpers.ts
@@ -2,13 +2,19 @@ import { ethers } from 'ethers';
 import { SCIdentity, sourcecred as sc } from 'sourcecred';
 
 export const getLatestEthAddress = (identity: SCIdentity): string | null => {
-  const ethAddress = identity.aliases.find((alias) => {
+  const ethAddresses = identity.aliases.filter((alias) => {
     const parts = sc.core.graph.NodeAddress.toParts(alias.address);
-    return parts.indexOf('ethereum') > 0;
-  })?.description;
-
-  if (ethAddress && ethers.utils.isAddress(ethAddress)) {
-    return ethAddress.toLowerCase();
+    return (
+      parts.indexOf('ethereum') > 0 && ethers.utils.isAddress(alias.description)
+    );
+  });
+  if (ethAddresses.length > 0) {
+    // this assumes that the fetched data is in chronological order, which should
+    // be the case for the SC Ledger
+    const latestAddress = ethAddresses.pop();
+    if (latestAddress != null) {
+      return latestAddress?.description.toLowerCase();
+    }
   }
 
   return null;
