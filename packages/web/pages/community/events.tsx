12b1a3d9fcb7147d23813d841c8c7cf24fdbb437
vance4243 <96993195+vance4243@users.noreply.github.com>
2022-05-12 17:37:48 +0200

    Change Events url to our Google Calendar
    Events Button in Community Menu was redirecting to House of DAOs. I created a url which redirects to our Google Calendar for Discord Events. Issue #1251

--- packages/web/pages/community/events.tsx
+++ packages/web/pages/community/events.tsx
@@ -5,7 +5,7 @@ const EventsPage: React.FC = () => (
   <EmbedContainer
     title="MetaGame Events"
     description={descriptions.events}
-    url={`https://wiki.metagame.wtf/docs/great-houses/house-of-daos`}
+    url={`https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&showPrint=0&mode=AGENDA&showTitle=0&showNav=1&showTabs=0&showPrint=0&showCalendars=0&src=nih59ktgafmm64ed4qk6ue8vv4@group.calendar.google.com&color=%23F09300`}
   />
 );
 
