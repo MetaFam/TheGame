7a741bba98fd2a5090df16a9343473bb87ef35b9
Alec LaLonde <alec@convergencelabs.com>
2022-06-10 23:04:57 -0400

    Compute show_discord_announcements flag based on given discord permissions

--- packages/web/components/Guild/GuildJoin.tsx
+++ packages/web/components/Guild/GuildJoin.tsx
@@ -38,7 +38,7 @@ export const GuildJoin: React.FC = () => {
     client_id: Constants.DISCORD_BOT_CLIENT_ID,
     // This will be passed-back and verified after the Discord auth redirect
     state: stateGuid as string,
-    permissions: Constants.JOIN_GUILD_DISCORD_BOT_PERMISSIONS,
+    permissions: `${Constants.JOIN_GUILD_DISCORD_BOT_PERMISSIONS}`,
     redirect_uri: encodeURI(discordOAuthCallbackURL),
     scope: Constants.JOIN_GUILD_DISCORD_OAUTH_SCOPES,
   });
