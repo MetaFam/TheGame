4fe4e707ea924d8e1b70baa619610b19d8f6af20
Alec LaLonde <alec@convergencelabs.com>
2022-07-12 12:21:47 -0600

    Explicitly reset all players' XP at the beginning of a season

--- packages/utils/tests/lib/xpHelpers.test.ts
+++ packages/utils/tests/lib/xpHelpers.test.ts
@@ -1,4 +1,8 @@
-import { getNumWeeksInSeason, getSeasonNum } from '../../src/xpHelpers';
+import {
+  getNumWeeksInSeason,
+  getSeasonNum,
+  isNewSeason,
+} from '../../src/xpHelpers';
 
 describe('getNumWeeksInSeason', () => {
   const currentDate = new Date();
@@ -36,5 +40,20 @@ describe('getSeasonNum', () => {
   it('April 1st 2022 should be season 6', () => {
     const Apr1st = new Date(Date.parse(`2022-04-01T00:01:00+00:00`));
     expect(getSeasonNum(Apr1st)).toBe(6);
+  });
+});
+
+describe('isNewSeason', () => {
+  it('June 20 should not be a new season', () => {
+    const date = new Date(Date.parse(`2022-06-20T00:00:00+00:00`));
+    expect(isNewSeason(date)).toBe(false);
+  });
+  it('June 21 should be a new season', () => {
+    const date = new Date(Date.parse(`2022-06-21T00:01:00+00:00`));
+    expect(isNewSeason(date)).toBe(true);
+  });
+  it('June 22 should not be a new season', () => {
+    const date = new Date(Date.parse(`2022-06-22T00:01:00+00:00`));
+    expect(isNewSeason(date)).toBe(false);
   });
 });
