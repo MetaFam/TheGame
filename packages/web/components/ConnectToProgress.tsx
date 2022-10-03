d401e6d3229806450b13948d117989fcd1e22a33
dan13ram <dan13ram@gmail.com>
2022-10-03 21:05:35 +0530

    fix: centered connect to progress

--- packages/web/components/ConnectToProgress.tsx
+++ packages/web/components/ConnectToProgress.tsx
@@ -38,12 +38,14 @@ export const ConnectToProgress: React.FC<{
   if (connected && !fetching) {
     if (chainId !== '0x1' && showSwitchButton) {
       return (
-        <Stack color="white" spacing={8} w="100%" maxW="30rem">
-          <MetaGameLogo />
-          {header && <MetaHeading>{header}</MetaHeading>}
-          <Text fontSize="md" w="100%" textAlign="center">
-            Please switch to <SwitchNetworkButton chainId="0x1" /> to progress
-          </Text>
+        <Stack w="100%" align="center">
+          <Stack color="white" spacing={8} w="100%" maxW="30rem">
+            <MetaGameLogo />
+            {header && <MetaHeading>{header}</MetaHeading>}
+            <Text fontSize="md" w="100%" textAlign="center">
+              Please switch to <SwitchNetworkButton chainId="0x1" /> to progress
+            </Text>
+          </Stack>
         </Stack>
       );
     }
@@ -51,117 +53,119 @@ export const ConnectToProgress: React.FC<{
   }
 
   return (
-    <Stack color="white" spacing={8} w="100%" maxW="28rem">
-      <MetaGameLogo />
-      {header && <MetaHeading> {header} </MetaHeading>}
-      {showNote && (
-        <Stack spacing={4} align="flex-start" w="100%" px={2}>
-          <Text>First things first, you should make yourself a profile.</Text>
-          <Text>
-            From there, you'll be able to show the world what you're about, what
-            you're working on & more.
-          </Text>
-          <Text>
-            Unlike profiles in the Web2 world, MyMeta profiles reside on a
-            decentralized service; they are controlled & owned by you alone.
-          </Text>
-          <Text>
-            Read more about MyMeta profiles{' '}
-            <Link
-              href="//wiki.metagame.wtf/docs/what-we-do/mymeta"
-              isExternal
-              color="pink.400"
-              textDecor="underline"
-              fontWeight="bold"
-            >
-              here
-            </Link>
-            .
-          </Text>
-        </Stack>
-      )}
-      <Stack w="100%" spacing={4} pt={2}>
+    <Stack w="100%" align="center">
+      <Stack color="white" spacing={8} w="100%" maxW="28rem">
+        <MetaGameLogo />
+        {header && <MetaHeading> {header} </MetaHeading>}
         {showNote && (
-          <Text textAlign="center">
-            Connect your wallet on Ethereum Mainnet to start
-          </Text>
+          <Stack spacing={4} align="flex-start" w="100%" px={2}>
+            <Text>First things first, you should make yourself a profile.</Text>
+            <Text>
+              From there, you'll be able to show the world what you're about,
+              what you're working on & more.
+            </Text>
+            <Text>
+              Unlike profiles in the Web2 world, MyMeta profiles reside on a
+              decentralized service; they are controlled & owned by you alone.
+            </Text>
+            <Text>
+              Read more about MyMeta profiles{' '}
+              <Link
+                href="//wiki.metagame.wtf/docs/what-we-do/mymeta"
+                isExternal
+                color="pink.400"
+                textDecor="underline"
+                fontWeight="bold"
+              >
+                here
+              </Link>
+              .
+            </Text>
+          </Stack>
         )}
-        <Button
-          w="100%"
-          size="lg"
-          textTransform="uppercase"
-          fontWeight="600"
-          onClick={connect}
-          isLoading={connecting || fetching}
-          colorScheme="pink"
-          fontSize="1.25rem"
-        >
-          {showNote ? 'Connect Wallet' : 'Connect to continue'}
-        </Button>
+        <Stack w="100%" spacing={4} pt={2}>
+          {showNote && (
+            <Text textAlign="center">
+              Connect your wallet on Ethereum Mainnet to start
+            </Text>
+          )}
+          <Button
+            w="100%"
+            size="lg"
+            textTransform="uppercase"
+            fontWeight="600"
+            onClick={connect}
+            isLoading={connecting || fetching}
+            colorScheme="pink"
+            fontSize="1.25rem"
+          >
+            {showNote ? 'Connect Wallet' : 'Connect to continue'}
+          </Button>
+          {showNote && (
+            <Link href="/dashboard" _hover={{}} w="100%">
+              <Button
+                w="100%"
+                size="lg"
+                fontSize="1.25rem"
+                fontWeight="600"
+                textTransform="uppercase"
+                colorScheme="blackAlpha"
+                color="whiteAlpha.700"
+              >
+                Explore First
+              </Button>
+            </Link>
+          )}
+        </Stack>
         {showNote && (
-          <Link href="/dashboard" _hover={{}} w="100%">
-            <Button
-              w="100%"
-              size="lg"
-              fontSize="1.25rem"
-              fontWeight="600"
-              textTransform="uppercase"
-              colorScheme="blackAlpha"
-              color="whiteAlpha.700"
+          <>
+            <Text
+              as="button"
+              color="white"
+              textDecor="underline"
+              onClick={toggle}
             >
-              Explore First
-            </Button>
-          </Link>
+              Don't have a wallet yet?
+            </Text>
+            <Collapse in={open}>
+              <Stack
+                spacing={2}
+                w="100%"
+                maxW="30rem"
+                textAlign="center"
+                align="center"
+              >
+                <Text>
+                  With an{' '}
+                  <MetaLink
+                    isExternal
+                    href="https://wiki.metagame.wtf/docs/great-houses/house-of-ethereum"
+                    color="pink.400"
+                    textDecor="underline"
+                  >
+                    Ethereum
+                  </MetaLink>{' '}
+                  wallet you can be your own bank and control your digital
+                  assets & ID. You will need one to play MetaGame as well as
+                  engage in the wider ecosystem.
+                </Text>
+                <Text>
+                  We recommend{' '}
+                  <MetaLink
+                    isExternal
+                    href="https://metamask.io/download"
+                    color="pink.400"
+                    textDecor="underline"
+                  >
+                    <MetaMaskIcon /> MetaMask
+                  </MetaLink>
+                  .
+                </Text>
+              </Stack>
+            </Collapse>
+          </>
         )}
       </Stack>
-      {showNote && (
-        <>
-          <Text
-            as="button"
-            color="white"
-            textDecor="underline"
-            onClick={toggle}
-          >
-            Don't have a wallet yet?
-          </Text>
-          <Collapse in={open}>
-            <Stack
-              spacing={2}
-              w="100%"
-              maxW="30rem"
-              textAlign="center"
-              align="center"
-            >
-              <Text>
-                With an{' '}
-                <MetaLink
-                  isExternal
-                  href="https://wiki.metagame.wtf/docs/great-houses/house-of-ethereum"
-                  color="pink.400"
-                  textDecor="underline"
-                >
-                  Ethereum
-                </MetaLink>{' '}
-                wallet you can be your own bank and control your digital assets
-                & ID. You will need one to play MetaGame as well as engage in
-                the wider ecosystem.
-              </Text>
-              <Text>
-                We recommend{' '}
-                <MetaLink
-                  isExternal
-                  href="https://metamask.io/download"
-                  color="pink.400"
-                  textDecor="underline"
-                >
-                  <MetaMaskIcon /> MetaMask
-                </MetaLink>
-                .
-              </Text>
-            </Stack>
-          </Collapse>
-        </>
-      )}
     </Stack>
   );
 };
