6180c52c879f3fa424612236635cae03384ec4e8
W.J. “dysbulic” H <dys@dhappy.org>
2022-10-02 20:29:24 -0400

    adding link to block creation quest to "Add Block" modal 🃏

--- packages/design-system/src/MetaButton.tsx
+++ packages/design-system/src/MetaButton.tsx
@@ -5,18 +5,25 @@ type LinkProps = { href?: string; target?: '_blank' };
 type RefProps = { ref?: React.Ref<HTMLButtonElement> };
 
 export const MetaButton: React.FC<ButtonProps & LinkProps & RefProps> =
-  React.forwardRef<HTMLButtonElement>(({ children, ...props }, ref) => (
-    <Button
-      colorScheme="purple"
-      textTransform="uppercase"
-      px={12}
-      letterSpacing="0.1em"
-      size="lg"
-      fontSize="sm"
-      bg="purple.400"
-      color="white"
-      {...{ ref, ...props }}
-    >
-      {children}
-    </Button>
-  ));
+  React.forwardRef<HTMLButtonElement>(({ children, ...props }, ref) => {
+    const args = props as LinkProps & ButtonProps;
+    if (args.href) {
+      args.as = 'a';
+    }
+
+    return (
+      <Button
+        colorScheme="purple"
+        textTransform="uppercase"
+        px={12}
+        letterSpacing="0.1em"
+        size="lg"
+        fontSize="sm"
+        bg="purple.400"
+        color="white"
+        {...{ ref, ...args }}
+      >
+        {children}
+      </Button>
+    );
+  });
---
+++ packages/design-system/src/icons/ExternalLinkIcon.tsx
@@ -0,0 +1,4 @@
+import { chakra } from '@chakra-ui/react';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+
+export const ExternalLinkIcon = chakra(FaExternalLinkAlt);
