e892d18be681db172785cfad713fa8106f8d1676
W.J. “dysbulic” H <dys@dhappy.org>
2022-10-02 20:59:58 -0400

    switching to Chakra icon instead of FontAwesome 🎃

--- packages/design-system/src/icons/index.ts
+++ packages/design-system/src/icons/index.ts
@@ -3,12 +3,10 @@ export { ChainIcon } from './ChainIcon';
 export { Dashboard } from './Dashboard';
 export { DropDownIcon } from './DropDownIcon';
 export { EthereumIcon } from './EthereumIcon';
+export { GnosisIcon } from './GnosisIcon';
 export { Icon3box } from './Icon3box';
 export { LogOut } from './LogOut';
 export { MeetWithWalletIcon } from './MeetWithWalletIcon';
 export { MetaMaskIcon } from './MetaMaskIcon';
 export { PolygonIcon } from './PolygonIcon';
 export { Profile } from './Profile';
-// export { XDaiIcon } from './XDaiIcon'; // Replaced by Gnosis icon
-export * from './ExternalLinkIcon';
-export { GnosisIcon } from './GnosisIcon';
