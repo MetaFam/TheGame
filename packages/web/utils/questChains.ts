ac5efbd849b0666a29be43f8f120df195902b4b1
vidvidvid <weetopol@gmail.com>
2022-10-03 09:30:38 +0200

    fix address for bridgebuilders quests

--- packages/web/utils/questChains.ts
+++ packages/web/utils/questChains.ts
@@ -10,10 +10,9 @@ export const QUESTS = {
     address: '0xea512722cc047bfdbe90a1a2750491e89a818ca7',
     graphHealthEndpoint: 'https://api.thegraph.com/index-node/graphql',
   },
-  // need to change
   BRIDGEBUILDERS: {
     chainId: '0x89',
-    address: '0xea512722cc047bfdbe90a1a2750491e89a818ca7',
+    address: '0xf7fbc471cbae68bf3833ff820c926ffe3c5bf0f7',
     graphHealthEndpoint: 'https://api.thegraph.com/index-node/graphql',
   },
   // need to change
