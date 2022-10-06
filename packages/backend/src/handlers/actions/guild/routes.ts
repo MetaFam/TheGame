30e2e4b3a4c93b0c8e2ef29d647230441b531de5
dan13ram <dan13ram@gmail.com>
2022-10-06 20:49:17 +0530

    added mutation to update guild profileLayout

--- packages/backend/src/handlers/actions/guild/routes.ts
+++ packages/backend/src/handlers/actions/guild/routes.ts
@@ -3,8 +3,10 @@ import express from 'express';
 import { asyncHandlerWrapper } from '../../../lib/apiHelpers';
 import { handleOAuthCallback } from './discord/oauthHandler';
 import { saveGuildHandler } from './saveGuildHandler';
+import { saveGuildLayoutHandler } from './saveGuildLayoutHandler';
 
 export const guildRoutes = express.Router();
 
 guildRoutes.post('/discord/auth', asyncHandlerWrapper(handleOAuthCallback));
 guildRoutes.post('/save', asyncHandlerWrapper(saveGuildHandler));
+guildRoutes.post('/saveLayout', asyncHandlerWrapper(saveGuildLayoutHandler));
