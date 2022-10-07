4c7acab0b7f81a9b27657515911f7a18cd104b22
HHH-GH <hayden@opie-etc.com>
2022-10-07 14:59:57 +0800

    Changed to a Typeform link for applications
    No need to check if logged in, no need to use DiscordAuthParams etc

--- packages/web/components/Guild/Join/DecideJoin.tsx
+++ packages/web/components/Guild/Join/DecideJoin.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Center,
   Container,
   Flex,
   Heading,
@@ -7,50 +8,10 @@ import {
   MetaButton,
   Text,
 } from '@metafam/ds';
-import { Constants, generateUUID } from '@metafam/utils';
 import GuildsImg from 'assets/guilds-sun_800x800.png';
-import { MetaLink } from 'components/Link';
-/* 
-    For Getting the logged in state 
-*/
-import { CONFIG } from 'config';
-import { useUser } from 'lib/hooks';
-import { get, set } from 'lib/store';
-import React, { useEffect, useState } from 'react';
-
-export const discordAuthStateGuidKey = 'metagame-add-guild';
 
 export const DecideJoin: React.FC = () => {
-  /* 
-        Get the logged in state 
-    */
-  const { user } = useUser();
-  const discordOAuthCallbackURL = `${CONFIG.publicURL}/${Constants.DISCORD_OAUTH_CALLBACK_PATH}`;
-
-  const [stateGuid, setStateGuid] = useState<string>();
-
-  useEffect(() => {
-    let guid = get(discordAuthStateGuidKey);
-    if (guid == null) {
-      guid = generateUUID();
-      set(discordAuthStateGuidKey, guid);
-    }
-    setStateGuid(guid);
-  }, [setStateGuid]);
-
-  const discordAuthParams = new URLSearchParams({
-    response_type: 'code',
-    client_id: Constants.DISCORD_BOT_CLIENT_ID,
-    // This will be passed-back and verified after the Discord auth redirect
-    state: stateGuid as string,
-    permissions: `${Constants.JOIN_GUILD_DISCORD_BOT_PERMISSIONS}`,
-    redirect_uri: encodeURI(discordOAuthCallbackURL),
-    scope: Constants.JOIN_GUILD_DISCORD_OAUTH_SCOPES,
-  });
-
-  const discordAuthURL = `${
-    CONFIG.discordApiBaseUrl
-  }/oauth2/authorize?${discordAuthParams.toString()}`;
+  const guildApplicationLink = 'https://form.typeform.com/to/V5YNcdMQ';
 
   return (
     <Container
@@ -90,53 +51,33 @@ export const DecideJoin: React.FC = () => {
           />
 
           <Box ml={{ base: 0, md: 16 }} flex="auto">
-            {/* 
-                If they have connected their wallet, they get the sign up button
-                If they have not connected their wallet, they get a prompt to sign up
-              */}
-            {stateGuid?.length && user ? (
-              <>
-                <Text as="p" mb={6}>
-                  Ready to join the Decentralized Factory &amp; become one of
-                  the Founding Guilds of MetaGame? Apply now 👇
-                </Text>
-
-                <MetaButton
-                  as="a"
-                  bg="#E839B7"
-                  borderRadius={0}
-                  color="white"
-                  href={discordAuthURL}
-                  mb={8}
-                  minW="10rem"
-                  px={6}
-                  textDecoration="underline"
-                  textTransform="uppercase"
-                  _hover={{
-                    backgroundColor: 'rgba(232, 57, 183, 0.6)',
-                  }}
-                  _active={{
-                    backgroundColor: 'rgba(232, 57, 183, 0.6)',
-                    transform: 'scale(0.8)',
-                  }}
-                >
-                  Apply
-                </MetaButton>
-              </>
-            ) : (
-              <Text as="p" fontStyle="italic" mb={4}>
-                Please log in or create a player profile by pressing the
-                "Connect" button to start the guild application process.
-              </Text>
-            )}
-
-            <Text mb={4}>
-              To apply, your guild must have a{' '}
-              <MetaLink isExternal href="https://discord.com/">
-                Discord
-              </MetaLink>{' '}
-              server.
+            <Text as="p" mb={6} textAlign="center">
+              Ready to join the Decentralized Factory &amp; become one of the
+              Founding Guilds of MetaGame? Apply now 👇
             </Text>
+
+            <Center>
+              <MetaButton
+                as="a"
+                bg="#E839B7"
+                borderRadius={0}
+                color="white"
+                href={guildApplicationLink}
+                mb={4}
+                minW="10rem"
+                px={6}
+                textTransform="uppercase"
+                _hover={{
+                  backgroundColor: 'rgba(232, 57, 183, 0.6)',
+                }}
+                _active={{
+                  backgroundColor: 'rgba(232, 57, 183, 0.6)',
+                  transform: 'scale(0.8)',
+                }}
+              >
+                Apply
+              </MetaButton>
+            </Center>
           </Box>
         </Flex>
       </Container>
