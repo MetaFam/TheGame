4a0704007423769091e13fb73293f3d747fbbaf6
luxumbra <me@luxumbra.dev>
2022-10-08 01:39:16 +0100

    WIP on adding images to the Newsletter updates - removed left pad on `TabList` to make it consistent with other blocks

--- packages/web/components/Dashboard/LatestContentTabs/Read.tsx
+++ packages/web/components/Dashboard/LatestContentTabs/Read.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Link, LoadingState, Text } from '@metafam/ds';
+import { Box, Heading, Image, Link, LoadingState, Text } from '@metafam/ds';
 import React from 'react';
 import useSWR from 'swr';
 
@@ -6,37 +6,68 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const Read: React.FC = () => {
   const { data, error } = useSWR('/api/feed', fetcher);
-
   return (
-    <Box px={2}>
+    <Box px={2} pl={0}>
       {data && data.response ? (
         data.response.items?.map(
-          (item: { title: string; description: string; link: string }) => (
+          (item: {
+            title: string;
+            description: string;
+            link: string;
+            enclosures: Record<string, string>[];
+          }) => (
             <Box
               key={item.title}
+              position="relative"
               mt={4}
-              p={6}
               backgroundColor="blackAlpha.500"
               borderRadius="md"
+              overflow="hidden"
+              sx={{
+                '&::after': {
+                  content: '""',
+                  position: 'absolute',
+                  inset: 0,
+                  backgroundColor: 'blackAlpha.700',
+                  zIndex: 1,
+                },
+              }}
             >
-              <Heading size="xs" color="white" pb={4}>
-                {item.title}
-              </Heading>
-              <Text dangerouslySetInnerHTML={{ __html: item.description }} />
-              <Link
-                href={item.link}
-                _hover={{ bg: 'none', textDecoration: 'none' }}
-                _focus={{ outline: 'none' }}
-              >
-                <Text
-                  cursor="pointer"
-                  color="blueLight"
-                  py={1}
-                  fontWeight="bold"
+              <Box position="relative" zIndex={5} p={6}>
+                <Heading size="xs" color="white" fontWeight="normal" pb={4}>
+                  {item.title}
+                </Heading>
+                <Text dangerouslySetInnerHTML={{ __html: item.description }} />
+                <Link
+                  href={item.link}
+                  _hover={{ bg: 'none', textDecoration: 'none' }}
+                  _focus={{ outline: 'none' }}
                 >
-                  Read Here
-                </Text>
-              </Link>
+                  <Text
+                    cursor="pointer"
+                    color="blueLight"
+                    py={1}
+                    fontWeight="bold"
+                  >
+                    Read Here
+                  </Text>
+                </Link>
+              </Box>
+              {item.enclosures &&
+                item.enclosures.length > 0 &&
+                item.enclosures[0].type === 'image/jpeg' && (
+                  <Box
+                    position="absolute"
+                    inset={0}
+                    width="full"
+                    height="full"
+                    bg="landingDarkGlass"
+                    bgImg={`url(${item.enclosures[0].url})`}
+                    bgSize="cover"
+                    opacity={0.2}
+                    zIndex={0}
+                  />
+                )}
             </Box>
           ),
         )
