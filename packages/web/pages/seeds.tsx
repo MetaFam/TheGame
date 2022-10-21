09860b9a70d473bff0a11003fcf68bd7fe6e96d3
HHH-GH <hayden@opie-etc.com>
2022-10-21 20:40:38 +0800

    Update the page layout
    Use the new Guild/Patron Join design as a model for the top half of the page
    
    Add the new seed-diagram.png in assets
    
    Make the cards look more like the new card design (background blur, width, etc

--- packages/web/pages/seeds.tsx
+++ packages/web/pages/seeds.tsx
@@ -2,7 +2,7 @@ import {
   ArrowUpIcon,
   Box,
   Button,
-  Flex,
+  Container,
   Grid,
   Heading,
   Image,
@@ -12,8 +12,10 @@ import {
   UnorderedList,
   VStack,
 } from '@metafam/ds';
+import BackgroundImage from 'assets/main-background.jpg';
 import Octopus from 'assets/octopus.png';
-import SEEDsFlowChart from 'assets/seeds-flowchart.png';
+import SEEDsFlowChart from 'assets/seed-diagram.png';
+import SeedsImg from 'assets/seed-icon.svg';
 import { PageContainer } from 'components/Container';
 import { Card } from 'components/Seeds/Card';
 import { cardsConfig } from 'components/Seeds/cardsConfig';
@@ -35,57 +37,119 @@ const SEEDsPage: React.FC = () => {
 
   return (
     <PageContainer py={8} px={[6, 6, 20, 24]}>
-      <VStack maxW="7xl" w="100%" spacing={{ base: 4, md: 8 }}>
+      <VStack maxW="6xl" w="100%" spacing={{ base: 4, md: 20 }}>
         <HeadComponent
           title="MetaGame Seeds Page"
           description="Seeds are MetaGame’s labor token. People contribute towards creation of MetaGame, meanwhile generating XP &amp; getting paid out in Seeds proportional to their gained XP. Find out more."
           url="https://metagame.wtf/seeds"
         />
-        <Grid templateColumns={['auto', 'auto', 'auto', '1fr 1fr']} gap={6}>
-          <Flex fontSize="md" flexDirection="column">
-            <Heading
-              fontSize="6xl"
-              fontWeight={600}
-              color="white"
-              fontFamily="mono"
-              mb={[4, 4, 4, 12]}
-              display="flex"
-              flexDir="row"
-              ref={topRef}
-            >
-              Seeds
-              <Text pl={4} alignSelf="center" fontSize="4xl">
-                🌱
-              </Text>
-            </Heading>
-            <Text mb={4}>Seeds are MetaGame's labor token.</Text>
-            <Text mb={4}>
-              People contribute towards creation of MetaGame, meanwhile
-              generating XP &amp; getting paid out in Seeds proportional to
-              their gained XP.
+
+        <Container w="100%" maxW="6xl">
+          {/* Needs to be a <Heading> instead of <MetaHeading> or ref won't work for the scroll to top */}
+          <Heading
+            fontSize="6xl"
+            fontWeight={600}
+            color="white"
+            fontFamily="body"
+            mb={[4, 4, 4, 12]}
+            display="flex"
+            flexDir="row"
+            justifyContent="center"
+            ref={topRef}
+          >
+            Seeds
+            <Text pl={4} alignSelf="center" fontSize="4xl">
+              🌱
             </Text>
-            <UnorderedList spacing={2} pl={3}>
-              <ListItem>
-                All tokens are retroactive rewards for non-financial
-                contributions.
-              </ListItem>
-              <ListItem>
-                No tokens were minted for investment or speculation purposes.
-              </ListItem>
-              <ListItem>
-                There was never any liquidity mining program, yet there is
-                liquidity. 🙃
-              </ListItem>
-            </UnorderedList>
-          </Flex>
-          <Image width="full" src={SEEDsFlowChart.src} alignSelf="end" mt={4} />
-        </Grid>
+          </Heading>
+        </Container>
+
+        <Container as="section" w="100%" maxW="6xl">
+          <Heading
+            as="h2"
+            color="white"
+            fontFamily="mono"
+            fontWeight={700}
+            mb={[4, 4, 4, 12]}
+          >
+            What are Seeds?
+          </Heading>
+
+          <Container
+            maxW="lg"
+            centerContent
+            backgroundColor="whiteAlpha.200"
+            backdropFilter="blur(7px)"
+            boxShadow="md"
+            borderRadius="lg"
+          >
+            <VStack spacing={8} py={8} px={4}>
+              <Image
+                alt="Seeds image"
+                backgroundColor="purpleModalDark"
+                bgImage={BackgroundImage.src}
+                backgroundSize="cover"
+                boxSize="16rem"
+                borderRadius="full"
+                maxW="80%"
+                mx="auto"
+                objectFit="scale-down"
+                src={SeedsImg}
+              />
+
+              <Box>
+                <Text as="h3" fontWeight={700} mb={4}>
+                  Seeds are MetaGame’s labor token
+                </Text>
+                <Text mb={4}>
+                  People contribute towards creation of MetaGame, meanwhile
+                  generating XP &amp; getting paid out in Seeds proportional to
+                  their gained XP.
+                </Text>
+
+                <UnorderedList>
+                  <ListItem>
+                    All tokens are retroactive rewards for non-financial
+                    contributions.
+                  </ListItem>
+                  <ListItem>
+                    No tokens were minted for investment or speculation
+                    purposes.
+                  </ListItem>
+                  <ListItem>
+                    There was never any liquidity mining program, yet there is
+                    liquidity. 🙃
+                  </ListItem>
+                </UnorderedList>
+              </Box>
+            </VStack>
+          </Container>
+        </Container>
+
+        <Container as="section" w="100%" maxW="6xl">
+          <Heading
+            as="h2"
+            color="white"
+            fontFamily="mono"
+            fontWeight={700}
+            mb={[4, 4, 4, 12]}
+          >
+            Watering Seeds
+          </Heading>
+
+          <Image
+            alt="Flowchart: how Seeds help MetaGame grow"
+            src={SEEDsFlowChart.src}
+            mx="auto"
+          />
+        </Container>
 
         <Grid templateColumns={['auto', 'auto', '1fr 1fr']} gap={6}>
           {cardsConfig.map(({ title, description, Content }) => (
             <Card {...{ title, description, Content }} key={title} />
           ))}
         </Grid>
+
         <Image src={Octopus.src} pt={8} />
         <Box pb={4}>
           <Button
