790a75a8e31d02ace22c67d1dbd7259c2a8c7cd8
vidvidvid <weetopol@gmail.com>
2022-11-06 20:20:55 +0100

    remove forwardRef

--- packages/web/components/Player/PlayerProfilePicture.tsx
+++ packages/web/components/Player/PlayerProfilePicture.tsx
@@ -9,33 +9,34 @@ type PlayerProfilePictureProps = AvatarProps & {
   player?: Player | GuildPlayer;
 };
 
-export const PlayerProfilePicture: React.FC<PlayerProfilePictureProps> =
-  React.forwardRef<HTMLSpanElement, PlayerProfilePictureProps>(
-    ({ player: user, src, ...props }) => {
-      const player = user as Player;
-      const { value: image } = useProfileField({
-        field: 'profileImageURL',
-        player,
-        getter: getPlayerImage,
-      });
-      const { name } = useProfileField({
-        field: 'name',
-        player,
-        getter: getPlayerName,
-      });
-      const attrs = {
-        src: src ?? image ?? undefined,
-        name: name ?? undefined,
-        color: 'white',
-        borderTopRadius: 10,
-        marginTop: '0 !important',
-        ...props,
-      };
+export const PlayerProfilePicture: React.FC<PlayerProfilePictureProps> = ({
+  player: user,
+  src,
+  ...props
+}) => {
+  const player = user as Player;
+  const { value: image } = useProfileField({
+    field: 'profileImageURL',
+    player,
+    getter: getPlayerImage,
+  });
+  const { name } = useProfileField({
+    field: 'name',
+    player,
+    getter: getPlayerName,
+  });
+  const attrs = {
+    src: src ?? image ?? undefined,
+    name: name ?? undefined,
+    color: 'white',
+    borderTopRadius: 10,
+    marginTop: '0 !important',
+    ...props,
+  };
 
-      if (src || hasImage(player)) {
-        attrs.bg = 'transparent';
-      }
+  if (src || hasImage(player)) {
+    attrs.bg = 'transparent';
+  }
 
-      return <Image {...attrs} />;
-    },
-  );
+  return <Image {...attrs} />;
+};
