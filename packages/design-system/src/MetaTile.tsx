1e938dce70d114ad77ddeef83a2aa083eb7e37d9
vidvidvid <weetopol@gmail.com>
2022-11-06 20:17:47 +0100

    redesign of the cards #1

--- packages/design-system/src/MetaTile.tsx
+++ packages/design-system/src/MetaTile.tsx
@@ -6,19 +6,30 @@ export const MetaTileHeader: React.FC<StackProps> = ({
   ...props
 }) => (
   <VStack
-    w="100%"
+    w="full"
     spacing="6"
     align="stretch"
-    mb={6}
     position="relative"
     {...props}
+    borderTopRadius={10}
+    bgColor="rgba(255, 255, 255, 0.2)"
   >
     {children}
   </VStack>
 );
 
 export const MetaTileBody: React.FC<StackProps> = ({ children, ...props }) => (
-  <VStack w="100%" spacing="6" align="stretch" {...props}>
+  <VStack
+    w="full"
+    spacing={3}
+    align="stretch"
+    p={3}
+    pt={8}
+    borderBottomRadius={10}
+    bgColor="rgba(255, 255, 255, 0.06)"
+    style={{ backdropFilter: 'blur(10px)' }}
+    {...props}
+  >
     {children}
   </VStack>
 );
@@ -26,16 +37,16 @@ export const MetaTileBody: React.FC<StackProps> = ({ children, ...props }) => (
 export const MetaTile: React.FC<FlexProps> = ({ children, ...props }) => (
   <Flex
     direction="column"
-    bg="whiteAlpha.200"
+    bg="rgba(255, 255, 255, 0.06)"
     style={{ backdropFilter: 'blur(7px)' }}
     rounded="lg"
     p={6}
     maxW="25rem" // (2 / 3.5) = ~0.571 aspect ratio desired
-    w="100%"
-    h="100%"
+    w="full"
+    h="full"
     align="stretch"
     position="relative"
-    overflow="hidden"
+    // overflow="hidden"
     justify="space-between"
     {...props}
   >
--- packages/web/components/Player/PlayerTile.tsx
+++ packages/web/components/Player/PlayerTile.tsx
@@ -1,6 +1,5 @@
 import {
-  Box,
-  getTimeZoneFor,
+  Flex,
   Heading,
   HStack,
   LinkBox,
@@ -10,24 +9,17 @@ import {
   MetaTileBody,
   MetaTileHeader,
   Text,
-  TimeZoneType,
-  Tooltip,
   VStack,
-  Wrap,
   WrapItem,
 } from '@metafam/ds';
-import { getSeasonNum, Maybe } from '@metafam/utils';
-import { PlayerAvatar } from 'components/Player/PlayerAvatar';
 import { PlayerContacts } from 'components/Player/PlayerContacts';
+import { PlayerProfilePicture } from 'components/Player/PlayerProfilePicture';
 import { PlayerTileMemberships } from 'components/Player/PlayerTileMemberships';
 import { SkillsTags } from 'components/Quest/Skills';
 import { Player, Skill } from 'graphql/autogen/types';
 import NextLink from 'next/link';
-import React, { useMemo } from 'react';
-import { FaGlobe } from 'react-icons/fa';
-import { convertToRoman } from 'utils/formatHelpers';
+import React from 'react';
 import {
-  getPlayerBanner,
   getPlayerDescription,
   getPlayerName,
   getPlayerURL,
@@ -40,14 +32,7 @@ type Props = {
 
 const MAX_BIO_LENGTH = 240;
 
-export const PlayerTile: React.FC<Props> = ({
-  player,
-  showSeasonalXP = false,
-}) => {
-  const tz: Maybe<TimeZoneType> = useMemo(
-    () => getTimeZoneFor({ location: player.profile?.timeZone ?? undefined }),
-    [player.profile?.timeZone],
-  );
+export const PlayerTile: React.FC<Props> = ({ player }) => {
   const description = getPlayerDescription(player);
   const displayDescription =
     (description?.length ?? 0) > MAX_BIO_LENGTH
@@ -58,106 +43,94 @@ export const PlayerTile: React.FC<Props> = ({
   return (
     <LinkBox>
       <MetaTile>
-        <Box
-          bgImage={`url(${getPlayerBanner(player)})`}
-          bgSize="cover"
-          bgPosition="center"
-          position="absolute"
-          top={0}
-          left={0}
-          w="100%"
-          h="4.5rem"
-        />
         <NextLink as={getPlayerURL(player)} href="/player/[username]" passHref>
-          <LinkOverlay>
+          <LinkOverlay display="flex" flexDirection="column" height="100%">
             <MetaTileHeader>
-              <VStack>
-                <PlayerAvatar {...{ player }} size="xl" />
-                <Heading size="lg" color="white" fontFamily="body">
-                  {getPlayerName(player)}
-                </Heading>
+              <VStack pos="relative">
+                <Flex
+                  flexDir="column"
+                  gap={1}
+                  pos="absolute"
+                  left={-8}
+                  p={3}
+                  top={-8}
+                  background="rgba(255, 255, 255, 0.1)"
+                  backdropFilter="blur(10.5px)"
+                  borderRadius="8px"
+                >
+                  {player.rank && (
+                    <WrapItem>
+                      <MetaTag
+                        backgroundColor={player.rank.toLowerCase()}
+                        size="md"
+                        color="blackAlpha.600"
+                      >
+                        {player.rank}
+                      </MetaTag>
+                    </WrapItem>
+                  )}
+                  <Text fontSize="sm" color="blueLight">
+                    XP: {Math.floor(player.totalXP).toLocaleString()}
+                  </Text>
+                </Flex>
+                <PlayerProfilePicture {...{ player }} size="xl" />
+                <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
+                  <Heading
+                    size="lg"
+                    color="white"
+                    bgColor="rgba(255, 255, 255, 0.06)"
+                    style={{ backdropFilter: 'blur(10px)' }}
+                    lineHeight={1.8}
+                    justifyContent="center"
+                    px={3}
+                    width="full"
+                    textAlign="center"
+                    borderRadius={10}
+                    fontFamily="body"
+                    fontWeight={400}
+                  >
+                    {getPlayerName(player)}
+                  </Heading>
+                </Flex>
               </VStack>
-              <Wrap w="100%" justify="center">
-                {player.profile?.explorerTypeTitle && (
-                  <WrapItem>
-                    <MetaTag size="md" textTransform="uppercase">
-                      {player.profile.explorerTypeTitle}
-                    </MetaTag>
-                  </WrapItem>
-                )}
-                {player.rank && (
-                  <WrapItem>
-                    <MetaTag
-                      backgroundColor={player.rank.toLowerCase()}
-                      size="md"
-                      color="blackAlpha.600"
-                    >
-                      {player.rank}
-                    </MetaTag>
-                  </WrapItem>
-                )}
-                <WrapItem>
-                  <MetaTag size="md">
-                    {showSeasonalXP ? 'TOTAL XP: ' : 'XP: '}
-                    {Math.floor(player.totalXP).toLocaleString()}
-                  </MetaTag>
-                </WrapItem>
-                {showSeasonalXP && (
-                  <WrapItem>
-                    <MetaTag size="md">
-                      SEASON {convertToRoman(getSeasonNum())} XP:{' '}
-                      {Math.floor(player.seasonXP).toLocaleString()}
-                    </MetaTag>
-                  </WrapItem>
-                )}
-              </Wrap>
-              {tz && (
-                <Tooltip label={tz.name} hasArrow>
-                  <HStack alignItems="baseline" w="auto" justify="center">
-                    <FaGlobe color="blueLight" fontSize="0.875rem" />
-                    <Text fontSize="lg">{tz.abbreviation}</Text>
-                    {tz.utc && <Text fontSize="sm">{tz.utc}</Text>}
-                  </HStack>
-                </Tooltip>
-              )}
+            </MetaTileHeader>
+            <MetaTileBody pos="relative" height="full">
               {displayDescription && (
-                <VStack spacing={2} align="stretch" pt="0.5rem">
+                <VStack spacing={2} align="stretch">
                   <Text textStyle="caption" textTransform="uppercase">
                     About
                   </Text>
                   <Text fontSize="sm">{displayDescription}</Text>
                 </VStack>
               )}
-            </MetaTileHeader>
+              {player.skills?.length && (
+                <VStack spacing={2} align="stretch">
+                  <Text textStyle="caption" textTransform="uppercase">
+                    Skills
+                  </Text>
+                  <SkillsTags
+                    skills={
+                      player.skills.map(({ Skill: skill }) => skill) as Skill[]
+                    }
+                  />
+                </VStack>
+              )}
+
+              <PlayerTileMemberships {...{ player }} />
+
+              {player.accounts?.length && (
+                <VStack spacing={2} align="stretch">
+                  <Text textStyle="caption" textTransform="uppercase">
+                    Contact
+                  </Text>
+                  <HStack mt={2}>
+                    <PlayerContacts {...{ player }} disableBrightId />
+                  </HStack>
+                </VStack>
+              )}
+            </MetaTileBody>
           </LinkOverlay>
         </NextLink>
-        <MetaTileBody>
-          {player.skills?.length && (
-            <VStack spacing={2} align="stretch">
-              <Text textStyle="caption" textTransform="uppercase">
-                Skills
-              </Text>
-              <SkillsTags
-                skills={
-                  player.skills.map(({ Skill: skill }) => skill) as Skill[]
-                }
-              />
-            </VStack>
-          )}
-
-          <PlayerTileMemberships {...{ player }} />
-
-          {player.accounts?.length && (
-            <VStack spacing={2} align="stretch">
-              <Text textStyle="caption" textTransform="uppercase">
-                Contact
-              </Text>
-              <HStack mt={2}>
-                <PlayerContacts {...{ player }} disableBrightId />
-              </HStack>
-            </VStack>
-          )}
-        </MetaTileBody>
       </MetaTile>
     </LinkBox>
   );
