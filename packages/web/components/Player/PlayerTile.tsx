86629af04c87cfa264d313b0ff261706b99e6cc8
vidvidvid <weetopol@gmail.com>
2022-11-09 20:22:20 +0100

    make all card images square & clickable

--- packages/web/components/Player/PlayerTile.tsx
+++ packages/web/components/Player/PlayerTile.tsx
@@ -3,7 +3,6 @@ import {
   Heading,
   HStack,
   Link,
-  LinkBox,
   LoadingState,
   MetaTile,
   MetaTileBody,
@@ -20,7 +19,6 @@ import { SkillsTags } from 'components/Quest/Skills';
 import { Player, Skill } from 'graphql/autogen/types';
 import { getAllMemberships, GuildMembership } from 'graphql/getMemberships';
 import { Patron } from 'graphql/types';
-import NextLink from 'next/link';
 import React, { useEffect, useState } from 'react';
 import {
   getPlayerDescription,
@@ -67,99 +65,94 @@ export const PlayerTile: React.FC<Props> = ({
   }, [player]);
 
   return (
-    <LinkBox height="full" width="full">
-      <MetaTile height="full" width="full">
-        <NextLink as={getPlayerURL(player)} href="/player/[username]" passHref>
-          <Link
-            display="flex"
-            flexDirection="column"
-            height="100%"
-            _hover={{
-              textUnderline: 'none',
-            }}
-          >
-            <MetaTileHeader>
-              {isPatron && typeof index === 'number' && pSeedPrice ? (
-                <PatronRank
-                  patron={player as Patron}
-                  pSeedPrice={pSeedPrice}
-                  index={index}
+    <Link
+      role="group"
+      _hover={{ textDecoration: 'none' }}
+      href={getPlayerURL(player)}
+    >
+      <MetaTile height="full" width="full" cursor="pointer">
+        <MetaTileHeader>
+          {isPatron && typeof index === 'number' && pSeedPrice ? (
+            <PatronRank
+              patron={player as Patron}
+              pSeedPrice={pSeedPrice}
+              index={index}
+            />
+          ) : (
+            <PlayerRank player={player} showSeasonalXP={showSeasonalXP} />
+          )}
+          <PlayerProfilePicture {...{ player }} size="xl" />
+          <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
+            <Heading
+              size="lg"
+              color="white"
+              bgColor="rgba(255, 255, 255, 0.06)"
+              style={{ backdropFilter: 'blur(10px)' }}
+              lineHeight={1.8}
+              justifyContent="center"
+              px={3}
+              width="full"
+              textAlign="center"
+              borderRadius={10}
+              fontFamily="body"
+              fontWeight={400}
+            >
+              {getPlayerName(player)}
+            </Heading>
+          </Flex>
+        </MetaTileHeader>
+        <MetaTileBody pos="relative" height="full">
+          <Flex flexDir="column" mb="auto">
+            {displayDescription && (
+              <VStack spacing={2} align="stretch">
+                <Text textStyle="caption">ABOUT</Text>
+                <Text fontSize="sm">{displayDescription}</Text>
+              </VStack>
+            )}
+            {!!player.skills?.length && (
+              <VStack spacing={2} align="stretch">
+                <Text textStyle="caption">SKILLS</Text>
+                <SkillsTags
+                  skills={
+                    player.skills.map(({ Skill: skill }) => skill) as Skill[]
+                  }
                 />
-              ) : (
-                <PlayerRank player={player} showSeasonalXP={showSeasonalXP} />
-              )}
-              <PlayerProfilePicture {...{ player }} size="xl" />
-              <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
-                <Heading
-                  size="lg"
-                  color="white"
-                  bgColor="rgba(255, 255, 255, 0.06)"
-                  style={{ backdropFilter: 'blur(10px)' }}
-                  lineHeight={1.8}
-                  justifyContent="center"
-                  px={3}
-                  width="full"
-                  textAlign="center"
-                  borderRadius={10}
-                  fontFamily="body"
-                  fontWeight={400}
-                >
-                  {getPlayerName(player)}
-                </Heading>
-              </Flex>
-            </MetaTileHeader>
-            <MetaTileBody pos="relative" height="full">
-              {displayDescription && (
-                <VStack spacing={2} align="stretch">
-                  <Text textStyle="caption">ABOUT</Text>
-                  <Text fontSize="sm">{displayDescription}</Text>
-                </VStack>
-              )}
-              {player.skills?.length && (
-                <VStack spacing={2} align="stretch">
-                  <Text textStyle="caption">SKILLS</Text>
-                  <SkillsTags
-                    skills={
-                      player.skills.map(({ Skill: skill }) => skill) as Skill[]
-                    }
-                  />
-                </VStack>
-              )}
+              </VStack>
+            )}
 
-              <PlayerTileMemberships {...{ player }} />
+            <PlayerTileMemberships {...{ player }} />
+          </Flex>
 
-              <Flex justifyContent="space-between" pointerEvents="none">
-                {!!memberships.length && (
-                  <VStack spacing={2} align="stretch">
-                    <Text textStyle="caption">MEMBER OF</Text>
-                    <HStack mt={2} position="relative" zIndex={1}>
-                      {loading && <LoadingState mb={6} />}
-                      {!loading &&
-                        memberships
-                          .slice(0, 3)
-                          .map((membership) => (
-                            <DAOMembershipSmall
-                              {...{ membership }}
-                              key={membership.address}
-                            />
-                          ))}
-                    </HStack>
-                  </VStack>
-                )}
+          <Flex justifyContent="space-between" pointerEvents="none">
+            {!!memberships.length && (
+              <VStack spacing={2} align="stretch">
+                <Text textStyle="caption">MEMBER OF</Text>
+                <HStack mt={2} position="relative" zIndex={1}>
+                  {loading && <LoadingState mb={6} />}
+                  {!loading &&
+                    memberships
+                      .slice(0, 3)
+                      .map((membership) => (
+                        <DAOMembershipSmall
+                          {...{ membership }}
+                          key={membership.address}
+                        />
+                      ))}
+                </HStack>
+              </VStack>
+            )}
 
-                {!!player.accounts?.length && (
-                  <VStack spacing={2} align="stretch">
-                    <Text textStyle="caption">CONTACT</Text>
-                    <HStack mt={2} pointerEvents="all">
-                      <PlayerContacts {...{ player }} disableBrightId />
-                    </HStack>
-                  </VStack>
-                )}
-              </Flex>
-            </MetaTileBody>
-          </Link>
-        </NextLink>
+            {!!player.accounts?.length && (
+              <VStack spacing={2} align="stretch">
+                <Text textStyle="caption">CONTACT</Text>
+                <HStack mt={2} pointerEvents="all">
+                  <PlayerContacts {...{ player }} disableBrightId />
+                </HStack>
+              </VStack>
+            )}
+          </Flex>
+        </MetaTileBody>
       </MetaTile>
-    </LinkBox>
+    </Link>
   );
 };
--- packages/web/components/Quest/QuestTile.tsx
+++ packages/web/components/Quest/QuestTile.tsx
@@ -3,7 +3,6 @@ import {
   Flex,
   Heading,
   HStack,
-  Image,
   Link,
   MetaTile,
   MetaTileBody,
@@ -17,6 +16,7 @@ import { MarkdownViewer as Markdown } from 'components/MarkdownViewer';
 import { RepetitionTag, StatusTag } from 'components/Quest/QuestTags';
 import { RolesTags } from 'components/Quest/Roles';
 import { SkillsTags } from 'components/Quest/Skills';
+import { SquareImage } from 'components/SquareImage';
 import { PlayerRole, QuestFragment, Skill } from 'graphql/autogen/types';
 import moment from 'moment';
 import { safelyParseNChakrifyHtml } from 'utils/stringHelpers';
@@ -36,98 +36,85 @@ export const QuestTile: React.FC<Props> = ({ quest }) => {
     : '';
 
   return (
-    <MetaLink
-      display="flex"
-      color="white"
-      as={`/quest/${quest.id}`}
-      href="/quest/[id]"
-      _hover={{ borderColor: 'transparent' }}
+    <Link
+      role="group"
+      _hover={{ textDecoration: 'none' }}
+      href={`/quest/${quest.id}`}
     >
       <MetaTile height="full" width="full">
-        <Link
-          display="flex"
-          flexDirection="column"
-          height="100%"
-          _hover={{
-            textUnderline: 'none',
-          }}
-        >
-          <MetaTileHeader>
-            <Image src={BackgroundImage} mt="0 !important" />
-            <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
-              <Heading
-                size="lg"
-                color="white"
-                bgColor="rgba(255, 255, 255, 0.06)"
-                style={{ backdropFilter: 'blur(10px)' }}
-                lineHeight={1.8}
-                justifyContent="center"
-                px={3}
-                width="full"
-                textAlign="center"
-                borderRadius={10}
-                fontFamily="body"
-                fontWeight={400}
-              >
-                {quest.title}
-              </Heading>
-            </Flex>
-          </MetaTileHeader>
-          <MetaTileBody>
-            <HStack mt={3}>
-              <RepetitionTag
-                repetition={quest.repetition}
-                cooldown={quest.cooldown}
-              />
-              <StatusTag status={quest.status} />
-              <Text>
-                <i>{moment(quest.createdAt).fromNow()}</i>
+        <MetaTileHeader>
+          <SquareImage src={BackgroundImage} />
+          <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
+            <Heading
+              size="lg"
+              color="white"
+              bgColor="rgba(255, 255, 255, 0.06)"
+              style={{ backdropFilter: 'blur(10px)' }}
+              lineHeight={1.8}
+              justifyContent="center"
+              px={3}
+              width="full"
+              textAlign="center"
+              borderRadius={10}
+              fontFamily="body"
+              fontWeight={400}
+            >
+              {quest.title}
+            </Heading>
+          </Flex>
+        </MetaTileHeader>
+        <MetaTileBody>
+          <HStack mt={3}>
+            <RepetitionTag
+              repetition={quest.repetition}
+              cooldown={quest.cooldown}
+            />
+            <StatusTag status={quest.status} />
+            <Text>
+              <i>{moment(quest.createdAt).fromNow()}</i>
+            </Text>
+          </HStack>
+          <Flex flexDir="column">
+            <Box pb={2}>
+              <Text textStyle="caption" pb={1}>
+                DESCRIPTION
               </Text>
-            </HStack>
-            <Flex flexDir="column">
-              <Box pb={2}>
-                <Text textStyle="caption" pb={1}>
-                  DESCRIPTION
-                </Text>
-                {descIsHtml ? (
-                  <Prose>{parsedDescription}</Prose>
-                ) : (
-                  <Markdown>{descriptionSummary}</Markdown>
-                )}
-                <MetaLink as={`/quest/${quest.id}`} href="/quest/[id]">
-                  Read more
-                </MetaLink>
-              </Box>
-              <Box pb={2}>
-                <Text textStyle="caption" pb={1}>
-                  SKILLS
-                </Text>
-                <SkillsTags
-                  skills={
-                    quest.quest_skills.map(({ skill }) => skill) as Skill[]
+              {descIsHtml ? (
+                <Prose>{parsedDescription}</Prose>
+              ) : (
+                <Markdown>{descriptionSummary}</Markdown>
+              )}
+              <MetaLink as={`/quest/${quest.id}`} href="/quest/[id]">
+                Read more
+              </MetaLink>
+            </Box>
+            <Box pb={2}>
+              <Text textStyle="caption" pb={1}>
+                SKILLS
+              </Text>
+              <SkillsTags
+                skills={quest.quest_skills.map(({ skill }) => skill) as Skill[]}
+              />
+            </Box>
+            <Box pb={2}>
+              <Text textStyle="caption" pb={1}>
+                ROLES
+              </Text>
+              {quest.quest_roles.length ? (
+                <RolesTags
+                  roles={
+                    quest.quest_roles.map(
+                      ({ PlayerRole: r }) => r,
+                    ) as PlayerRole[]
                   }
                 />
-              </Box>
-              <Box pb={2}>
-                <Text textStyle="caption" pb={1}>
-                  ROLES
-                </Text>
-                {quest.quest_roles.length ? (
-                  <RolesTags
-                    roles={
-                      quest.quest_roles.map(
-                        ({ PlayerRole: r }) => r,
-                      ) as PlayerRole[]
-                    }
-                  />
-                ) : (
-                  <Text>/</Text>
-                )}
-              </Box>
-            </Flex>
-          </MetaTileBody>
-        </Link>
+              ) : (
+                <Text>/</Text>
+              )}
+            </Box>
+          </Flex>
+        </MetaTileBody>
       </MetaTile>
-    </MetaLink>
+    </Link>
   );
 };
