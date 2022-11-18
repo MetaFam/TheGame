cd0c6e15c90e28c8c143bc3098e79f3ac320f04d
nitegeist <nitegeist.dev@gmail.com>
2022-11-18 12:25:13 -0500

    refactor: :recycle: Refactor code based on comments

--- packages/web/components/Landing/OnboardingGuidance/index.tsx
+++ packages/web/components/Landing/OnboardingGuidance/index.tsx
@@ -10,53 +10,25 @@ import {
   Text,
   VStack,
 } from '@metafam/ds';
-import BabyOctopus from 'assets/quests/baby_octo.png';
-import Octopus from 'assets/quests/octopus.png';
-import SpaceOctopi from 'assets/quests/space_octo.png';
 import { PageContainer } from 'components/Container';
 import { MarkdownViewer } from 'components/MarkdownViewer';
+import { SquareImage } from 'components/SquareImage';
 import { useRouter } from 'next/router';
-import { useEffect, useRef } from 'react';
-
-import roleJson from './guidance.json';
-import { RolePicture } from './RolePicture';
-
-type GuidanceRole = {
-  role: string;
-  details: string[];
-  image: string;
-};
-
-enum Role {
-  CuriousOcto = 'Curious Octo',
-  EngagedOcto = 'Engaged Octo',
-  SpaceOcto = 'Space Octo',
-}
+import { guidanceDetails, Role } from 'utils/guidanceData';
 
 const OnboardingGuidance: React.FC = () => {
   const router = useRouter();
-  const guidanceRoles = useRef<GuidanceRole[]>([]);
-  useEffect(() => {
-    guidanceRoles.current = roleJson.roles.map(({ role, list }) => {
-      const guidance: GuidanceRole = { role, details: list, image: '' };
-      if (role === Role.CuriousOcto) guidance.image = BabyOctopus.src;
-      if (role === Role.EngagedOcto) guidance.image = Octopus.src;
-      if (role === Role.SpaceOcto) guidance.image = SpaceOctopi.src;
-      return guidance;
-    });
-  }, []);
 
   const handleClick = () => {
     router.push('/profile/setup/name');
   };
 
   return (
-    <PageContainer w="100%" mt={24} px={8}>
+    <PageContainer w="100%" px={8}>
       <VStack
         pos="relative"
         align="center"
         justify="center"
-        mb={{ base: '3rem' }}
         spacing={0}
         maxW="full"
       >
@@ -71,78 +43,137 @@ const OnboardingGuidance: React.FC = () => {
           Your journey into MetaGame has just begun. Here's how it works:
         </Text>
       </VStack>
-      {!!guidanceRoles.current?.length && (
-        <SimpleGrid
-          columns={[1, null, 2, 3]}
-          spacing={{ base: 20, lg: 8 }}
-          autoRows="minmax(35rem, auto)"
-        >
-          {guidanceRoles.current?.map(({ role, details, image }) => (
-            <Box key={role}>
-              <MetaTile height="full" width="full" noTilt>
-                <MetaTileHeader>
-                  <RolePicture src={image} size="xl" />
-                  <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
-                    {role && (
-                      <Heading
-                        size="lg"
-                        color="white"
-                        bgColor="rgba(255, 255, 255, 0.06)"
-                        style={{ backdropFilter: 'blur(10px)' }}
-                        lineHeight={1.8}
-                        justifyContent="center"
-                        px={3}
-                        width="full"
-                        textAlign="center"
-                        borderRadius={10}
-                        fontFamily="body"
-                        fontWeight={400}
-                      >
-                        {role}
-                      </Heading>
-                    )}
-                  </Flex>
-                </MetaTileHeader>
-                <MetaTileBody pos="relative" height="full">
-                  {!!details?.length && (
-                    <Box pt={4}>
-                      {details.map((item, index) => (
-                        <MarkdownViewer key={index}>{item}</MarkdownViewer>
-                      ))}
-                    </Box>
-                  )}
-                </MetaTileBody>
-              </MetaTile>
-              {role === Role.CuriousOcto && (
-                <Flex
-                  mt={{ base: 2, lg: 4 }}
-                  direction="column"
-                  alignItems="center"
-                  justifyContent="start"
-                >
-                  <Text fontSize={{ base: '2xl', lg: '4xl' }}>☝️</Text>
-                  <Text fontSize="md">You are here</Text>
-                </Flex>
-              )}
+      <SimpleGrid
+        p={12}
+        columns={[1, null, 2, 3]}
+        spacing={{ base: 20, lg: 8 }}
+        autoRows="minmax(35rem, auto)"
+      >
+        <MetaTile height="full" width="full" noTilt>
+          <MetaTileHeader>
+            <SquareImage
+              src={guidanceDetails[Role.CuriousOcto].image}
+              size="xl"
+            />
+            <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
+              <Heading
+                size="lg"
+                color="white"
+                bgColor="aplhaWhite.100"
+                style={{ backdropFilter: 'blur(10px)' }}
+                lineHeight={1.8}
+                justifyContent="center"
+                px={3}
+                width="full"
+                textAlign="center"
+                borderRadius={10}
+                fontFamily="body"
+                fontWeight={400}
+              >
+                {guidanceDetails[Role.CuriousOcto].role}
+              </Heading>
+            </Flex>
+          </MetaTileHeader>
+          <MetaTileBody pos="relative" height="full">
+            <Box pt={4}>
+              {guidanceDetails[Role.CuriousOcto].details.map((item) => (
+                <MarkdownViewer>{item}</MarkdownViewer>
+              ))}
             </Box>
-          ))}
-        </SimpleGrid>
-      )}
-      <Box pt={{ base: 16, lg: 32 }} pb={16}>
-        <MetaButton
-          onClick={handleClick}
-          colorScheme="purple"
-          textTransform="uppercase"
-          letterSpacing="0.1em"
-          p={4}
-          size="lg"
-          fontSize="md"
-          color="white"
-          _hover={{ background: 'purple.600' }}
+          </MetaTileBody>
+        </MetaTile>
+        <MetaTile height="full" width="full" noTilt>
+          <MetaTileHeader>
+            <SquareImage
+              src={guidanceDetails[Role.EngagedOcto].image}
+              size="xl"
+            />
+            <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
+              <Heading
+                size="lg"
+                color="white"
+                bgColor="aplhaWhite.100"
+                style={{ backdropFilter: 'blur(10px)' }}
+                lineHeight={1.8}
+                justifyContent="center"
+                px={3}
+                width="full"
+                textAlign="center"
+                borderRadius={10}
+                fontFamily="body"
+                fontWeight={400}
+              >
+                {guidanceDetails[Role.EngagedOcto].role}
+              </Heading>
+            </Flex>
+          </MetaTileHeader>
+          <MetaTileBody pos="relative" height="full">
+            <Box pt={4}>
+              {guidanceDetails[Role.EngagedOcto].details.map((item) => (
+                <MarkdownViewer>{item}</MarkdownViewer>
+              ))}
+            </Box>
+          </MetaTileBody>
+        </MetaTile>
+        <MetaTile height="full" width="full" noTilt>
+          <MetaTileHeader>
+            <SquareImage
+              src={guidanceDetails[Role.SpaceOcto].image}
+              size="xl"
+            />
+            <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
+              <Heading
+                size="lg"
+                color="white"
+                bgColor="aplhaWhite.100"
+                style={{ backdropFilter: 'blur(10px)' }}
+                lineHeight={1.8}
+                justifyContent="center"
+                px={3}
+                width="full"
+                textAlign="center"
+                borderRadius={10}
+                fontFamily="body"
+                fontWeight={400}
+              >
+                {guidanceDetails[Role.SpaceOcto].role}
+              </Heading>
+            </Flex>
+          </MetaTileHeader>
+          <MetaTileBody pos="relative" height="full">
+            <Box pt={4}>
+              {guidanceDetails[Role.SpaceOcto].details.map((item) => (
+                <MarkdownViewer>{item}</MarkdownViewer>
+              ))}
+            </Box>
+          </MetaTileBody>
+        </MetaTile>
+      </SimpleGrid>
+      <Flex w="4xl" alignItems="center" justifyContent="start">
+        <Flex
+          mt={{ base: 2, lg: 4 }}
+          direction="column"
+          alignItems="center"
+          justifyContent="start"
         >
-          I Understand
-        </MetaButton>
-      </Box>
+          <Text fontSize={{ base: '2xl', lg: '4xl' }}>☝️</Text>
+          <Text fontSize="md">You are here</Text>
+        </Flex>
+      </Flex>
+
+      <MetaButton
+        onClick={handleClick}
+        colorScheme="purple"
+        textTransform="uppercase"
+        letterSpacing="0.1em"
+        p={4}
+        size="lg"
+        fontSize="md"
+        color="white"
+        _hover={{ background: 'purple.600' }}
+      >
+        I Understand
+      </MetaButton>
     </PageContainer>
   );
 };
--- packages/web/pages/onboarding/guide.tsx
+++ packages/web/pages/onboarding/guide.tsx
@@ -5,7 +5,7 @@ import { LandingHeader } from 'components/Landing/LandingHeader';
 import OnboardingGuidance from 'components/Landing/OnboardingGuidance';
 import { HeadComponent } from 'components/Seo';
 import { Socials } from 'pages';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 export const getStaticProps = async () => ({
   props: {
@@ -17,64 +17,14 @@ const OnboardingGuide: React.FC = () => {
   const responsiveBg = useBreakpointValue({
     base: BackgroundImage,
   });
-  const scrollContainer =
-    typeof document !== 'undefined'
-      ? document.getElementById('scroll-container')
-      : null;
-  const [section, setSection] = useState(0);
-  const hostName = useRef('https://metagame.wtf');
-  const setHostName = useCallback((host: string) => {
-    hostName.current = host;
-  }, []);
-
-  const handleScroll = useCallback(() => {
-    if (!scrollContainer) return;
-    const { scrollTop } = scrollContainer;
-    const height = typeof window !== 'undefined' ? window.innerHeight : 1;
-    setSection(Math.floor(scrollTop / height));
-  }, [scrollContainer]);
-
-  const handleKeyDown = useCallback(
-    (e: KeyboardEvent) => {
-      if (!scrollContainer) return;
-      const { scrollTop, scrollHeight } = scrollContainer;
-      const height = typeof window !== 'undefined' ? window.innerHeight : 1;
-      let newScrollTop = scrollTop;
-      if (e.key === 'ArrowLeft' || e.key === 'ArrowUp') {
-        newScrollTop -= height;
-        newScrollTop = Math.max(0, newScrollTop);
-      } else if (e.key === 'ArrowRight' || e.key === 'ArrowDown') {
-        newScrollTop += height;
-        newScrollTop = Math.min(scrollHeight, newScrollTop);
-      }
-      scrollContainer.scrollTop = newScrollTop;
-      setSection(Math.floor(newScrollTop / height));
-    },
-    [scrollContainer],
-  );
-
-  useEffect(() => {
-    scrollContainer?.addEventListener('scroll', handleScroll);
-    document.addEventListener('keydown', handleKeyDown);
-    document.querySelector('body')?.classList.add('onboarding-guide');
-
-    if (typeof window !== 'undefined') {
-      setHostName(window.location.origin);
-    }
-
-    return () => {
-      scrollContainer?.removeEventListener('scroll', handleScroll);
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [handleScroll, handleKeyDown, scrollContainer, section, setHostName]);
-
+  const [hostName, setHostName] = useState('https://metagame.wtf');
   return (
     <>
       <HeadComponent
         title="MetaGame: Onboarding Guide 🐙"
         description=" Your journey into MetaGame has just begun. Here's how it works..."
-        url={hostName.current}
-        img={`${hostName.current}/assets/social.png`}
+        url={hostName}
+        img={`${hostName}/assets/social.png`}
         cardStyle="summary_large_image"
       />
       <LandingHeader />
@@ -84,8 +34,7 @@ const OnboardingGuide: React.FC = () => {
         bgSize="cover"
         bgImageUrl={responsiveBg?.src}
         overflow="auto"
-        spacing={12}
-        minH={{ base: '100vh' }}
+        minH="100vh"
       >
         <OnboardingGuidance />
       </FullPageContainer>
