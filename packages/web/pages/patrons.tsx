5f9e2a43214a6ec096b41a995e8ff8f8027810dc
HHH-GH <hayden@opie-etc.com>
2022-11-27 14:18:37 +0800

    Patrons list shows 30 patrons on first load; click the Load More button to show the next 30, and so on
    Text below the Load More button shows how many patrons there are in total, and shows a tooltip when there are no more patrons to load into the list

--- packages/web/pages/patrons.tsx
+++ packages/web/pages/patrons.tsx
@@ -3,6 +3,9 @@ import {
   Box,
   Button,
   Image,
+  MetaButton,
+  Text,
+  Tooltip,
   VisuallyHidden,
   VStack,
 } from '@metafam/ds';
@@ -12,12 +15,17 @@ import { PatronList } from 'components/Patron/PatronList';
 import { HeadComponent } from 'components/Seo';
 import { getPatrons, getPSeedPrice } from 'graphql/getPatrons';
 import { InferGetStaticPropsType } from 'next';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export const getStaticProps = async () => {
-  const patrons = await getPatrons();
+  /**
+   * Get all the Patrons
+   * (MetaGame rules say there can be 150 max, so we'll limit the query to that many)
+   */
+  const patronsLimit = 150;
+  const patrons = await getPatrons(patronsLimit);
   const pSeedPrice = await getPSeedPrice().catch((error) => {
     console.error('Error fetching pSeed price', error);
     return null;
@@ -40,6 +48,67 @@ const PatronsPage: React.FC<Props> = ({ patrons, pSeedPrice }) => {
     topRef?.current?.scrollIntoView({ behavior: 'smooth' });
   }
 
+  /**
+   * Settings for the Load More patrons button
+   */
+  /* How many patrons were found, in total? */
+  const totalItems = patrons.length;
+
+  /* How many patrons to show initially, and how many more to load each time the Load More button is clicked? */
+  const showHowMany = 30;
+
+  /* How many Patrons are visible right now? */
+  const [visible, setVisible] = useState(showHowMany);
+
+  /* Increment the number visible by `showHowMany` when the Load More button is clicked */
+  const handleShowMoreItems = () => {
+    setVisible((prevValue) => prevValue + showHowMany);
+  };
+
+  /**
+   * Print out the Load More button
+   * If there are no more patrons to load, disable the button and add a tooltip that says 'no more to load'
+   */
+  function renderLoadMoreButton() {
+    const buttonStyles = {
+      minW: '10rem',
+    };
+
+    if (visible < totalItems) {
+      /* When there are more items to show */
+      return (
+        <MetaButton onClick={handleShowMoreItems} {...buttonStyles}>
+          Load more
+        </MetaButton>
+      );
+    }
+
+    /* When there are no more items to show */
+    return (
+      <Tooltip label="No more to load" aria-label="A tooltip">
+        <MetaButton as="span" isDisabled {...buttonStyles}>
+          Load more
+        </MetaButton>
+      </Tooltip>
+    );
+  }
+
+  /**
+   * Print text 'Showing X of Y items'
+   * if X (visible items) is greater than Y (total items), then just print Y of Y
+   * (or X could overflow and print 60 of 43)
+   */
+  function renderShowingXOfYItems() {
+    const showingXOf = visible >= totalItems ? totalItems : visible;
+    const showingYOf = totalItems;
+
+    return (
+      <Text>
+        Showing {showingXOf} of {showingYOf} patrons
+      </Text>
+    );
+  }
+
   return (
     <PageContainer>
       <HeadComponent
@@ -55,9 +124,18 @@ const PatronsPage: React.FC<Props> = ({ patrons, pSeedPrice }) => {
         Patrons of MetaGame
       </VisuallyHidden>
 
-      {/* VStack is used to make a consistent gap between the Patrons list, and the Octo image and back to top link */}
+      {/* VStack is used to make a consistent gap between the Patrons list, the Load More button, the X of Y patrons text, and the Octo image and back to top link */}
       <VStack maxW="7xl" w="100%" spacing={{ base: 4, md: 8 }}>
-        <PatronList {...{ patrons, pSeedPrice }} />
+        <PatronList
+          patrons={patrons.slice(0, visible)}
+          pSeedPrice={pSeedPrice}
+        />
+
+        {/* Load More button */}
+        {renderLoadMoreButton()}
+
+        {/* Showing X of Y patrons text */}
+        {renderShowingXOfYItems()}
 
         {/* Back to top link */}
         <Image src={Octopus.src} pt={8} />
