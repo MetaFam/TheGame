ca146d42a3da1214cb7e82486ddb783fe06cd939
HHH-GH <hayden@opie-etc.com>
2022-12-04 21:12:08 +0800

    Add the join CTA to guilds and patrons pages
    (The easy ones first...)

--- packages/web/pages/patrons.tsx
+++ packages/web/pages/patrons.tsx
@@ -2,7 +2,9 @@ import {
   ArrowUpIcon,
   Box,
   Button,
+  Center,
   Image,
+  Link,
   MetaButton,
   Text,
   Tooltip,
@@ -124,8 +126,22 @@ const PatronsPage: React.FC<Props> = ({ patrons, pSeedPrice }) => {
         Patrons of MetaGame
       </VisuallyHidden>
 
-      {/* VStack is used to make a consistent gap between the Patrons list, the Load More button, the X of Y patrons text, and the Octo image and back to top link */}
-      <VStack maxW="7xl" w="100%" spacing={{ base: 4, md: 8 }}>
+      {/* VStack is used to make a consistent gap between the Patrons CTA, the Patrons list, the Load More button, the X of Y patrons text, and the Octo image and back to top link */}
+      <VStack maxW="7xl" w="100%" spacing={{ base: 8, md: 12 }}>
+        <Center fontWeight="700" w="100%" maxW="4xl">
+          <Text as="p" textAlign="center">
+            Want to get on this leaderboard and rank high? Why not{' '}
+            <Link
+              href="/community/join/patrons"
+              color="pink.300"
+              textDecoration="underline"
+            >
+              become a patron
+            </Link>{' '}
+            or up your stake in the Seed farm?
+          </Text>
+        </Center>
+
         <PatronList
           patrons={patrons.slice(0, visible)}
           pSeedPrice={pSeedPrice}
