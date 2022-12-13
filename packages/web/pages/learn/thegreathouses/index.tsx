d78d51c50ad1c888e57428e090caa688c1c450b3
vidvidvid <weetopol@gmail.com>
2022-12-13 14:39:25 +0100

    fix the great houses descriptions

--- packages/web/pages/learn/thegreathouses/index.tsx
+++ packages/web/pages/learn/thegreathouses/index.tsx
@@ -1,26 +1,19 @@
 import {
-  Box,
   Flex,
   Grid,
   Heading,
   Link,
-  MetaTag,
   MetaTile,
   MetaTileBody,
   MetaTileHeader,
   Text,
-  VStack,
 } from '@metafam/ds';
 import { PageContainer } from 'components/Container';
 import { HeadComponent } from 'components/Seo';
 import { SquareImage } from 'components/SquareImage';
 import React from 'react';
 import { descriptions } from 'utils/menuLinks';
-import {
-  Difficulty,
-  QuestChainGreatHousesDetails,
-  Time,
-} from 'utils/questChains';
+import { QuestChainGreatHousesDetails } from 'utils/questChains';
 
 const TheGreatHousesPage: React.FC = () => (
   <PageContainer>
@@ -32,9 +25,10 @@ const TheGreatHousesPage: React.FC = () => (
     <Heading mt={8} mb={8}>
       The Great Houses
     </Heading>
-    <Text mb={8}>
-      The Great Houses are curated learning resources sending you on a journey
-      of discovery.
+    <Text mb={8} maxW="xl" textAlign="center">
+      The Great Houses are curated learning resources here to give you a meta
+      overview of different fields of interest & send you on a journey of
+      discovery.
     </Text>
     <Grid
       templateColumns={['1fr', '1fr', '1fr 1fr', '1fr 1fr 1fr']}
@@ -66,94 +60,43 @@ type CardProps = {
   description: string;
   link: string;
   image: string;
-  difficulty: string;
-  time: string;
 };
 
-const Card: React.FC<CardProps> = ({
-  title,
-  description,
-  link,
-  image,
-  difficulty,
-  time,
-}) => {
-  let difficultyBgColor;
-  switch (difficulty) {
-    case Difficulty.HARD:
-      difficultyBgColor = '#e53e3e87';
-      break;
-    case Difficulty.MEDIUM:
-      difficultyBgColor = '#d69e2e8a';
-      break;
-    default:
-      difficultyBgColor = '#38a16987';
-  }
+const Card: React.FC<CardProps> = ({ title, description, link, image }) => (
+  <Link role="group" _hover={{ textDecoration: 'none' }} href={link}>
+    <MetaTile height="full" width="full">
+      <MetaTileHeader>
+        <SquareImage src={image} />
+        <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
+          <Heading
+            size="lg"
+            color="white"
+            bgColor="rgba(255, 255, 255, 0.06)"
+            style={{ backdropFilter: 'blur(10px)' }}
+            lineHeight={1.8}
+            justifyContent="center"
+            px={3}
+            width="full"
+            textAlign="center"
+            borderRadius={10}
+            fontFamily="body"
+            fontWeight={400}
+          >
+            {title}
+          </Heading>
+        </Flex>
+      </MetaTileHeader>
+      <MetaTileBody pos="relative" height="full">
+        <Flex flexDir="column">
+          <Text textStyle="caption">ABOUT</Text>
 
-  let timeBgColor;
-  switch (time) {
-    case Time.LONG:
-      timeBgColor = '#e53e3e87';
-      break;
-    case Time.MEDIUM:
-      timeBgColor = '#d69e2e8a';
-      break;
-    default:
-      timeBgColor = '#38a16987';
-  }
-
-  return (
-    <Link role="group" _hover={{ textDecoration: 'none' }} href={link}>
-      <MetaTile height="full" width="full">
-        <MetaTileHeader>
-          <SquareImage src={image} />
-          <Flex px={3} w="full" pos="absolute" bottom={-6} zIndex={1}>
-            <Heading
-              size="lg"
-              color="white"
-              bgColor="rgba(255, 255, 255, 0.06)"
-              style={{ backdropFilter: 'blur(10px)' }}
-              lineHeight={1.8}
-              justifyContent="center"
-              px={3}
-              width="full"
-              textAlign="center"
-              borderRadius={10}
-              fontFamily="body"
-              fontWeight={400}
-            >
-              {title}
-            </Heading>
-          </Flex>
-        </MetaTileHeader>
-        <MetaTileBody pos="relative" height="full">
-          <Flex flexDir="column">
-            <Text textStyle="caption">ABOUT</Text>
-
-            <Text mb={2} h="3rem" fontSize="sm">
-              {description}
-            </Text>
-          </Flex>
-          <Flex justifyContent="space-between">
-            <VStack spacing={2} align="stretch">
-              <Text textStyle="caption">Difficulty</Text>
-              <Box>
-                <MetaTag size="md" fontSize="sm" bgColor={difficultyBgColor}>
-                  <Text>{difficulty}</Text>
-                </MetaTag>
-              </Box>
-            </VStack>
-            <VStack spacing={2} align="stretch">
-              <Text textStyle="caption">Time</Text>
-              <MetaTag size="md" fontSize="sm" bgColor={timeBgColor}>
-                <Text>{time}</Text>
-              </MetaTag>
-            </VStack>
-          </Flex>
-        </MetaTileBody>
-      </MetaTile>
-    </Link>
-  );
-};
+          <Text mb={2} h="3rem" fontSize="sm">
+            {description}
+          </Text>
+        </Flex>
+      </MetaTileBody>
+    </MetaTile>
+  </Link>
+);
 
 export default TheGreatHousesPage;
