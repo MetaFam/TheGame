b5cc996685bc87bcf7ae78d7214d2a10c997e5d4
HHH-GH <hayden@opie-etc.com>
2022-12-15 11:42:47 +0800

    Add filters icon to design system

---
+++ packages/design-system/src/icons/FiltersIcon.tsx
@@ -0,0 +1,13 @@
+import { createIcon } from '@chakra-ui/icons';
+import * as React from 'react';
+
+export const FiltersIcon = createIcon({
+  displayName: 'Filters',
+  path: (
+    <path
+      d="M9.5 12h-2a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5Zm-4-2H4V.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V10H.5a.5.5 0 0 0-.354.853l2.5 3a.5.5 0 0 0 .707 0l2.5-3A.5.5 0 0 0 5.5 10Zm8-6h-6a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5Zm-2 4h-4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5Zm4-8h-8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5Z"
+      fill="#fff"
+    />
+  ),
+  viewBox: '0 0 16 16',
+});
