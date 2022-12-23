466c1e828f923249a58e097cf205383784f9029a
HHH-GH <hayden@opie-etc.com>
2022-12-23 09:54:09 +0800

    Better spacing for Join CTA
    Make a larger margin on small screens, a consistent space between page header and the CTA and the lists

--- packages/web/pages/players.tsx
+++ packages/web/pages/players.tsx
@@ -77,7 +77,7 @@ const Players: React.FC<Props> = () => {
         description="See the players of MetaGame."
         url="https://metagame.wtf/players"
       />
-      <VStack w="100%" spacing={{ base: 4, md: 8, lg: 12 }} pb={8}>
+      <VStack w="100%" spacing={{ base: 6, md: 8, lg: 12 }} pb={8}>
         <Center
           fontSize={{
             base: 'sm',
@@ -86,6 +86,10 @@ const Players: React.FC<Props> = () => {
           fontWeight={{
             base: '400',
             md: '700',
+          }}
+          marginTop={{
+            base: 3, // Consistent gap between top and Players list on small screens
+            md: 0,
           }}
           w="100%"
           maxW="4xl"
