a40da84843ac5a980b85b7fb0a817341f010eccb
HHH-GH <hayden@opie-etc.com>
2022-12-24 14:53:45 +0800

    Make the appearance of Player tiles and Guild tiles match better
    Contact section is pushed to the bottom of the tile
    
    Consistent spacing between section headings and text in the tile body
    
    Bit of text shadow on the tile heading in case the player picture/guild logo has a light background

--- packages/web/components/Player/PlayerTile.tsx
+++ packages/web/components/Player/PlayerTile.tsx
@@ -37,11 +37,6 @@ type Props = {
   index?: number;
 };
 
-<<<<<<< HEAD
-=======
-// const MAX_BIO_LENGTH = 240; // Going to use line-clamp instead
-
->>>>>>> Use Chakra noOfLines to limit the length of Player/Patron/
 export const PlayerTile: React.FC<Props> = ({
   player,
   isPatron = false,
@@ -50,16 +45,6 @@ export const PlayerTile: React.FC<Props> = ({
   index,
 }) => {
   const description = getPlayerDescription(player);
-<<<<<<< HEAD
-=======
-  /** 
-  const displayDescription =
-    typeof description === 'string' && description.length > MAX_BIO_LENGTH
-      ? `${description?.substring(0, MAX_BIO_LENGTH - 9)}…`
-      : description;
-  */
-  const displayDescription = typeof description === 'string' ? description : '';
->>>>>>> Use Chakra noOfLines to limit the length of Player/Patron/
 
   const [memberships, setMemberships] = useState<GuildMembership[]>([]);
 
