11d6d79a4fb2ac0d69993376fc14712c5999dcc5
Alec LaLonde <alec@convergencelabs.com>
2023-01-26 21:56:03 -0700

    Refactored setup logic into ProfileWizardContext

--- packages/web/components/Setup/SetupDescription.tsx
+++ packages/web/components/Setup/SetupDescription.tsx
@@ -1,44 +1,54 @@
 import { Flex, Textarea } from '@metafam/ds';
+import {
+  ProfileWizardContextProvider,
+  useProfileContext,
+} from 'contexts/ProfileWizardContext';
+import { mutationComposeDBCreateProfileDescription } from 'graphql/composeDB/mutations/profile';
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
-import { ProfileWizardPane } from './ProfileWizardPane';
-import { WizardPaneCallbackProps } from './WizardPane';
+import { WizardPane } from './WizardPane';
 
-export const SetupDescription: React.FC = () => {
-  const field = 'description';
-
-  return (
-    <ProfileWizardPane
-      {...{ field }}
+export const SetupDescription: React.FC = () => (
+  <ProfileWizardContextProvider
+    query={mutationComposeDBCreateProfileDescription}
+    field="description"
+  >
+    <WizardPane
       title="Bio"
       prompt="This is where you get to tell the world who you are! What interests you? What are you up to these days? What are your goals & aspirations?? Why are you here???"
     >
-      {({ register, errored }: WizardPaneCallbackProps) => {
-        const { ref: registerRef, ...props } = register(field, {
-          maxLength: {
-            value: 420,
-            message: 'Maximum length is 420 characters.',
-          },
-        });
+      <DescriptionField />
+    </WizardPane>
+  </ProfileWizardContextProvider>
+);
 
-        return (
-          <Flex justify="center" mt={5}>
-            <Textarea
-              maxW="36rem"
-              placeholder="Come on now, don't be shy… 🙃"
-              h="10em"
-              color="white"
-              _focus={errored ? { borderColor: 'red' } : undefined}
-              bg="dark"
-              ref={(ref) => {
-                ref?.focus();
-                registerRef(ref);
-              }}
-              {...props}
-            />
-          </Flex>
-        );
-      }}
-    </ProfileWizardPane>
+const DescriptionField: React.FC = () => {
+  const { register } = useFormContext();
+  const { errored, field } = useProfileContext();
+
+  const { ref: registerRef, ...props } = register(field, {
+    maxLength: {
+      value: 420,
+      message: 'Maximum length is 420 characters.',
+    },
+  });
+
+  return (
+    <Flex justify="center" mt={5}>
+      <Textarea
+        maxW="36rem"
+        placeholder="Come on now, don't be shy… 🙃"
+        h="10em"
+        color="white"
+        _focus={errored ? { borderColor: 'red' } : undefined}
+        bg="dark"
+        ref={(ref) => {
+          ref?.focus();
+          registerRef(ref);
+        }}
+        {...props}
+      />
+    </Flex>
   );
 };
