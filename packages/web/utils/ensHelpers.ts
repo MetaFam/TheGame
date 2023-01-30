9ff3340300aa6852bf2090207f624fa3801d57c4
Sero <69639595+Seroxdesign@users.noreply.github.com>
2023-01-30 18:52:01 -0500

    Reduce lines of code
    Remove unnecessary binding

--- packages/web/utils/ensHelpers.ts
+++ packages/web/utils/ensHelpers.ts
@@ -6,12 +6,10 @@ const mainnetProvider = new ethers.providers.JsonRpcProvider(
 
 export const getAddressFromName = async (ens: string) => {
   const address = await mainnetProvider.resolveName(ens);
-  const data = address?.length === 42 ? address : ens; 
-  return data;
+   return address?.length === 42 ? address : ens; 
 };
 
 export const getNameFromAddress = async (address: string) => {
   const name = await mainnetProvider.lookupAddress(address);
-  const data  = name ? name : address;
-  return data;
+  return name ? name : address;
 }
