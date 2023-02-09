992111943d53a3547310ba37cf657a5f1b25a7fe
Sero <69639595+Seroxdesign@users.noreply.github.com>
2023-02-09 11:35:46 -0500

    add check

--- packages/web/pages/player/[username].tsx
+++ packages/web/pages/player/[username].tsx
@@ -51,7 +51,7 @@ export const PlayerPage: React.FC<Props> = ({ player }): ReactElement => {
   const username = router.query.username as string;
 
   const { data: profileInfo, isValidating } = useSWR(
-    username.includes('.') ? username : null,
+    username && username.includes('.') ? username : null,
     getPlayerData,
     {
       revalidateOnFocus: false,
