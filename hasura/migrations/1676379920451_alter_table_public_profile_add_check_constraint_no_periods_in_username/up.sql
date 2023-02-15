f64b4802933d203f27e155c819b3e8cac3d00a3d
Sero <69639595+Seroxdesign@users.noreply.github.com>
2023-02-15 10:53:42 -0500

    Delete up.sql

--- hasura/migrations/1676379920451_alter_table_public_profile_add_check_constraint_no_periods_in_username/up.sql
+++
@@ -1,2 +0,0 @@
-alter table "public"."profile" drop constraint "no_periods_in_username";
-alter table "public"."profile" add constraint "no_periods_in_username" check (username SIMILAR TO '[^-a-z0-9_]+');
