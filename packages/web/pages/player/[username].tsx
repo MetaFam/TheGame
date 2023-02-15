42205bd63cafe6c2c99d4e263f4827358250ab4d
Seroxdesign <sherifcherfa@gmail.com>
2023-02-15 17:53:26 -0500

    fix error with page load break

--- packages/web/pages/player/[username].tsx
+++ packages/web/pages/player/[username].tsx
@@ -86,9 +86,7 @@ export const PlayerPage: React.FC<Props> = ({ player }): ReactElement => {
         const name = await getENSForAddress(user?.ethereumAddress);
         const userPlayer = await getPlayer(user?.ethereumAddress);
         setPlayerData(userPlayer as Player);
-        if (name) {
-          setENS(name);
-        }
+        setENS(name || '');
       }
     };
     const getURL = async () => {
@@ -110,7 +108,12 @@ export const PlayerPage: React.FC<Props> = ({ player }): ReactElement => {
   }
 
   if (isValidating && !playerData) return <LoadingState />;
-  if (!profileInfo?.playerProfile && username.includes('.') && !isValidating)
+  if (
+    !profileInfo?.playerProfile &&
+    username &&
+    username.includes('.') &&
+    !isValidating
+  )
     return <Page404 />;
   if (!playerData && router.pathname === '/me') return <Page404 />;
 
