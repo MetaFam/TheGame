d9ca2efaf23f8a2070a94a013834484253f53d3f
Sero <69639595+Seroxdesign@users.noreply.github.com>
2023-02-15 12:39:27 -0500

    Update up.sql

---
+++ hasura/migrations/1676381264763_alter_table_public_profile_add_check_constraint_no_periods_in_username/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."profile" drop constraint if exists "no_periods_in_username";
+alter table "public"."profile" add constraint "no_periods_in_username" check (username ~ '^[A-Za-z0-9]+$'::text);
