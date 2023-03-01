d73b1cc248bb2a01355df45927812793bb93aec1
Alec LaLonde <alec@boxelderweb.com>
2023-03-01 10:10:13 -0700

    Updated model ID in definition

--- packages/utils/src/graphql/composeDBDefinition.ts
+++ packages/utils/src/graphql/composeDBDefinition.ts
@@ -1,11 +1,10 @@
 /* eslint-disable */
-
 // This is an auto-generated file, do not edit manually
 import type { RuntimeCompositeDefinition } from '@composedb/types';
 export const definition: RuntimeCompositeDefinition = {
   models: {
     Profile: {
-      id: 'kjzl6hvfrbw6c5mo6czalm1f1tflwwwn7u8ktii9w30hcut5d53nfztpg0vvhy3',
+      id: 'kjzl6hvfrbw6c9a47m2s7hoklin60281u08im4lhs8hnpo40xvfdon1j0wi5ysr',
       accountRelation: { type: 'single' },
     },
   },
