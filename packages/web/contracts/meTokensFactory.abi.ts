0782f277ac7ea8a81177f2a3049c27f6792b8efe
Seroxdesign <sherifcherfa@gmail.com>
2023-03-22 11:36:48 -0400

    add contracts, add abis, temprarily return a modal in custom blocks (build and replace with metokens modal)

---
+++ packages/web/contracts/meTokensFactory.abi.ts
@@ -0,0 +1,13 @@
+export default [
+  {
+    inputs: [
+      { internalType: 'string', name: 'name', type: 'string' },
+      { internalType: 'string', name: 'symbol', type: 'string' },
+      { internalType: 'address', name: 'diamond', type: 'address' },
+    ],
+    name: 'create',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
