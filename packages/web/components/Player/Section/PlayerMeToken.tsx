1eb411cdceaa41cb1b155eaeb7616f0959d0baf6
Seroxdesign <sherifcherfa@gmail.com>
2023-03-28 14:57:52 -0400

    fixes

--- packages/web/components/Player/Section/PlayerMeToken.tsx
+++ packages/web/components/Player/Section/PlayerMeToken.tsx
@@ -1,4 +1,4 @@
-import { Text, Wrap } from '@metafam/ds';
+import { Image, Input, Text, Wrap } from '@metafam/ds';
 import { ProfileSection } from 'components/Section/ProfileSection';
 import { Player } from 'graphql/autogen/types';
 import React, { useEffect, useState } from 'react';
@@ -10,6 +10,72 @@ type Props = {
   isOwnProfile?: boolean;
   editing?: boolean;
 };
+
+type BlockProps = {
+  symbol: string;
+  profilePicture: string;
+  address: string;
+};
+
+type SwapProps = {
+  symbol: string;
+  profilePicture: string;
+  collateral: string;
+  address: string;
+};
+
+const MeTokenSwap: React.FC<SwapProps> = ({
+  symbol,
+  profilePicture,
+  address,
+  collateral,
+}) => (
+  <Wrap>
+    <Input
+      bg="dark"
+      w="100%"
+      placeholder="Provide the URL of the content"
+      _placeholder={{ color: 'whiteAlpha.500' }}
+      value={''}
+      onChange={() => {}}
+      size="lg"
+      borderRadius={0}
+      borderColor="borderPurple"
+      fontSize="md"
+      borderWidth="2px"
+    />
+    <>{symbol + profilePicture + address + collateral}</>
+  </Wrap>
+);
+
+const MeTokenBlock: React.FC<BlockProps> = ({
+  symbol,
+  profilePicture,
+  address,
+}) => (
+  <Wrap>
+    <Image
+      src={profilePicture}
+      height="70px"
+      width="70px"
+      borderRadius={50}
+      mx="auto"
+      alt="profile picture"
+    />
+    <Wrap>
+      <Text>{symbol}</Text>
+      <Text>{address}</Text>
+
+      <MeTokenSwap
+        profilePicture={profilePicture}
+        address={address}
+        symbol={symbol}
+        collateral={''}
+      />
+    </Wrap>
+  </Wrap>
+);
+
 export const PlayerMeTokens: React.FC<Props> = ({
   player,
   isOwnProfile,
@@ -17,7 +83,6 @@ export const PlayerMeTokens: React.FC<Props> = ({
 }) => {
   const [meTokenAddress, setMeTokenAddress] = useState<string>('');
   const [meTokenData, setMeTokenData] = useState<any>('');
-
   useEffect(() => {
     const getTokenByOwner = async () => {
       await getMeToken(player?.ethereumAddress).then((r) =>
@@ -30,9 +95,7 @@ export const PlayerMeTokens: React.FC<Props> = ({
 
   useEffect(() => {
     const getInfoByToken = async () => {
-      setMeTokenData(
-        await getMeTokenInfo(meTokenAddress).then((r) => setMeTokenData(r)),
-      );
+      await getMeTokenInfo(meTokenAddress).then((r) => setMeTokenData(r));
     };
 
     getInfoByToken();
@@ -40,7 +103,7 @@ export const PlayerMeTokens: React.FC<Props> = ({
 
   return (
     <ProfileSection
-      title="Me Token"
+      title="MeToken"
       type={BoxTypes.PLAYER_ROLES}
       {...{ isOwnProfile, editing }}
     >
@@ -57,8 +120,13 @@ export const PlayerMeTokens: React.FC<Props> = ({
           </>
         ) : (
           <>
-            <>{meTokenData.symbol}</>
-            <>{meTokenData.address}</>
+            {meTokenData && (
+              <MeTokenBlock
+                profilePicture={meTokenData?.profilePicture || ''}
+                address={meTokenData?.address || ''}
+                symbol={meTokenData?.symbol || ''}
+              />
+            )}
           </>
         )}
       </Wrap>
