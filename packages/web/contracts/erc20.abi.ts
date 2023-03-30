52af83836ecf136c2f2731770e3b620fc514d417
Seroxdesign <sherifcherfa@gmail.com>
2023-03-30 11:53:16 -0400

    approve

--- packages/web/contracts/erc20.abi.ts
+++ packages/web/contracts/erc20.abi.ts
@@ -56,4 +56,29 @@ export default [
     payable: false,
     type: 'function',
   },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Approval',
+    type: 'event',
+  },
 ];
