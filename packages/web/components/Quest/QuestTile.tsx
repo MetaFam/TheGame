3fb5b9c1d25f83ab87827782b441a88298cdf7f3
HHH-GH <hayden@opie-etc.com>
2023-04-19 09:21:09 +0800

    Put back the LinkOverlay so the whole card is a link target
    BUT there needs to be a LinkOverlay for MetaTileHeader and MetaTileBody so that links inside the Quest Description don't cause hydration errors

--- packages/web/components/Quest/QuestTile.tsx
+++ packages/web/components/Quest/QuestTile.tsx
@@ -2,12 +2,14 @@ import {
   Box,
   Flex,
   Heading,
-  Link,
+  LinkBox,
+  LinkOverlay,
   MetaTile,
   MetaTileBody,
   MetaTileHeader,
   Prose,
   Text,
+  VisuallyHidden,
   VStack,
 } from '@metafam/ds';
 import { httpLink, isSGML } from '@metafam/utils';
@@ -36,45 +38,46 @@ export const QuestTile: React.FC<Props> = ({ quest }) => {
     : null;
 
   return (
-    <Link
-      role="group"
-      _hover={{ textDecoration: 'none' }}
-      href={`/quest/${quest.id}`}
-    >
-      <MetaTile minW={'300px'} height="full" width="full" cursor="pointer">
-        <MetaTileHeader>
-          <SquareImage
-            src={httpLink(quest.image) ?? BackgroundImage.src}
-            size="xl"
-          />
-          <Flex px={5} w="full" pos="absolute" bottom={-6} zIndex={1}>
-            <Heading
-              fontSize="3xl"
-              color="white"
-              bgColor="whiteAlpha.100"
-              backdropFilter="blur(10px)"
-              lineHeight={1.1}
-              px={4}
-              py={3}
-              width="full"
-              textAlign="center"
-              borderRadius={10}
-              fontFamily="body"
-              fontWeight={400}
-              textShadow="0 0 8px var(--chakra-colors-blackAlpha-400)" // v. light shadow makes the text readable if the logo/avatar is white
-              noOfLines={2}
-            >
-              {quest.title}
-            </Heading>
-          </Flex>
-        </MetaTileHeader>
-        <MetaTileBody>
+    <MetaTile minW={'300px'} height="full" width="full">
+      <MetaTileHeader>
+        <LinkBox>
+          <LinkOverlay href={`/quest/${quest.id}`}>
+            <SquareImage
+              src={httpLink(quest.image) ?? BackgroundImage.src}
+              size="xl"
+            />
+            <Box px={5} w="full" pos="absolute" bottom={-6} zIndex={1}>
+              <Heading
+                fontSize="3xl"
+                color="white"
+                bgColor="whiteAlpha.100"
+                backdropFilter="blur(10px)"
+                lineHeight={1.1}
+                px={4}
+                py={3}
+                width="full"
+                textAlign="center"
+                borderRadius={10}
+                fontFamily="body"
+                fontWeight={400}
+                textShadow="0 0 8px var(--chakra-colors-blackAlpha-400)" // v. light shadow makes the text readable if the logo/avatar is white
+                noOfLines={2}
+              >
+                {quest.title}
+              </Heading>
+            </Box>
+          </LinkOverlay>
+        </LinkBox>
+      </MetaTileHeader>
+
+      <MetaTileBody pt={0}>
+        <LinkBox height="full">
           <Flex
             direction="column"
             gap={4}
-            mt={2}
             px={2}
             pb={2}
+            pt={10}
             height="full"
             justifyItems="space-between"
           >
@@ -125,8 +128,20 @@ export const QuestTile: React.FC<Props> = ({ quest }) => {
               )}
             </VStack>
           </Flex>
-        </MetaTileBody>
-      </MetaTile>
-    </Link>
+          {/**
+           * A hidden link overlay that makes the MetaTileBody a clickable link
+           * Why?
+           * 1. Links are allowed in the Quest Description
+           * 2. If there is a link in the Quest Description it causes a hydration error because you can't have links inside links
+           * 3. So the link overlay cannot wrap the description
+           * 4. If the link overlay is added to the Skills or Roles Tags, it can't break out of the Vstack that wraps it, and the description is then not included in the link target
+           * 5. So there's a hidden link down here
+           */}
+          <LinkOverlay href={`/quest/${quest.id}`}>
+            <VisuallyHidden as="span">Read more</VisuallyHidden>
+          </LinkOverlay>
+        </LinkBox>
+      </MetaTileBody>
+    </MetaTile>
   );
 };
