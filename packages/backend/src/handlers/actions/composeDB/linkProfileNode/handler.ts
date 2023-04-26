3933b7537e6b43d24f1ceb77effefb0edb61263e
Alec LaLonde <alec@boxelderweb.com>
2023-04-25 21:12:12 -0600

    Added context to 'port data' modal

--- packages/backend/src/handlers/actions/composeDB/linkProfileNode/handler.ts
+++ packages/backend/src/handlers/actions/composeDB/linkProfileNode/handler.ts
@@ -47,7 +47,7 @@ export default async (req: Request, res: Response): Promise<void> => {
     const controllerEthAddress = controller.substring(
       controller.lastIndexOf(':') + 1,
     );
-    // controller is something like did:pkh:eip155:137:0x4c81e51aa103490838fd9db1d859a5fcd8276324
+    // controller is something like did:pkh:eip155:1:0x4c81e51aa103490838fd9db1d859a5fcd8276324
     // make sure this is a mainnet address!
     if (
       controller.startsWith('did:pkh') &&
