a1708a30b117e8b83dba66cf05caff95337ba50a
HHH-GH <hayden@opie-etc.com>
2023-05-07 20:01:01 +0800

    The main image on the quest details is 1/1 ratio til md size, then 4/3 ratio after that

--- packages/web/components/Quest/QuestDetailsImage.tsx
+++ packages/web/components/Quest/QuestDetailsImage.tsx
@@ -9,9 +9,7 @@ export const QuestDetailsImage: React.FC<Props> = ({ src }) => (
   <AspectRatio
     ratio={{
       base: 1 / 1,
-      md: 2 / 1,
-      lg: 4 / 3,
-      xl: 2 / 1,
+      md: 4 / 3,
     }}
   >
     <Image src={src} alt="" borderRadius={10} fit="cover" />
