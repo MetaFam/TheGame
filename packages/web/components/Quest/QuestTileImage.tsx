8929d53449f045044c8c5a1bbe3de9ca20ba8f40
HHH-GH <hayden@opie-etc.com>
2023-05-07 19:58:14 +0800

    A component to use Chakra AspectRatio for the Quest Tile image
    Plus rounded corners for the Quest Images to match the Figma design

--- packages/web/components/Quest/QuestTileImage.tsx
+++ packages/web/components/Quest/QuestTileImage.tsx
@@ -5,15 +5,8 @@ type Props = {
   src: string;
 };
 
-export const QuestDetailsImage: React.FC<Props> = ({ src }) => (
-  <AspectRatio
-    ratio={{
-      base: 1 / 1,
-      md: 2 / 1,
-      lg: 4 / 3,
-      xl: 2 / 1,
-    }}
-  >
+export const QuestTileImage: React.FC<Props> = ({ src }) => (
+  <AspectRatio ratio={1}>
     <Image src={src} alt="" borderRadius={10} fit="cover" />
   </AspectRatio>
 );
