04f11aefc9349634b9fe9e6ea906de0c29c53d48
Seroxdesign <sherifcherfa@gmail.com>
2023-07-04 05:48:05 -0400

    set up boxtype, playersection and dework profile component

--- packages/web/utils/boxTypes.ts
+++ packages/web/utils/boxTypes.ts
@@ -14,6 +14,7 @@ export const BoxTypes = {
   PLAYER_ROLES: 'player-roles',
   PLAYER_COMPLETED_QUESTS: 'completed-quests',
   PLAYER_METOKENS: 'meToken',
+  DEWORK: 'dework',
   // Guild Profile Boxes
   GUILD_HERO: 'guild-hero',
   GUILD_SKILLS: 'guild-skills',
