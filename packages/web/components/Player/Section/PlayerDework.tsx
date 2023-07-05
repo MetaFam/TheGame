1d58e82e62a365a779ad78658ab9c833ff639848
Seroxdesign <sherifcherfa@gmail.com>
2023-07-05 11:22:45 -0400

    get data from dework

--- packages/web/components/Player/Section/PlayerDework.tsx
+++ packages/web/components/Player/Section/PlayerDework.tsx
@@ -1,7 +1,8 @@
-import { Text, Wrap } from '@metafam/ds';
+import { Box, Text, Wrap } from '@metafam/ds';
 import { ProfileSection } from 'components/Section/ProfileSection';
 import { Player } from 'graphql/autogen/types';
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { getDeworkData } from 'utils/dework';
 
 type Props = {
   player: Player;
@@ -13,10 +14,25 @@ export const PlayerDework: React.FC<Props> = ({
   player,
   isOwnProfile,
   editing,
-}) => (
-  <ProfileSection title="MeToken" {...{ isOwnProfile, editing }}>
-    <Wrap mb={4} justify="center">
-      <Text>Dework Profile</Text>
-    </Wrap>
-  </ProfileSection>
-);
+}) => {
+  const [deworkData, setDeworkData] = useState<any>();
+
+  useEffect(() => {
+    const getData = async () => {
+      await getDeworkData(player?.ethereumAddress);
+    };
+    getData().then(setDeworkData);
+  }, [player?.ethereumAddress]);
+
+  return (
+    <ProfileSection title="Dework Profile" {...{ isOwnProfile, editing }}>
+      <Wrap>
+        {deworkData && (
+          <Box mb={4} display={'flex'} flexDirection={'column'}>
+            <Text>Tasks completed: {deworkData?.tasks.length}</Text>
+          </Box>
+        )}
+      </Wrap>
+    </ProfileSection>
+  );
+};
