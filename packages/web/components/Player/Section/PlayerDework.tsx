6984d3e7f11734cc38cba5b2df679c4c71e76fd4
Seroxdesign <sherifcherfa@gmail.com>
2023-07-10 18:27:27 -0400

    process data and display

--- packages/web/components/Player/Section/PlayerDework.tsx
+++ packages/web/components/Player/Section/PlayerDework.tsx
@@ -2,7 +2,7 @@ import { Box, Text, Wrap } from '@metafam/ds';
 import { ProfileSection } from 'components/Section/ProfileSection';
 import { Player } from 'graphql/autogen/types';
 import React, { useEffect, useMemo, useState } from 'react';
-import { getDeworkData } from 'utils/dework';
+import { getDeworkData, processDeworkData } from 'utils/dework';
 
 type Props = {
   player: Player;
@@ -19,17 +19,32 @@ export const PlayerDework: React.FC<Props> = ({
 
   useEffect(() => {
     const getData = async () => {
-      await getDeworkData(player?.ethereumAddress);
+      await getDeworkData(player?.ethereumAddress).then((res: any) =>
+        setDeworkData(res),
+      );
     };
-    getData().then(setDeworkData);
+    getData();
   }, [player?.ethereumAddress]);
+
+  const processedData = useMemo(
+    () => processDeworkData(deworkData),
+    [deworkData],
+  );
 
   return (
     <ProfileSection title="Dework Profile" {...{ isOwnProfile, editing }}>
       <Wrap>
-        {deworkData && (
+        {deworkData && processedData && (
           <Box mb={4} display={'flex'} flexDirection={'column'}>
             <Text>Tasks completed: {deworkData?.tasks.length}</Text>
+            <Text>Tags: {processedData?.uniqueTags.length}</Text>
+            <Text>
+              Organisations: {processedData?.uniqueOrganisations.length}
+            </Text>
+            <Text>
+              Earned: {(processedData.totalEarnedInUSDC / 10 ** 18).toFixed(2)}$
+              (only counting USDC)
+            </Text>
           </Box>
         )}
       </Wrap>
