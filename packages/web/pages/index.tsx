57da40646bbe1057a4809c12ece4500d46e483b2
vidvidvid <weetopol@gmail.com>
2023-07-17 15:59:16 +0200

    add what people are saying section

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -165,7 +165,7 @@ const Landing: React.FC<Props> = ({ patrons, guilds }) => {
           nextSection={sections[8]}
           activeSectionIndex={section}
         />
-        <JoinUs section={sections[7]} activeSectionIndex={section} />
+        <JoinUs section={sections[8]} activeSectionIndex={section} />
       </PageContainer>
       <SectionWayPoints currentWaypoint={section} />
       <SocialsDesktop />
