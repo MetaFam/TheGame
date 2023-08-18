9ced0fa25bff53bb158ec55ec89e7796b66ee640
HHH-GH <hayden@opie-etc.com>
2023-08-18 16:43:09 +0800

    Font for form headings matches new design

--- packages/web/pages/quest/create.tsx
+++ packages/web/pages/quest/create.tsx
@@ -1,4 +1,4 @@
-import { MetaHeading, useToast } from '@metafam/ds';
+import { Heading, useToast } from '@metafam/ds';
 import { PageContainer } from 'components/Container';
 import { CreateQuestFormInputs, QuestForm } from 'components/Quest/QuestForm';
 import { HeadComponent } from 'components/Seo';
@@ -91,7 +91,15 @@ const CreateQuestPage: React.FC<Props> = ({
         description="Create a quest for MetaGame."
         url="https://metagame.wtf/quest/create"
       />
-      <MetaHeading mb={4}>New Quest</MetaHeading>
+      <Heading
+        as="h1"
+        fontFamily="body"
+        fontWeight="600"
+        fontSize={{ base: '4xl', md: '6xl' }}
+        mb={5}
+      >
+        New Quest
+      </Heading>
 
       <QuestForm
         {...{ guilds, skillChoices, onSubmit, roleChoices }}
