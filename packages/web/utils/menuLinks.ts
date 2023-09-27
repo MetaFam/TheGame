0523bc13476a68596d9518f8986a8f2cddc51456
luxumbra <me@luxumbra.dev>
2023-09-28 00:39:11 +0100

    Commit wip

--- packages/web/utils/menuLinks.ts
+++ packages/web/utils/menuLinks.ts
@@ -43,31 +43,36 @@ export const MenuSectionLinks: MenuLinkSet[] = [
     type: 'internal-link',
     url: '/community',
   },
+  // {
+  //   label: 'learn',
+  //   type: 'menu',
+  //   menuItems: [
+  //     {
+  //       title: 'Roles',
+  //       explainerText:
+  //         'Find about all roles in MetaGame, see which ones are open & how to play them',
+  //       // url: 'https://meta-game.notion.site/Internal-Roles-Guilds-bec3a0437f684322b650dbb7aca616e8',
+  //       url: '/roles',
+  //       icon: 'roles',
+  //     },
+  //     {
+  //       title: 'Playbooks',
+  //       explainerText: descriptions.playbooks,
+  //       url: '/learn/playbooks',
+  //       icon: 'playbooks',
+  //     },
+  //     {
+  //       title: 'The Great Houses',
+  //       explainerText: descriptions.thegreathouses,
+  //       url: '/learn/thegreathouses',
+  //       icon: 'thegreathouses',
+  //     },
+  //   ],
+  // },
   {
-    label: 'learn',
-    type: 'menu',
-    menuItems: [
-      {
-        title: 'Roles',
-        explainerText:
-          'Find about all roles in MetaGame, see which ones are open & how to play them',
-        // url: 'https://meta-game.notion.site/Internal-Roles-Guilds-bec3a0437f684322b650dbb7aca616e8',
-        url: '/roles',
-        icon: 'roles',
-      },
-      {
-        title: 'Playbooks',
-        explainerText: descriptions.playbooks,
-        url: '/learn/playbooks',
-        icon: 'playbooks',
-      },
-      {
-        title: 'The Great Houses',
-        explainerText: descriptions.thegreathouses,
-        url: '/learn/thegreathouses',
-        icon: 'thegreathouses',
-      },
-    ],
+    label: 'paths & playbooks',
+    type: 'internal-link',
+    url: '/paths-and-playbooks'
   },
   {
     label: 'quests',
