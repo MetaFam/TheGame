e29b46cee4802bf7045453d2b80327a3757db5e3
Udit Takkar <53316345+Udit-takkar@users.noreply.github.com>
2023-10-04 04:08:57 +0530

    feat: global search (#1587)
    * feat: global search
    
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>
    
    * add: name field
    
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>
    
    * fix: bugs
    
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>
    
    * fix: err
    
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>
    
    * fix: add mobile support
    
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>
    
    * GitHub Actions use node 18 to run the seeding script 🍬
    
    * feat: add cmd +k search
    
    * fix: improvements
    
    * fix: improvements
    
    * chore: name
    
    * 🐌 `yarn lint --fix`
    
    * fix: improvements
    
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>
    
    * fix: body empty
    
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>
    
    * fix: guild search limit
    
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>
    
    * chore: typo
    
    ---------
    
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>
    Co-authored-by: vidvidvid <35112939+vidvidvid@users.noreply.github.com>
    Co-authored-by: Sero <69639595+Seroxdesign@users.noreply.github.com>
    Co-authored-by: δυς <dys@dhappy.org>

---
+++ packages/web/pages/search/guilds.tsx
@@ -0,0 +1,42 @@
+import { PageContainer } from 'components/Container';
+import { GuildList } from 'components/Guild/GuildList';
+import { GuildNotFound } from 'components/Guild/GuildNotFound';
+import SearchFilters from 'components/SearchFilters';
+import { HeadComponent } from 'components/Seo';
+import { GuildFragment } from 'graphql/autogen/types';
+import { searchGuilds } from 'graphql/queries/guild';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { GlobalFilters } from 'utils/GlobalSearch';
+
+const GuildSearchPage: React.FC = () => {
+  const { query } = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [guilds, setGuilds] = useState<Array<GuildFragment>>([]);
+  const search: string = decodeURI(query.q as string);
+  useEffect(() => {
+    if (search) {
+      setIsLoading(true);
+      const getData = async () => {
+        try {
+          const { guilds: gs } = await searchGuilds({ search });
+          setGuilds(gs);
+        } catch (err) {
+          console.error('Unable to search guilds', err);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+      getData();
+    }
+  }, [search]);
+  return (
+    <PageContainer>
+      <HeadComponent url="https://my.metagame.wtf/community/search" />
+      <SearchFilters activeFilter={GlobalFilters.GUILDS} {...{ search }} />
+      <GuildList {...{ guilds }} />
+      {!isLoading && guilds.length === 0 && <GuildNotFound />}
+    </PageContainer>
+  );
+};
+export default GuildSearchPage;
