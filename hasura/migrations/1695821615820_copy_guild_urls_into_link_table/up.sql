0ae946afcc25873ea9d4ca60e202e254c941ca4b
Sero <69639595+Seroxdesign@users.noreply.github.com>
2023-10-03 12:19:01 -0400

    Update up.sql

--- hasura/migrations/1695821615820_copy_guild_urls_into_link_table/up.sql
+++ hasura/migrations/1695821615820_copy_guild_urls_into_link_table/up.sql
@@ -1,3 +1,3 @@
 INSERT INTO link(type, guild_id, url, name) (SELECT 'TWITTER', id, twitter_url, 'Twitter' FROM guild WHERE twitter_url is not null);
 INSERT INTO link(type, guild_id, url, name) (SELECT 'DISCORD', id, discord_invite_url, 'Discord' FROM guild WHERE discord_invite_url is not null);
-INSERT INTO link(type, guild_id, url, name) (SELECT 'GITHUB', id, github_url, 'Github' FROM guild WHERE github_url is not null);
+INSERT INTO link(type, guild_id, url, name) (SELECT 'GITHUB', id, github_url, 'GitHub' FROM guild WHERE github_url is not null);
