6c0f3f22e602b75d64434ba46546554caa91d4ce
dysbulic <dys@dhappy.org>
2023-10-16 20:21:49 -0400

    fixing `trigger` `drop`s 📬

--- hasura/migrations/1697478824592_alter_table_public_xp_add_column_updated_at/down.sql
+++ hasura/migrations/1697478824592_alter_table_public_xp_add_column_updated_at/down.sql
@@ -1,4 +1,4 @@
 ALTER TABLE public.xp DROP COLUMN updated_at CASCADE;
-DROP TRIGGER IF EXISTS public.set_current_timestamp_updated_at
+DROP TRIGGER IF EXISTS set_current_timestamp_updated_at
   ON public.xp
 ;
