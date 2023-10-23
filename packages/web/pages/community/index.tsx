c001ec3d706ad7ef126c65efc86fa93b24bb4871
Seroxdesign <sherifcherfa@gmail.com>
2023-10-23 08:37:38 -0400

    make community page dynamic, reduce repitition

--- packages/web/pages/community/index.tsx
+++ packages/web/pages/community/index.tsx
@@ -1,8 +1,5 @@
 import {
   Box,
-  Button,
-  Flex,
-  HStack,
   MetaHeading,
   Tab,
   TabList,
@@ -19,16 +16,19 @@ import GuildsPage from 'pages/guilds';
 import PatronsPage from 'pages/patrons';
 import Players from 'pages/players';
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export const getStaticProps = async () => {
   const patronsLimit = 150;
   const patrons = await getPatrons(patronsLimit);
+
   const pSeedPrice = await getPSeedPrice().catch((error) => {
     console.error('Error fetching pSeed price', error);
     return null;
   });
+
   return {
     props: {
       guilds: await getGuilds(),
@@ -43,64 +43,84 @@ const UnifiedCommunityPage: React.FC<Props> = ({
   guilds,
   patrons,
   pSeedPrice,
-}) => (
-  <PageContainer>
-    <VStack>
-      <MetaHeading>Community</MetaHeading>
-      <Box>
-        <Tabs
-          mt="2em"
-          w={{
-            lg: '100%',
-          }}
-        >
-          <TabList ml={{ sm: '0em', lg: '4em' }} mr={{ sm: '0em', lg: '4em' }}>
-            <Tab
-              _selected={{
-                color: 'teal.200',
-                borderBottom: '2px solid #81E6D9',
-              }}
-              w="100%"
-            >
-              Players
-            </Tab>
-            <Tab
-              _selected={{
-                color: 'teal.200',
-                borderBottom: '2px solid #81E6D9',
-              }}
-              w="100%"
-            >
-              Guilds
-            </Tab>
-            <Tab
-              _selected={{
-                color: 'teal.200',
-                borderBottom: '2px solid #81E6D9',
-              }}
-              w="100%"
-            >
-              Patrons
-            </Tab>
-            {/* <Tab _selected={{ color: 'teal.200' }} w='100%'>Elders</Tab> */}
-          </TabList>
-          <TabPanels w="100%" p="0">
-            <TabPanel>
-              <Players />
-            </TabPanel>
-            <TabPanel>
-              <GuildsPage guilds={guilds} />
-            </TabPanel>
-            <TabPanel>
-              <PatronsPage patrons={patrons} pSeedPrice={pSeedPrice} />
-            </TabPanel>
-            {/* <TabPanel>
-                Elders
-              </TabPanel> */}
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </VStack>
-  </PageContainer>
-);
+}) => {
+
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState<{ link: string, index: number }>({ link: 'Players', index: 0 });
+
+  const communityTabs = [
+    { link: 'Players', component: <Players /> },
+    { link: 'Guilds', component: <GuildsPage guilds={guilds} />},
+    { link: 'Patrons', component: <PatronsPage patrons={patrons} pSeedPrice={pSeedPrice} /> }
+  ]
+
+  useEffect(() => {
+    const { query } = router;
+    if (query.tab) {
+      const index = communityTabs.findIndex(tab => tab.link === query.tab);
+      setActiveTab({ link: query.tab as string, index });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.query]);
+
+  const handleTabChange = (index: number) => {
+    const tab = communityTabs[index];
+    setActiveTab({ link: tab.link, index });
+    router.push(`/community/?tab=${tab.link}`, undefined, { shallow: true });
+  };
+
+  return (
+    <PageContainer>
+      <VStack>
+        <MetaHeading>Community</MetaHeading>
+        <Box>
+          <Tabs
+            mt="2em"
+            w={{
+              lg: '100%',
+            }}
+            index={activeTab.index}
+            onChange={handleTabChange}
+          >
+            <TabList ml={{ sm: '0em', lg: '4em' }} mr={{ sm: '0em', lg: '4em' }}>
+              <>
+                {
+                  communityTabs.map(({ link }) => {
+                    return(
+                      <Tab
+                        key={`tab-${link}`}
+                        _selected={{
+                          color: 'teal.200',
+                          borderBottom: '2px solid #81E6D9',
+                        }}
+                        w="100%"
+                      >
+                        {link}
+                      </Tab>
+                    )
+                  })
+                }
+              </>
+            </TabList>
+            <TabPanels w="100%" p="0">
+              
+                {
+                  communityTabs.map(({ link, component }) => {
+                    return (
+                      <TabPanel key={`panel-${link}`}>
+                        {component}
+                      </TabPanel>
+                    )
+                  })
+                }
+              
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </VStack>
+    </PageContainer>
+  )
+  
+};
+
 export default UnifiedCommunityPage;
