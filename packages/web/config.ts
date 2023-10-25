f5d7236c28314b52657348b2ceec869df54caeb8
luxumbra <me@luxumbra.dev>
2023-10-25 19:38:52 +0100

    forgot to change the url back

--- packages/web/config.ts
+++ packages/web/config.ts
@@ -29,5 +29,5 @@ export const CONFIG = {
   openseaAPIKey: process.env.OPENSEA_API_KEY,
   alchemyAPIKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
   mainnetRPC: process.env.NEXT_PUBLIC_MAINNET_RPC || 'https://eth.llamarpc.com',
-  metagameCalendarBackend: 'http://localhost:1188/events',
+  metagameCalendarBackend: 'https://mgapi.luxumbra.dev/events',
 };
