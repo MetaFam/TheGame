5c5a3a509f9884d8e01ca4597ab64b7bd2e9206e
luxumbra <me@luxumbra.dev>
2023-10-30 13:09:23 +0000

    replace localhost api url with

--- packages/web/config.ts
+++ packages/web/config.ts
@@ -29,5 +29,5 @@ export const CONFIG = {
   openseaAPIKey: process.env.OPENSEA_API_KEY,
   alchemyAPIKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
   mainnetRPC: process.env.NEXT_PUBLIC_MAINNET_RPC || 'https://eth.llamarpc.com',
-  metagameCalendarBackend: 'http://localhost:3000/api/events',
+  metagameCalendarBackend: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/events`,
 };
