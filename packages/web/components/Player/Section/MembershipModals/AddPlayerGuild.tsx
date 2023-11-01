2708eefa166c7acc04199ce6c75c3c2e2b394e29
Seroxdesign <sherifcherfa@gmail.com>
2023-11-01 12:56:40 -0400

    guild stuff

--- packages/web/components/Player/Section/MembershipModals/AddPlayerGuild.tsx
+++ packages/web/components/Player/Section/MembershipModals/AddPlayerGuild.tsx
@@ -13,46 +13,61 @@ import {
 import { GuildSearchBar } from 'components/GuildSearchBar';
 import { Player } from 'graphql/autogen/types';
 import React from 'react';
+import { UnverifiedGuildForm } from 'components/Guild/UnverifiedGuildForm';
 
 export const AddPlayerGuild: React.FC<{
   isOpen: boolean;
   onClose: () => void;
   player: Player;
-}> = ({ isOpen, onClose, player }) => (
-  <Modal {...{ isOpen, onClose }}>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>
-        Add Guild Membership
-        <Text
-          fontStyle="italic"
-          color="gray.400"
-          textAlign="center"
-          fontSize="md"
-          mt={3}
-          mb={10}
-        >
-          Search for a Guild
-        </Text>
-      </ModalHeader>
-      <ModalCloseButton />
-      <ModalBody p={[0, 6]}>
-        <GuildSearchBar player={player} />
-      </ModalBody>
-      <Button variant="outline" w="20%" ml="40%" leftIcon={<AddIcon />}>
-        Add New Guild
-      </Button>
-      <ModalFooter mt={6} justifyContent="center">
-        <Button
-          variant="ghost"
-          onClick={onClose}
-          color="landing450"
-          _hover={{ bg: '#FFFFFF11' }}
-          _active={{ bg: '#FF000011' }}
-        >
-          Go Back to Profile
-        </Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
-);
+}> = ({ isOpen, onClose, player }) => {
+  const [addUnverifiedGuildView, setAddUnverifiedGuildView] = React.useState(false);
+  
+  return (
+    <Modal {...{ isOpen, onClose }} scrollBehavior='outside'>
+      <ModalOverlay />
+      <ModalContent>
+        {
+          addUnverifiedGuildView ? (
+            <UnverifiedGuildForm onSubmit={() => console.log('hi')} />
+          ) : (
+            <>
+            <ModalHeader>
+                      Add Guild Membership
+                      <Text
+                        fontStyle="italic"
+                        color="gray.400"
+                        textAlign="center"
+                        fontSize="md"
+                        mt={3}
+                        mb={10}
+                      >
+                        Search for a Guild
+                      </Text>
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody p={[0, 6]}>
+                      <GuildSearchBar player={player} />
+                    </ModalBody>
+                    <Button variant="outline" w="20%" ml="40%" leftIcon={<AddIcon />} onClick={() => setAddUnverifiedGuildView(true)}>
+                      Add New Guild
+                    </Button>
+                    <ModalFooter mt={6} justifyContent="center">
+                      <Button
+                        variant="ghost"
+                        onClick={onClose}
+                        color="landing450"
+                        _hover={{ bg: '#FFFFFF11' }}
+                        _active={{ bg: '#FF000011' }}
+                      >
+                        Go Back to Profile
+                      </Button>
+                    </ModalFooter>
+            </>
+          )
+        }
+       
+      </ModalContent>
+    </Modal>
+  )
+}
+
