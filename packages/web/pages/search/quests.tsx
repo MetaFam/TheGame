3937a240fb71bbebbec1a18f5bb3d6e3e6bffa90
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2023-12-16 17:46:41 +0530

    feat: complete quest search page

--- packages/web/pages/search/quests.tsx
+++ packages/web/pages/search/quests.tsx
@@ -1,12 +1,20 @@
+import { Box, LoadingState, Text, VStack } from '@metafam/ds';
 import { PageContainer } from 'components/Container';
+import { QuestFilter } from 'components/Quest/QuestFilter';
+import { QuestList } from 'components/Quest/QuestList';
 import { questListDescriptionCss } from 'components/Quest/QuestListDescriptionCss';
 import { HeadComponent } from 'components/Seo';
+import { getPlayerRoles } from 'graphql/queries/enums/getRoles';
 import { useQuestFilter } from 'lib/hooks/quests';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { errorHandler } from 'utils/errorHandler';
+
+type RoleChoices = Awaited<Promise<ReturnType<typeof getPlayerRoles>>>;
 
 const QuestsSearchPage: React.FC = () => {
   const { query } = useRouter();
+  const [roleChoices, setRoleChoices] = useState<RoleChoices>([]);
   const {
     quests,
     aggregates,
@@ -15,6 +23,7 @@ const QuestsSearchPage: React.FC = () => {
     queryVariables,
     setQueryVariable,
   } = useQuestFilter();
+
   const searchQuery = Array.from(query.q ?? '')[0];
 
   const search = decodeURI(searchQuery);
@@ -23,9 +32,40 @@ const QuestsSearchPage: React.FC = () => {
     setQueryVariable('search', `%${search}%`);
   }, [search, setQueryVariable]);
 
+  useEffect(() => {
+    const setPlayerRoles = async () => {
+      try {
+        const res = await getPlayerRoles();
+        setRoleChoices(res);
+      } catch (err) {
+        errorHandler(err as Error);
+      }
+    };
+    setPlayerRoles();
+  }, []);
+
   return (
     <PageContainer sx={questListDescriptionCss}>
       <HeadComponent url="https://my.metagame.wtf/community/search" />
+      <VStack w="100%" spacing={8} pb={8}>
+        <QuestFilter
+          quests={quests || []}
+          {...{
+            roleChoices,
+            aggregates,
+            queryVariables,
+            setQueryVariable,
+          }}
+        />
+
+        <Box w="full" maxW="7xl">
+          <Box w="full">
+            {fetching && <LoadingState />}
+            {error && <Text>{`Error: ${error.message}`}</Text>}
+            {quests && !fetching && <QuestList {...{ quests }} />}
+          </Box>
+        </Box>
+      </VStack>
     </PageContainer>
   );
 };
