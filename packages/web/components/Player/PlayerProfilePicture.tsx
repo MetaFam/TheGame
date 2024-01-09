f68862baddd28947b8e19f8a600893d8c81feb75
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-01-09 07:14:17 -0500

    image fallback fix

--- packages/web/components/Player/PlayerProfilePicture.tsx
+++ packages/web/components/Player/PlayerProfilePicture.tsx
@@ -2,7 +2,7 @@ import type { AvatarProps } from '@metafam/ds';
 import { SquareImage } from 'components/SquareImage';
 import type { Player } from 'graphql/autogen/types';
 import type { GuildPlayer } from 'graphql/types';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { getPlayerImage } from 'utils/playerHelpers';
 
 type PlayerProfilePictureProps = AvatarProps & {
@@ -14,11 +14,33 @@ export const PlayerProfilePicture: React.FC<PlayerProfilePictureProps> = ({
   src,
   ...props
 }) => {
-  const avatarImg = useMemo(() => src ?? getPlayerImage(player), [player, src]);
+  const avatarImg = useMemo(() => getPlayerImage(player), [player, src]);
+  
+  const [imageURL, setImageURL] = useState(avatarImg.profileIcon);
+  const profileImage = avatarImg.link;
+
+  useEffect(() => {
+    if (!profileImage) return;
+    const image = new Image();
+    image.src = profileImage;
+
+    image.onload = () => {
+      setImageURL(profileImage);
+    };
+
+    image.onerror = () => {
+      console.error('Original image failed to load.');
+    };
+
+    return () => {
+      image.onload = null;
+      image.onerror = null;
+    };
+  }, [profileImage]);
 
   return (
     <>
-      <SquareImage src={avatarImg ?? undefined} {...props} />
+      <SquareImage src={imageURL} {...props} />
     </>
   );
 };
