7bbf92c320a635acaa3428bc5eb63659ff95cfc1
dysbulic <dys@dhappy.org>
2024-01-10 19:10:26 -0500

    trying to fix web3.storage… 🦨

--- packages/web/utils/uploadHelpers.ts
+++ packages/web/utils/uploadHelpers.ts
@@ -1,3 +1,6 @@
+import * as Delegation from '@ucanto/core/delegation';
+import * as Client from '@web3-storage/w3up-client';
+
 export const uploadFile = async (file: File): Promise<string> => {
   const formData = new FormData();
   formData.append('ipfsHash', file);
@@ -28,4 +31,26 @@ export const uploadFiles = async (formData: FormData) => {
   }
 
   return response;
+};
+
+export const directUpload = async (files: Array<File>) => {
+  const client = await Client.create();
+
+  const response = await fetch('/api/w3s-delegation', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ did: client.did().toString() }),
+  });
+  const raw = await response.arrayBuffer();
+  const delegation = await Delegation.extract(new Uint8Array(raw));
+  if (!delegation.ok) {
+    throw new Error('Failed to extract delegation', {
+      cause: delegation.error,
+    });
+  }
+
+  const space = await client.addSpace(delegation.ok);
+  client.setCurrentSpace(space.did());
+
+  return client.uploadDirectory(files);
 };
