7232009071721c464a094ca3284c9ca3a37d4fa7
vidvidvid <weetopol@gmail.com>
2024-01-18 14:40:53 +0100

    wip
    tabs
    
    wip2

---
+++ packages/web/components/Seeds/Trade.tsx
@@ -0,0 +1,36 @@
+import { Box, Button, Image, Link, Text, VStack } from '@metafam/ds';
+import Balancer from 'assets/seeds/balancer-trade.png';
+import React from 'react';
+import { GoLinkExternal } from 'react-icons/go';
+
+export const Trade: React.FC = () => (
+  <VStack spacing={4} my={8}>
+    <Text>This is a place for you to sell or buy Seeds.</Text>
+    <Text>
+      As our own UI still doesn't exist, simply follow the link & search for
+      "Seed".
+    </Text>
+    <Image
+      borderRadius="12px"
+      border="8px solid rgba(255, 255, 255, 0.10)"
+      src={Balancer.src}
+      alt="Droplet1"
+      mx="auto"
+      my={8}
+    />
+
+    <Box>
+      <Button
+        as={Link}
+        href="https://app.balancer.fi/#/polygon/swap"
+        target="_blank"
+        size="md"
+        w="full"
+        colorScheme="purple"
+      >
+        <Text mr={2}>Trade Seeds (Balancer)</Text>
+        <GoLinkExternal />
+      </Button>
+    </Box>
+  </VStack>
+);
