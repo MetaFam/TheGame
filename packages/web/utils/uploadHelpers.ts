198278d7b88f667eb873b516b361e0e40fb4278a
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-01-19 09:07:03 -0500

    Revert "removing obsolesced web3.storage integration 🧛"
    This reverts commit 05bb53bc769eed297efadd50ea464fa8aba670b4.

--- packages/web/utils/uploadHelpers.ts
+++ packages/web/utils/uploadHelpers.ts
@@ -1,7 +1,39 @@
 import * as Delegation from '@ucanto/core/delegation';
 import * as Client from '@web3-storage/w3up-client';
 
-async function getW3SClient() {
+export const uploadFile = async (file: File): Promise<string> => {
+  const formData = new FormData();
+  formData.append('ipfsHash', file);
+
+  const { ipfsHash } = await uploadFiles(formData);
+
+  if (!ipfsHash) {
+    throw new Error("Uploaded file but didn't get a response back.");
+  }
+
+  return ipfsHash;
+};
+
+export const uploadFiles = async (formData: FormData) => {
+  const result = await fetch(`/api/storage`, {
+    method: 'POST',
+    body: formData,
+    credentials: 'include',
+  });
+
+  const response = await result.json();
+  const { error } = response;
+
+  if (result.status >= 400 || error) {
+    throw new Error(
+      `web3.storage ${result.status} response: "${error ?? result.statusText}"`,
+    );
+  }
+
+  return response;
+};
+
+export const directUpload = async (files: Array<File>) => {
   const client = await Client.create();
 
   const response = await fetch('/api/w3s-delegation', {
@@ -20,15 +52,5 @@ async function getW3SClient() {
   const space = await client.addSpace(delegation.ok);
   client.setCurrentSpace(space.did());
 
-  return client;
-}
-
-export const uploadFile = async (file: File) => {
-  const client = await getW3SClient();
-  return client.uploadFile(file);
-};
-
-export const uploadFiles = async (files: Array<File>) => {
-  const client = await getW3SClient();
   return client.uploadDirectory(files);
 };
