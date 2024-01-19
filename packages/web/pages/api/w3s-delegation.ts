36178811bdcfa9001cc90e048b82f7f7e42be24e
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-01-19 09:09:08 -0500

    Revert "Revert "trying to fix web3.storage… 🦨""
    This reverts commit 5869e6dffc09ba372fb6a0c8287d39a67016b519.

---
+++ packages/web/pages/api/w3s-delegation.ts
@@ -0,0 +1,69 @@
+import { CarReader } from '@ipld/car';
+import * as DID from '@ipld/dag-ucan/did';
+import * as Delegation from '@ucanto/core/delegation';
+import type { Block } from '@ucanto/interface';
+import * as Signer from '@ucanto/principal/ed25519';
+import * as Client from '@web3-storage/w3up-client';
+import { CONFIG } from 'config';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+async function w3sDelegation(did: string) {
+  if (!CONFIG.web3StorageKey) {
+    throw new Error('$WEB3_STORAGE_KEY is not set.');
+  }
+  const principal = Signer.parse(CONFIG.web3StorageKey);
+  const client = await Client.create({ principal });
+
+  if (!CONFIG.web3StorageProof) {
+    throw new Error('$WEB3_STORAGE_PROOF is not set.');
+  }
+  const proof = await parseProof(CONFIG.web3StorageProof);
+  const space = await client.addSpace(proof);
+  await client.setCurrentSpace(space.did());
+
+  const audience = DID.parse(did);
+  const abilities = ['store/add', 'upload/add'];
+  const expiration = Math.floor(Date.now() / 1000) + 60 * 60 * 24; // 24 hours from now
+  const delegation = await client.createDelegation(audience, abilities, {
+    expiration,
+  });
+
+  const archive = await delegation.archive();
+  return archive.ok;
+}
+
+/** @param {string} data Base64 encoded CAR file */
+async function parseProof(proof: string) {
+  const blocks = [];
+  const reader = await CarReader.fromBytes(Buffer.from(proof, 'base64'));
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const block of reader.blocks()) {
+    blocks.push(block as Block<unknown, number, number, 1>);
+  }
+  return Delegation.importDAG(blocks);
+}
+
+export const handler: (
+  req: NextApiRequest,
+  res: NextApiResponse<Record<string, string>>,
+) => Promise<void> = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Record<string, string>>,
+) => {
+  try {
+    const { did } = req.body;
+    if (!did) {
+      throw new Error('Missing `did` in request body.');
+    }
+    const ucan = await w3sDelegation(did);
+    if (!ucan) {
+      throw new Error(`No UCAN generated for "${did}".`);
+    }
+    res.status(201).json({ ucan: ucan.toString() });
+  } catch (error) {
+    console.error({ 'error generating W3S UCAN': error });
+    res.status(500).json({ error: (error as Error).message });
+  }
+};
+
+export default handler;
