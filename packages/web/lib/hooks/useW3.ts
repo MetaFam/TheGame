440ace9287b30b03a588ac2975fcd0b802523c9e
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-01-19 09:07:03 -0500

    Revert "yay"
    This reverts commit 4585b4742308caa5555514c05918be413ee2708e.

--- packages/web/lib/hooks/useW3.ts
+++ packages/web/lib/hooks/useW3.ts
@@ -1,26 +1,18 @@
 import { delegate } from "pages/api/w3up-client";
 import { Client } from "@web3-storage/w3up-client";
-import { useState, useEffect } from "react";
-import { CONFIG } from "config";
 
-export function useW3upClient() {
-  const [w3upClient, setW3upClient] = useState<Client | null>(null);
-  const { web3StorageDID } = CONFIG;
-  useEffect(() => {
-    if (!web3StorageDID) return;
-    async function fetchW3upClient() {
-      try {
-        const result = await delegate(web3StorageDID);
-        if (result) {
-          setW3upClient(result[1] as Client);
-        }
-      } catch (error) {
-        console.error('Error occurred during delegation:', error);
-      }
+export async function useW3upClient(did: string) {
+  try {
+    const result = await delegate(did);
+    console.log('result', result);
+    if (result) {
+      const res = (result[1] as Client)?.uploadFile(new File(['hello, is this working???'], 'file.txt'));
+      console.log('res', res);
+      return result[1];
+    } else {
+      console.log('Delegation failed');
     }
-
-    fetchW3upClient();
-  }, [web3StorageDID]);
-
-  return w3upClient;
+  } catch (error) {
+    console.error('Error occurred during delegation:', error);
+  }
 }
