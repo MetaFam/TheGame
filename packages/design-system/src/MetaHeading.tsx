02ec404712f1e079ab12bec1c4eb8aba627dc162
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-01-26 10:23:12 -0500

    remove react imports, remove sourced

--- packages/design-system/src/MetaHeading.tsx
+++ packages/design-system/src/MetaHeading.tsx
@@ -1,5 +1,5 @@
 import { Heading, HeadingProps } from '@chakra-ui/react';
-
+import React from 'react';
 
 export const MetaHeading: React.FC<HeadingProps> = ({ children, ...props }) => (
   <Heading
