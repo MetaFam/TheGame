b039c70c203475abbabf37105b3c2bec71190122
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-01-29 06:40:28 -0500

    restructure

--- packages/web/components/Player/Section/PlayerAchievements.tsx
+++ packages/web/components/Player/Section/PlayerAchievements.tsx
@@ -1,6 +1,7 @@
 import { HStack, Text } from '@metafam/ds';
 import { ProfileSection } from 'components/Section/ProfileSection';
 import { Player } from 'graphql/autogen/types';
+import React from 'react';
 import { FaMedal } from 'react-icons/fa';
 import { BoxTypes } from 'utils/boxTypes';
 
