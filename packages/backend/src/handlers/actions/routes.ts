37c05d7882809e392bf2c030fe75c18aec037be4
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-02-06 10:14:00 -0500

    remove sourcecred

--- packages/backend/src/handlers/actions/routes.ts
+++ packages/backend/src/handlers/actions/routes.ts
@@ -6,16 +6,10 @@ import { guildRoutes } from './guild/routes.js';
 import { syncAllGuildDiscordMembers } from './guild/sync.js';
 import syncBalances from './player/syncBalances.js';
 import { questsRoutes } from './quests/routes.js';
-import { syncSourceCredAccounts } from './sourcecred/sync.js';
-
 export const actionRoutes = express.Router();
 
 actionRoutes.post('/syncBalances', asyncHandlerWrapper(syncBalances));
 
-actionRoutes.post(
-  '/syncSourceCredAccounts',
-  asyncHandlerWrapper(syncSourceCredAccounts),
-);
 actionRoutes.post(
   '/syncAllGuildDiscordMembers',
   asyncHandlerWrapper(syncAllGuildDiscordMembers),
